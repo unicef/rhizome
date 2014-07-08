9f59dd3ddc803bc1abd6ea72f8899d6b4b872f56
John Dingee <dingee@seedscientific.com>
2014-07-08 17:38:12 -0400

    add models for datapoints and indicators

---
+++ datapoints/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+class DataPointIndicator(models.Model):
+	name = models.CharField(max_length=55)
+	description = models.CharField(max_length=255)
+	is_reported = models.BooleanField(default=True)
+	parent_indicator_id = models.IntegerField(null=True)
+
+
+class DataPoint(models.Model):
+	indicator = models.ForeignKey(DataPointIndicator)
+	value = models.IntegerField(default=0)
+	note = models.CharField(max_length=255)
+
+### DataPoiint Extras ###
+	# status_id
+	# geography # change to geo_id
+	# source_id
+	# time_period 
+	# updated_by_user_id
+	# group_id 
+
+
+### Indicator Extras ###	
+	# time_interval 
+	# evaluation_levels
+	# aggregation_details
