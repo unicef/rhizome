f6286017e6c000b4ef24dd2dd8c488ed7a3f25d3
John Dingee <dingee@seedscientific.com>
2014-07-09 16:42:00 -0400

    add test cases for datapoints modules

--- datapoints/models.py
+++ datapoints/models.py
@@ -4,8 +4,11 @@ class DataPointIndicator(models.Model):
     name = models.CharField(max_length=55)
     description = models.CharField(max_length=255)
     is_reported = models.BooleanField(default=True)
-    parent_indicator_id = models.ForeignKey("DataPointIndicator")
+    parent_indicator_id = models.ForeignKey("DataPointIndicator",null=True)
     created_at = models.DateTimeField(auto_now=True)
+
+    def __unicode__(self):
+        return self.name
 
     class Meta:
         db_table = 'datapoint_indicator'
@@ -14,11 +17,14 @@ class Region(models.Model):
     full_name = models.CharField(max_length=255)
     short_name = models.CharField(max_length=55)
     description = models.CharField(max_length=255,null=True)
-    parent_region_id = models.ForeignKey("Region")
+    parent_region_id = models.ForeignKey("Region",null=True)
     shape_file_path  = models.CharField(max_length=255,null=True)
     latitude = models.DecimalField(max_digits=12, decimal_places =10,null=True)
     longitude = models.DecimalField(max_digits=13, decimal_places =10,null=True)
     created_at = models.DateTimeField(auto_now=True)
+
+    def __unicode__(self):
+        return self.short_name
 
     class Meta:
         db_table = 'region'
@@ -29,6 +35,9 @@ class DataPoint(models.Model):
     value = models.IntegerField(default=0)
     note = models.CharField(max_length=255,null=True)
     created_at = models.DateTimeField(auto_now=True)
+
+    def __unicode__(self):
+        return self.value
 
     class Meta:
         db_table = 'datapoint'
---
+++ datapoints/tests/test_models.py
@@ -0,0 +1,36 @@
+from django.test import TestCase
+from datapoints.models import DataPointIndicator, Region, DataPoint
+from django.core.urlresolvers import reverse
+
+
+class DataPointIndicatorTest(TestCase):
+
+    def create_data_point_indicator(self, name="test", description = "dtest"):
+        return DataPointIndicator.objects.create(name=name,description=description)
+
+    def test_datapoint_indicator_createion(self):
+        dpi = self.create_data_point_indicator()
+        self.assertTrue(isinstance,(dpi,DataPointIndicator))
+        self.assertEqual(dpi.__unicode__(),dpi.name)
+
+
+class RegionTest(TestCase):
+
+    def create_region(self, full_name="test", short_name = "short test"):
+        return Region.objects.create(full_name=full_name,short_name=short_name)
+
+    def test_datapoint_indicator_createion(self):
+        r = self.create_region()
+        self.assertTrue(isinstance,(r,Region))
+        self.assertEqual(r.__unicode__(),r.short_name)
+
+class DataPointTest(TestCase):
+
+    def create_datapoint(self, note="test"):
+        return DataPoint.objects.create(note=note, indicator_id=99, region_id = 99)
+        ## This should break on a foreign key violation but it doesnt!!
+
+    def test_datapoint_createion(self):
+        dp = self.create_datapoint()
+        self.assertTrue(isinstance,(dp,DataPoint))
+        self.assertEqual(dp.__unicode__(),dp.value)
