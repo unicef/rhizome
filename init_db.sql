ecbb304cfdf14a4b2bec9927a5aefeb03fd07fba
John Dingee <dingee@seedscientific.com>
2014-07-09 15:10:55 -0400

    getting start with tests

--- init_db.sql
+++ init_db.sql
@@ -6,3 +6,13 @@ GRANT ALL PRIVILEGES ON DATABASE "polio" to djangoapp
 
 select * from information_schema.tables
 where table_schema = 'public'
+
+DROP TABLE datapoint;
+DROP TABLE datapoint_indicator;
+DROP TABLE region;
+
+
+select * from datapoint;
+select * from datapoint_indicator;
+select * from region;
+
