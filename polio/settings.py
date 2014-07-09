a0d84e7e50bd69a60d66607fcc3db3cc567c2618
John Dingee <dingee@seedscientific.com>
2014-07-09 17:55:58 -0400

    create bash script to run tests

--- polio/settings.py
+++ polio/settings.py
@@ -10,6 +10,7 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+import sys
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 
@@ -57,6 +58,7 @@ WSGI_APPLICATION = 'polio.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
+
 
 DATABASES = {
     'default': {
