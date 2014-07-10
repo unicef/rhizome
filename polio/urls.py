b4e131b13de23f9ec1e948951929ebf621c2cb72
John Dingee <dingee@seedscientific.com>
2014-07-10 13:59:33 -0400

    namespace url names

--- polio/urls.py
+++ polio/urls.py
@@ -7,6 +7,8 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'polio.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-    url(r'^datapoints/', include('datapoints.urls')),
+    # url(r'^datapoints/', include('datapoints.urls')),
+
+    url(r'^datapoints/', include('datapoints.urls', namespace="datapoints")),
     url(r'^admin/', include(admin.site.urls)),
 )
