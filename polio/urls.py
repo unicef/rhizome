edad2f0ab972add762b2299b033161863cda4dbe
John Dingee <dingee@seedscientific.com>
2014-07-10 12:30:49 -0400

    add first view and set up route

--- polio/urls.py
+++ polio/urls.py
@@ -7,6 +7,6 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'polio.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^datapoints/', include('datapoints.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
