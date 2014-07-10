cc18c6789b5f9f0964e47643e3a5f8e6e38fdfc1
John Dingee <dingee@seedscientific.com>
2014-07-10 13:14:00 -0400

    retreive all datapoints from view

--- datapoints/urls.py
+++ datapoints/urls.py
@@ -3,5 +3,7 @@ from django.conf.urls import url
 from datapoints import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index')
+    url(r'^$', views.index, name='index'),
+    # ex: /polls/5/
+    url(r'^(?P<datapoint_id>[0-9]+)/$', views.show, name='show'),
 ]
--- datapoints/views.py
+++ datapoints/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from datapoints.models import DataPoint
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the datapoints index.")
+    latest_datapoints = DataPoint.objects.order_by('-created_at')[:5]
+    output = ', '.join([str(d.value) for d in latest_datapoints])
+    return HttpResponse(output)
+
+
+def show(request, datapoint_id):
+    return HttpResponse("You're looking at datapoint %s." % datapoint_id)
