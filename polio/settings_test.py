010dbea95bb2e9d046113a15bd2ebba48e47a221
John Dingee <dingee@seedscientific.com>
2014-07-10 09:45:15 -0400

    remove comments from settings file

--- polio/settings_test.py
+++ polio/settings_test.py
@@ -3,9 +3,3 @@ from settings import *
 SOUTH_TESTS_MIGRATE = False
 DATABASES['default'] = {'ENGINE': 'django.db.backends.sqlite3'}
 
-# from project root run:
-
-    # coverage run manage.py test datapoints --settings=polio.settings_test 
-    # coverage html --omit='venv/*.py,datapoints/tests/*.py'
-
-    
