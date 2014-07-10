420f77503406550be96d21683b4987938f456b8e
John Dingee <dingee@seedscientific.com>
2014-07-10 11:09:24 -0400

    add bootstrap to django admin

--- polio/settings.py
+++ polio/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = (
     'south',
+    'django_admin_bootstrapped',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
