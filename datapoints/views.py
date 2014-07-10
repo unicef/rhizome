3afe528853efd3a8b25d16bc4da8302db84e2639
John Dingee <dingee@seedscientific.com>
2014-07-10 13:21:06 -0400

    plug in template for datapoints

--- datapoints/views.py
+++ datapoints/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import RequestContext, loader
+
 from datapoints.models import DataPoint
 
 def index(request):
     latest_datapoints = DataPoint.objects.order_by('-created_at')[:5]
-    output = ', '.join([str(d.value) for d in latest_datapoints])
-    return HttpResponse(output)
+    template = loader.get_template('datapoints/index.html')
+    context = RequestContext(request, {
+            'latest_datapoints': latest_datapoints,
+        })
+
+    return HttpResponse(template.render(context))
 
 
 def show(request, datapoint_id):
