9340f5188a2ef288b33bd78d0afb33e8d73d503d
John Dingee <dingee@seedscientific.com>
2014-07-11 14:20:02 -0400

    dont throw 404 when requesting the index page

--- polio/urls.py
+++ polio/urls.py
@@ -5,7 +5,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'polio.views.home', name='home'),
+    url(r'^$', 'polio.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
     # url(r'^datapoints/', include('datapoints.urls')),
 
