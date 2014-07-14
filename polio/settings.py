8dfcf7670af10478cc9edc1e4e031cae927b95d1
John Dingee <dingee@seedscientific.com>
2014-07-14 12:39:35 -0400

    get started with bootstrap

--- polio/settings.py
+++ polio/settings.py
@@ -39,8 +39,10 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bootstrap3',
     'datapoints',
     'coverage',
+
 )
 
 MIDDLEWARE_CLASSES = (
