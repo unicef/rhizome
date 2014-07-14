07d07c348d8364694a6a1c2047a9033c87995f74
John Dingee <dingee@seedscientific.com>
2014-07-14 09:54:42 -0400

    put datpoint and region links on homepage

--- polio/views.py
+++ polio/views.py
@@ -2,4 +2,5 @@ from django.http import HttpResponse
 
 
 def home(request):
-    return HttpResponse("Hello Welcome to the Polio Management app!")
+
+    return HttpResponse("Hello Welcome to the Polio Management app!\n<ul><li><a href='/datapoints/'>dataoints  </a></li><li><a href='/regions/'>regions  </a></li></ul>")
