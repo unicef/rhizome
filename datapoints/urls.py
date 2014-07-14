de6555848b3a9dbf18000ae3c2def3cf7f1e029b
John Dingee <dingee@seedscientific.com>
2014-07-14 17:52:50 -0400

    move ALL logic from view to urls.py

--- datapoints/urls.py
+++ datapoints/urls.py
@@ -3,37 +3,77 @@ from datapoints import views
 from datapoints.models import DataPoint, Region, Indicator
 
 urlpatterns = [
-    ## DATAPOINTS ##
-    url(r'^$', views.DataPointIndexView.as_view(), name='datapoint_index'),
-    url(r'^(?P<pk>[0-9]+)/$', views.DataPointDetailView.as_view(), name='datapoint_detail'),
-    url(r'^create_datapoint/$', views.CreateView.as_view
-        (model=DataPoint
-        ,success_url="/datapoints"
-        ,template_name='datapoints/create_datapoint.html')
-        , name='create_datapoint'),
+
+        ################
+        ## DATAPOINTS ##
+        ################
+
+    ## INDEX ##
+    url(r'^$', views.IndexView.as_view(
+        model=DataPoint,
+        template_name = 'datapoints/index.html',
+        context_object_name = 'latest_datapoints'),
+    name='datapoint_index'),
+
+    ## DETAIL ##
+    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(
+        model=DataPoint,
+        template_name='datapoints/detail.html'),
+    name='datapoint_detail'),
+
+    ## CREATE ##
+    url(r'^create_datapoint/$', views.CreateView.as_view(
+        model=DataPoint,
+        success_url="/datapoints",
+        template_name='datapoints/create_datapoint.html'),
+    name='create_datapoint'),
     
+        #############
+        ## REGIONS ##
+        #############
+
+    ## INDEX ##
+    url(r'^regions$', views.IndexView.as_view(
+        template_name = 'regions/index.html',
+        context_object_name = 'top_regions'),
+    name='region_index'),
+
+    ## DETAIL ##
+    url(r'^regions/(?P<pk>[0-9]+)/$', views.DetailView.as_view(
+        model=Region,
+        template_name='regions/detail.html'),
+    name='region_detail'),
+
+    ## CREATE ##
+    url(r'^regions/create_region/$', views.CreateView.as_view(
+        model=Region,
+        success_url="/datapoints/regions",
+        template_name='regions/create_region.html'),
+    name='create_region'),
 
 
-    ## REGIONS ##
-    url(r'^regions$', views.RegionIndexView.as_view(), name='region_index'),
-    url(r'^regions/(?P<pk>[0-9]+)/$',views.RegionDetailView.as_view(), name='region_detail'),
-    url(r'^regions/create_region/$', views.CreateView.as_view
-        (model=Region
-        ,success_url="/datapoints/regions"
-        ,template_name='regions/create_region.html')
-        , name='create_region'),
+        ################
+        ## INDICATORS ##
+        ################
 
+    ## INDEX ##
+    url(r'^indicators$', views.IndexView.as_view(
+        model=Indicator,
+        template_name = 'indicators/index.html',
+        context_object_name = 'top_indicators'),
+    name='indicator_index'),
 
-    ## INDICATORS ##
-    url(r'^indicators$', views.IndicatorIndexView.as_view(), name='indicator_index'),
-    url(r'^indicators/(?P<pk>[0-9]+)/$',views.IndicatorDetailView.as_view(), name='indicator_detail'),
-    url(r'^indicators/create_indicator/$', views.CreateView.as_view
-        (model=Indicator
-        ,success_url="/datapoints/indicators"
-        ,template_name='indicators/create_indicator.html')
-        , name='create_indicator'),
+    ## DETAIL ##
+    url(r'^indicators/(?P<pk>[0-9]+)/$', views.DetailView.as_view(
+        model=Indicator,
+        template_name='indicators/detail.html'),
+    name='indicator_detail'),
 
-
-
+    ## CREATE ##
+    url(r'^indicators/create_indicator/$', views.CreateView.as_view(
+         model=Indicator,
+        success_url="/datapoints/indicators",
+        template_name='indicators/create_indicator.html'),
+    name='create_indicator'),
 
 ]
--- datapoints/views.py
+++ datapoints/views.py
@@ -7,69 +7,15 @@ from datapoints.models import DataPoint,Region,Indicator
 from datapoints.forms import RegionForm,IndicatorForm,DataPointForm
 
 
-class DataPointIndexView(generic.ListView):
-    template_name = 'datapoints/index.html'
-    context_object_name = 'latest_datapoints'
+class IndexView(generic.ListView):
+    pass # template name and model passed via the URL.
 
     def get_queryset(self):
-        return DataPoint.objects.order_by('-created_at')[:10]  
+        return self.model.objects.order_by('-created_at')[:10]  
 
 
-class DataPointDetailView(generic.DetailView):
-    model = DataPoint
-    template_name = 'datapoints/detail.html'
-
+class DetailView(generic.DetailView):
+    pass # template name and model passed via the URL.
 
 class CreateView(generic.CreateView):
     pass # template name and model passed via the URL.
-
-
-
-    ###### REGIONS ######
-
-
-class RegionIndexView(generic.ListView):
-    template_name = 'regions/index.html'
-    context_object_name = 'top_regions'
-
-
-    def get_queryset(self):
-        return Region.objects.order_by('-created_at')[:10]  
-
-
-class RegionDetailView(generic.DetailView):
-    model = Region
-    template_name = 'regions/detail.html'
-
-
-            ###### INDICATORS ######
-
-
-class IndicatorIndexView(generic.ListView):
-    template_name = 'indicators/index.html'
-    context_object_name = 'top_indicators'
-
-
-    def get_queryset(self):
-        return Indicator.objects.order_by('-created_at')[:10]  
-
-
-class IndicatorDetailView(generic.DetailView):
-    model = Indicator
-    template_name = 'indicators/detail.html'
-
-
-def create_indicator(request):
-    if request.method == 'GET':
-        form = IndicatorForm()
-    else:
-        form = IndicatorForm(request.POST)
-        if form.is_valid():
-            content = form.cleaned_data
-            indicator = Indicator.objects.create(**content)
-
-            return HttpResponseRedirect('/datapoints/indicators')
-
-    return render(request, 'indicators/create_indicator.html',{'form':form,})
-
-
