f538504cbf51c541413ae483f942cfbb29c5088a
John Dingee <dingee@seedscientific.com>
2014-07-15 10:48:07 -0400

    create prod settings file

---
+++ polio/prod_settings.py
@@ -0,0 +1,64 @@
+import os
+import sys
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '7i_%j5chyhx2k3#874-!8kwwlcr88sn9blbsb7$%58h&t#n84f' # make this envi var
+
+DEBUG = False
+TEMPLATE_DEBUG = False
+
+ALLOWED_HOSTS = []
+
+INSTALLED_APPS = (
+    'south',
+    'django_admin_bootstrapped',
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'datapoints',
+    'coverage',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+ROOT_URLCONF = 'polio.urls'
+WSGI_APPLICATION = 'polio.wsgi.application'
+
+## DATABASE ##
+
+# DATABASES['default'] = {'ENGINE': 'django.db.backends.sqlite3'}
+DATABASES = {
+    'default': { # I HAVENT SET THIS UP YET
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'polio',
+        'USER': 'djangoapp',
+        'PASSWORD': 'w3b@p01i0',
+        'HOST': '50.57.77.252',
+        'PORT': '5432',
+    }
+}
+
+## INTERNATIONALIZATION ##
+# https://docs.djangoproject.com/en/1.6/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+TIME_ZONE = 'UTC'
+USE_I18N = True
+USE_L10N = True
+USE_TZ = True
+
+# STATIC FILES (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.6/howto/static-files/
+
+STATIC_URL = '/static/'
