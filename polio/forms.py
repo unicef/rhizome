d571c21e76076d995697d3b68fd766942fabaf72
John Dingee <dingee@seedscientific.com>
2014-07-18 13:48:39 -0400

    encrypt password by inheriting from the UserCreationForm

--- polio/forms.py
+++ polio/forms.py
@@ -1,9 +1,9 @@
-from django.forms import ModelForm
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
-class UserForm(ModelForm):
+class UserForm(UserCreationForm):
 
     class Meta:
         model = User
-        fields = ['username','email','password']
+        fields = ['username','email']
 
