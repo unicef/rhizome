616da705e66797c0ca07dc5cfb73c386f48199d3
John Dingee <dingee@seedscientific.com>
2014-09-08 13:27:01 -0400

    abstract work table ingest completely

--- source_data/etl_tasks/refresh_work_tables.py
+++ source_data/etl_tasks/refresh_work_tables.py
@@ -23,22 +23,18 @@ class WorkTableTask(object):
 
         self.request_guid = request_guid
         self.file_to_process = file_to_process
+        self.file_to_function_map = {
+            "VCM_Sett_Coordinates_1_2.csv" : VCMSettlement,
+            "New_VCM_Summary.csv" : VCMSummaryNew
+        }
 
-
+        # this below shoudl be a configuration
         self.csv_dir = '/Users/johndingee_seed/code/polio/static/odk_source/csv_exports/'
         self.columns = odk_settings.HEADER_DICT[self.file_to_process]
 
-        self.file_to_function_map = {
-            "VCM_Sett_Coordinates_1_2.csv" : self.ingest_sett_coordinates,
-            "New_VCM_Summary.csv" : self.ingest_vcm_summary_new
-        }
-
-        # prep the data frame
-        self.df = self.build_dataframe(self.file_to_process)
         # execute the relevant function
-        fn = self.file_to_function_map[self.file_to_process]
-
-        x = fn()
+        work_table_obj = self.file_to_function_map[self.file_to_process]
+        self.csv_to_work_table(work_table_obj)
 
 
     def df_row_to_dict(self,row):
@@ -54,59 +50,25 @@ class WorkTableTask(object):
         return output_dict
 
 
-    def build_dataframe(self,file_name):
+    def build_dataframe(self):
 
-        f = self.csv_dir + file_name
+        f = self.csv_dir + self.file_to_process
 
         df = pd.read_csv(f, error_bad_lines = False)  # YOU NEED TO HANDLE ERRORS!
 
         return df
 
-    def ingest_sett_coordinates(self):
+    def csv_to_work_table(self, work_table_object):
 
-        for i,row in enumerate(self.df.values):
+        df = self.build_dataframe()
+
+        for i, row in enumerate(df.values):
             to_create = self.df_row_to_dict(row)
 
             try:
-                created = VCMSettlement.objects.create(**to_create)
+                created = work_table_object.objects.create(**to_create)
             except IntegrityError:
                 print 'key: ' +  row[self.columns.index('KEY')] + ' already exists...'
-
-
-    def ingest_vcm_summary_new(self):
-
-        for i, row in enumerate(self.df.values):
-            to_create = self.df_row_to_dict(row)
-            try:
-                created = VCMSummaryNew.objects.create(**to_create)
-            except IntegrityError:
-                print 'key: ' +  row[self.columns.index('KEY')] + ' already exists...'
-
-
-
-    def ingest_birth_records(self):
-
-        with open ("/Users/johndingee_seed/Desktop/ALL_ODK_DATA_8_25/VCM_Birth_Record.csv") as f:
-            f_reader = csv.reader(f, delimiter = ',', quotechar='"')
-            for i, row in enumerate(f_reader):
-                if i > 0:
-                    created = VCMBirthRecord.objects.create(
-                        SubmissionDate =row[0], \
-                        deviceid =row[1], \
-                        simserial =row[2], \
-                        phonenumber =row[3], \
-                        DateOfReport =row[4], \
-                        DateReport =row[5], \
-                        SettlementCode =row[6], \
-                        HouseHoldNumber =row[7], \
-                        DOB =row[8], \
-                        NameOfChild =row[9], \
-                        VCM0Dose =row[10], \
-                        VCMRILink =row[11], \
-                        VCMNameCAttended =row[12], \
-                        meta_instanceID =row[13], \
-                        KEY =row[13]
-                        )
 
 
 
