684f0f0bac98dca726078d7d31d4292e89b39721
John Dingee <dingee@seedscientific.com>
2014-09-17 12:36:36 -0400

    parsing uploaded xls

--- source_data/views.py
+++ source_data/views.py
@@ -1,11 +1,13 @@
+import xlrd, csv, pandas
+
 from django.shortcuts import render
-from source_data.forms import DocumentForm
-from source_data.models import Document
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from pandas.io.excel import read_excel
 
-import xlrd
-import csv
+from source_data.forms import DocumentForm
+from source_data.models import Document
+
 
 def file_upload(request):
     # Handle file upload
@@ -14,6 +16,11 @@ def file_upload(request):
         if form.is_valid():
             newdoc = Document(docfile = request.FILES['docfile'])
             newdoc.save()
+
+            file_path = newdoc.docfile.url
+
+            if file_path.endswith('xls') or file_path.endswith('xlsx'):
+                process_xls(file_path)
 
     else:
         form = DocumentForm() # A empty, unbound form
@@ -28,22 +35,48 @@ def file_upload(request):
         context_instance=RequestContext(request)
     )
 
-def process_xls():
 
-        f_path = '/Users/johndingee_seed/code/polio/media/documents/2014/09/16/SocMob.xlsx'
 
-        wb = xlrd.open_workbook(f_path)
+def process_xls(f_path):
 
-        for sheet in wb.sheets():
-            # process_sheet(sheet)
+    f_path = '/Users/johndingee_seed/code/polio/media/documents/2014/09/16/SocMob.xlsx'
 
-            if sheet.nrows == 0:
-                pass
-            else:
-                process_sheet(f_path,sheet.name)
+    wb = xlrd.open_workbook(f_path)
+
+    for sheet in wb.sheets():
+        # process_sheet(sheet)
+
+        if sheet.nrows == 0:
+            pass
+        else:
+            process_sheet(f_path,sheet.name)
+
 
 def process_sheet(file_path,sheet_name):
 
     df = read_excel(file_path,sheet_name)
 
-    print df
+    cols = [col.lower() for col in df]
+
+    row_basics = {}
+
+
+    for i,(row) in enumerate(df.values):
+
+        if i == 5:
+
+            region_string = row[cols.index('lga')] + '-' + row[cols.index('state')] \
+                + '-' + row[cols.index('ward')] + '-' + str(row[cols.index('settlement')])
+
+            row_basics['row_number'] = i
+            row_basics['region_string'] = region_string
+            row_basics['uniquesoc'] = row[cols.index('uniquesoc')]
+            row_basics['datesoc'] = str(row[cols.index('datesoc')])
+
+            for i,(cell) in enumerate(row):
+
+                to_create = row_basics
+                to_create['indicator_string'] = cols[i]
+                to_create['cell_value'] = cols[i]
+
+                print to_create
