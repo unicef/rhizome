fd99979424cc3f26f1391601643864f4da60b65a
John Dingee <dingee@seedscientific.com>
2014-09-22 17:19:37 -0400

    remove meta map app from prod settings

--- polio/prod_settings.py
+++ polio/prod_settings.py
@@ -25,7 +25,6 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'datapoints',
-    'meta_map',
     'simple_history',
     'coverage',
     # 'stronghold',
