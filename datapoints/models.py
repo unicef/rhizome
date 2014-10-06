bfd656fce31f9a6243b21a9b45acb8b1c9176d64
John Dingee <dingee@seedscientific.com>
2014-10-06 11:36:27 -0400

    regoin code is a string

--- datapoints/models.py
+++ datapoints/models.py
@@ -47,7 +47,7 @@ class Office(models.Model):
 class Region(models.Model):
 
     full_name = models.CharField(max_length=55,unique=True)
-    region_code = models.IntegerField(unique=True)
+    region_code = models.CharField(max_length=10,unique=True)
     region_type = models.CharField(max_length=55)
     office = models.ForeignKey(Office)
     shape_file_path  = models.CharField(max_length=255,null=True,blank=True)
