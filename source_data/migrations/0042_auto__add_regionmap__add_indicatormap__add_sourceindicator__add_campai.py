fef5eb13e4e6dc1578b40a555dd6931a8c5f063d
John Dingee <dingee@seedscientific.com>
2014-10-10 16:28:47 -0400

    tweaking with the migration dependencies

--- source_data/migrations/0042_auto__add_regionmap__add_indicatormap__add_sourceindicator__add_campai.py
+++ source_data/migrations/0042_auto__add_regionmap__add_indicatormap__add_sourceindicator__add_campai.py
@@ -7,8 +7,9 @@ from django.db import models
 
 class Migration(SchemaMigration):
     depends_on = (
-        ('datapoints','0001_init_datapoints_db.py')
+        ("datapoints","0001_init_datapoints_db.py"),
     )
+
 
     def forwards(self, orm):
         # Adding model 'RegionMap'
