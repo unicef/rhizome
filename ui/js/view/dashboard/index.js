59cf8a2aa441d2821e9d29042a7bbbbd5d135ee1
W. Evan Sheehan <wallace.sheehan@gmail.com>
2014-12-03 17:16:07 -0500

    Remove stale requires.

--- ui/js/view/dashboard/index.js
+++ ui/js/view/dashboard/index.js
@@ -14,19 +14,8 @@ module.exports = {
 
 	components: {
 		'management-dashboard': require('../../dashboard/management'),
-		'chart-axis'          : require('../../component/chart/axis'),
 		'chart-base'          : require('../../component/chart'),
-		'chart-line'          : require('../../component/chart/line'),
 		'chart-pie'           : require('../../component/chart/pie'),
-		'chart-arc'           : require('../../component/chart/arc'),
-		'chart-stacked'       : require('../../component/chart/stacked'),
-		'chart-bullet'        : require('../../component/chart/bullet'),
 		'chart-stackedTime'   : require('../../component/chart/stackedTime')
-	},
-
-	partials: {
-		'y-grid' : require('../../component/chart/yAxis.html'),
-		'y-ticks': require('../../component/chart/yAxis.html'),
-		'x-ticks': require('../../component/chart/xAxis.html')
 	}
 };
