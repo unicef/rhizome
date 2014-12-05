0c26ac0a1a480c7959a598e0ff8ae216fb17d720
John Dingee <dingee@seedscientific.com>
2014-12-05 16:27:03 -0500

    dont break if not passed indicators

--- datapoints/api/simple.py
+++ datapoints/api/simple.py
@@ -130,8 +130,8 @@ class CustomSerializer(Serializer):
 
         response['objects'] = response_objects
 
-        return json.dumps(response)
-        # return json.dumps(data)
+        # return json.dumps(response)
+        return json.dumps(data)
 
 
 
@@ -322,6 +322,11 @@ class DataPointResource(SimpleApiResource):
         regions, campaigns, indicators, the_limit = \
             self.parse_url_params(query_dict)
 
+        if isinstance(indicators,list):
+            indicator_list = indicators
+
+        else:
+            indicator_list = [int(ind) for ind in indicators.split(',')]
 
         # find all of the distinct regions / campaigns in the db
         all_region_campaign_tuples = DataPoint.objects.values_list('region',\
@@ -332,7 +337,7 @@ class DataPointResource(SimpleApiResource):
         # if there was no region or campaign passed in just take the first
         # x elements in the list ( where x is the_limit ) and return that
         if len(regions) == 0 and len(campaigns) == 0:
-            return all_region_campaign_tuples[:the_limit], indicators
+            return all_region_campaign_tuples[:the_limit], indicator_list
 
 
 
@@ -345,7 +350,7 @@ class DataPointResource(SimpleApiResource):
         for r,c in all_region_campaign_tuples:
 
             if len(final_region_campaign_tuples) == the_limit:
-                return final_region_campaign_tuples, indicators
+                return final_region_campaign_tuples, indicator_list
 
             elif str(r) in regions and str(c) in campaigns:
                 final_region_campaign_tuples.append((r,c))
@@ -360,7 +365,7 @@ class DataPointResource(SimpleApiResource):
                 pass
 
 
-        return final_region_campaign_tuples, indicators
+        return final_region_campaign_tuples, indicator_list
 
 
     def obj_get_list(self, bundle, **kwargs):
@@ -385,11 +390,15 @@ class DataPointResource(SimpleApiResource):
         regions = list(set([rc[0] for rc in region_campaign_tuples]))
         campaigns = list(set([rc[1] for rc in region_campaign_tuples]))
 
+        print indicators
+
         object_list = DataPoint.objects.filter(
             region__in = regions,
             campaign__in = campaigns,
-            indicator__in = indicators.split(',')
+            indicator__in = indicators
         )
+        print 'THIS IS SOMETHING '
+        print object_list
 
         return object_list
 
