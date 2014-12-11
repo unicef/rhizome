c6b65fa5f3fae0115b5ad33f22ab5b863b97dd18
John Dingee <dingee@seedscientific.com>
2014-12-11 10:57:58 -0500

    fix api

--- datapoints/api/datapoint.py
+++ datapoints/api/datapoint.py
@@ -1,9 +1,7 @@
-import pprint as pp
-
 from tastypie.resources import ALL
-from tastypie.resources import Resource
 from tastypie.bundle import Bundle
 from tastypie import fields
+from tastypie.resources import Resource
 from pandas import DataFrame
 
 from datapoints.models import *
@@ -44,8 +42,8 @@ class DataPointResource(Resource):
       https://gist.github.com/nomadjourney/794424
       http://django-tastypie.readthedocs.org/en/latest/non_orm_data_sources.html
     '''
-
-    pk = fields.IntegerField(attribute = 'pk')
+    error = None
+    pk = fields.IntegerField(attribute = 'id')
 
     # region = fields.ToOneField(RegionResource, 'region')
     # indicator = fields.ToOneField(IndicatorResource, 'indicator')
@@ -58,14 +56,6 @@ class DataPointResource(Resource):
         resource_name = 'datapoint'
         max_limit = None
         # serializer = CustomSerializer()
-
-    def __init__(self):
-        self.error = None
-
-
-    # def get_resource_uri(self, bundle_or_obj):
-        # https://gist.github.com/nomadjourney/794424
-        # return self._build_reverse_url('api_dispatch_detail', kwargs = {})
 
 
     def detail_uri_kwargs(self, bundle_or_obj):
@@ -82,11 +72,12 @@ class DataPointResource(Resource):
 
         results = []
 
-        err, params = self.parse_url_params(request.GET)
+        err,params = self.parse_url_params(request.GET)
 
         if err:
             self.error = err
             return results
+
 
         ## get distinct regions/campaigns for the provided indicators
         all_region_campaign_tuples = DataPoint.objects.filter(indicator__in=\
@@ -95,11 +86,13 @@ class DataPointResource(Resource):
         df = DataFrame(list(all_region_campaign_tuples),columns=['region',\
             'campaign'])
 
+        print df
+
         for result in range(0,5):
 
             new_obj = ResultObject()
             new_obj.id = result
-            results.append('new_obj')
+            results.append(new_obj)
 
         return results
 
@@ -107,7 +100,7 @@ class DataPointResource(Resource):
     def obj_get_list(self,bundle,**kwargs):
         '''
         Outer method for get_object_list... this calls get_object_list and
-        could be a point at which additional filtering may be applied.
+        could be a point at which additional filtering may be applied
         '''
 
         return self.get_object_list(bundle.request)
@@ -118,31 +111,20 @@ class DataPointResource(Resource):
         try:
             return data[pk]
         except KeyError:
-            print 'blasblasbfalsfbs\n' * 10
             raise NotFound("Object not found")
+
 
     def alter_list_data_to_serialize(self, request, data):
         '''
         If there is an error for this resource, add that to the response.  If
         there is no error, than add this key, but set the value to null'''
 
-        # print self.error
         if self.error:
             data['error'] = self.error
         else:
             data['error'] = None
 
-
         return data
-
-    def full_dehydrate(self,bundle,for_list):
-        '''
-        When i dont overide this method, i get a maximum recursion error.
-        http://stackoverflow.com/questions/11570443/django-tastypie-throws-a-maximum-recursion-depth-exceeded-when-full-true-on-re
-        '''
-        print bundle.data
-
-        return bundle
 
 
     ##########################
@@ -154,20 +136,11 @@ class DataPointResource(Resource):
         parsed_params = {}
 
         optional_params = {
-            'region__in':None,
-            'campaign__in':None,
-            'campaign_end':None,
-            'campaign_start':None,
-            'the_limit':None,
-            'the_offset':0,
-            'uri_format':'id',
-            'agg_level':'mixed',
+            'region__in':None,'campaign__in':None,'campaign_end':None,\
+            'campaign_start':None,'the_limit':None,'the_offset':0,
+            'uri_format':'id','agg_level':'mixed'}
 
-        }
-
-        required_params = {
-            'indicator__in':None,
-        }
+        required_params = {'indicator__in':None}
 
 
         for k,v in required_params.iteritems():
@@ -175,9 +148,6 @@ class DataPointResource(Resource):
             try:
                 parsed_params[k] = query_dict[k].split(',')
             except KeyError as err:
-                print 'THIS IS HAPPENING\n' * 10
-
                 return str(err).replace('"','') + ' is a required paramater!', None
-
 
         return None, parsed_params
