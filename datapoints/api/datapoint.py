5eddb1256c705e63b79fe6fb29e402d6343436fc
John Dingee <dingee@seedscientific.com>
2014-12-11 13:27:30 -0500

    cleanup, document, and add total_count to meta object

--- datapoints/api/datapoint.py
+++ datapoints/api/datapoint.py
@@ -1,3 +1,4 @@
+import pprint as pp
 from tastypie.resources import ALL
 from tastypie.bundle import Bundle
 from tastypie import fields
@@ -18,7 +19,6 @@ class ResultObject(object):
 
     campaign = None
     region = None
-    # changed_by_id = None
     # indicators = []
 
 
@@ -45,46 +45,45 @@ class DataPointResource(Resource):
     '''
 
     error = None
+    total_count = 0
     campaign = fields.IntegerField(attribute = 'campaign')
     region = fields.IntegerField(attribute = 'region')
 
 
 
     class Meta(BaseApiResource.Meta):
-        object_class = ResultObject
-        resource_name = 'datapoint'
-        max_limit = None
+
+        object_class = ResultObject # use the class above to devine the response
+        resource_name = 'datapoint' # cooresponds to the URL of the resource
+        max_limit = None # return all rows by default ( limit defaults to 20 )
         # serializer = CustomSerializer()
 
 
     def get_object_list(self,request):
+        '''
+        This method is overriden from tastypie.  When using a model resource
+        the typical behavior of this method woudl be to select all datapoints
+        for example.  Instead, all of this behaviour is 100pct custom and
+        and cooresponds to the business case that we need to accomidate
+        as well as the requirements of the front end application.
+        '''
 
         results = []
 
-        err,params = self.parse_url_params(request.GET)
+        err,parsed_params = self.parse_url_params(request.GET)
 
         if err:
             self.error = err
             return results
 
-        ## get distinct regions/campaigns for the provided indicators
-        all_region_campaign_tuples = DataPoint.objects.filter(
-            indicator__in=params['indicator__in'],\
-            region__in=params['region__in']).values_list('region','campaign').distinct()
+        err, r_c_df = self.build_campaign_region_df(parsed_params)
 
-        ## throw error if the indicators yield no r/c couples
-        if len(all_region_campaign_tuples) == 0:
-            self.error = 'There are no datapoints for the parameters requested'
+        if err:
+            self.error = err
             return results
 
-        the_offset, the_limit = int(params['the_offset']), int(params['the_limit'])
 
-        ## build a dataframe with the region / campaign tuples and slice it
-        ## in accordance to the_offset and the_limit
-        df = DataFrame(list(all_region_campaign_tuples),columns=['region',\
-            'campaign'])[the_offset:the_limit + the_offset]
-
-        for row in df.values:
+        for row in r_c_df.values:
 
             print row
             new_obj = ResultObject()
@@ -116,12 +115,17 @@ class DataPointResource(Resource):
     def alter_list_data_to_serialize(self, request, data):
         '''
         If there is an error for this resource, add that to the response.  If
-        there is no error, than add this key, but set the value to null'''
+        there is no error, than add this key, but set the value to null.  Also
+        add the total_count to the meta object as well'''
+
+        data['meta']['total_count'] = self.total_count
 
         if self.error:
             data['error'] = self.error
         else:
             data['error'] = None
+
+
 
         return data
 
@@ -130,22 +134,74 @@ class DataPointResource(Resource):
     ##### HELPER METHODS #####
     ##########################
 
+    def build_campaign_region_df(self,parsed_params):
+        '''
+        Build a dataframe that represents the regions and campaigns relevant to
+        the request.  These tuples fit the offset / limit bounds, as well as the
+        region / campaign / indicator filters on the datapoints table.  This way
+        we only return region/campaingns that actually have data in the db.
+        '''
+        ## get distinct regions/campaigns for the provided indicators
+
+        pp.pprint(parsed_params)
+
+        all_region_campaign_tuples = DataPoint.objects.filter(
+            indicator__in = parsed_params['indicator__in'],\
+            region__in = parsed_params['region__in'],\
+            campaign__in = parsed_params['campaign__in']).values_list\
+            ('region','campaign').distinct()
+
+        ## will save this to the meta object to allow for pagination
+        self.total_count = len(all_region_campaign_tuples)
+
+        ## throw error if the indicators yield no r/c couples
+        if len(all_region_campaign_tuples) == 0:
+            err = 'There are no datapoints for the parameters requested'
+            return err, None
+
+
+        ## find the offset and the limit
+        the_offset, the_limit = int(parsed_params['the_offset']), \
+            int(parsed_params['the_limit'])
+
+        ## build a dataframe with the region / campaign tuples and slice it
+        ## in accordance to the_offset and the_limit
+        df = DataFrame(list(all_region_campaign_tuples),columns=['region',\
+            'campaign'])[the_offset:the_limit + the_offset]
+
+        print df
+
+
+        return None, df
+
+
+
     def parse_url_params(self,query_dict):
+        '''
+        For the query dict return another dictionary ( or error ) in accordance
+        to the expected ( both required and optional ) parameters in the request
+        URL.
+        '''
 
         parsed_params = {}
 
-        optional_params = {
-            'campaign__in':None,'campaign_end':None,\
-            'campaign_start':None,'the_limit':10000,'the_offset':0,
-            'uri_format':'id','agg_level':'mixed'}
+        ## find the campaign__in parameter via the method below
+        parsed_params['campaign__in'] = self.find_campaigns(query_dict)
 
-        required_params = {'indicator__in': None,'region__in': None}
+        ## try to find optional parameters in the dictionary. If they are not
+        ## there return the default values ( given in the dict below)
+        optional_params = {'the_limit':10000,'the_offset':0,
+            'uri_format':'id','agg_level':'mixed'}
 
         for k,v in optional_params.iteritems():
             try:
                 parsed_params[k] = query_dict[k]
             except KeyError:
                 parsed_params[k] = v
+
+        ## find the Required Parameters and if they
+        ## dont exists return an error to the response
+        required_params = {'indicator__in': None,'region__in': None}
 
         for k,v in required_params.iteritems():
 
@@ -155,3 +211,36 @@ class DataPointResource(Resource):
                 return str(err).replace('"','') + ' is a required paramater!', None
 
         return None, parsed_params
+
+
+    def find_campaigns(self,query_dict):
+        '''
+        Based on the parameters passed for campaigns, start/end or __in
+        return to the parsed params dictionary a list of campaigns to query
+        '''
+        try:
+            ## if the campaign_in parameter exists return this
+            ## and ignore the campaign_start and end parameters.
+            campaign__in = query_dict['campaign__in'].split(',')
+            return campaign__in
+        except KeyError:
+            pass
+
+        try:
+            campaign_start = query_dict['campaign_start']
+        except KeyError:
+            campaign_start = '2001-01-01'
+
+        try:
+            campaign_end = query_dict['campaign_end']
+        except KeyError:
+            campaign_end = '2900-01-01'
+
+        cs = Campaign.objects.filter(
+            start_date__gte = campaign_start,\
+            start_date__lte = campaign_end,\
+        )
+
+        campaign__in = [c.id for c in cs]
+
+        return campaign__in
