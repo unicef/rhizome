6e568113ed6003e96d6006988ead9cea099e8b94
W. Evan Sheehan <wallace.sheehan@gmail.com>
2014-12-11 15:51:06 -0500

    Add a campaign dropdown to the management dashboard.
     - Add a second dropdown to the management dashboard for displaying
       campaigns
     - Fetch campaigns for the selected office
     - Include start and end dates in the requests for data

--- ui/js/dashboard/management.js
+++ ui/js/dashboard/management.js
@@ -23,8 +23,6 @@ var ratio     = require('../data/transform/ratio');
 var sort      = require('../data/transform/sort');
 var variables = require('../data/transform/variables');
 
-// FIXME: Hard-coded mapping from office ID to region ID for countries because
-// region_type currently doesn't distinguish states and countries.
 var OFFICE = {
 	1: 23,   // Nigeria
 	2: 4404, // Afghanistan
@@ -96,6 +94,7 @@ module.exports = {
 	data: function () {
 		return {
 			offices    : [],
+			campaigns  : [],
 			region     : null,
 			start      : new Date(),
 			cases      : {
@@ -213,8 +212,12 @@ module.exports = {
 		this.polio.forEach(rangeFactory);
 		this.resources.forEach(rangeFactory);
 
-		this.$on('selection-changed', function (data) {
-			self.region = OFFICE[data.selected.value];
+		this.$on('selection-changed', function () {
+			function isSelected(d) {
+				return d.selected;
+			}
+
+			self.region = OFFICE[self.offices.filter(isSelected)[0].value];
 			self.loadData();
 		});
 
@@ -230,7 +233,23 @@ module.exports = {
 			offices[0].selected = true;
 			self.region = OFFICE[offices[0].value];
 			self.offices = offices;
-			self.loadData();
+
+			api.campaign({ office: offices[0].value }).done(function (data) {
+				self.$set('campaigns', data.objects.map(function (o) {
+					var startDate = moment(o.start_date, 'YYYY-MM-DD');
+
+					return {
+						title   : startDate.format('MMM YYYY'),
+						value   : o.start_date,
+						sortVal : startDate.format('YYYYMMDD'),
+						selected: false
+					};
+				}));
+
+				self.campaigns[0].selected = true;
+
+				self.loadData();
+			});
 		});
 	},
 
@@ -271,13 +290,19 @@ module.exports = {
 			}
 
 			var self  = this;
-			var start = (this.start ? moment(this.start) : moment()).subtract(2, 'years').format('YYYY-MM-DD');
+			var start = moment(this.campaigns.filter(function (d) {
+					return d.selected;
+				})[0].value, 'YYYY-MM-DD');
+
 
 			// Query parameters shared by all queries
 			var q = {
 				parent_region : self.region,
-				campaign_start: start
+				campaign_start: start.clone().subtract(2, 'years').format('YYYY-MM-DD'),
+				campaign_end  : start.format('YYYY-MM-DD')
 			};
+
+			console.log(q.campaign_start);
 
 			// Polio Cases YTD
 			indicators([69, 70, 159, 160, 161, 162], q)
@@ -354,9 +379,9 @@ module.exports = {
 					}];
 				}).done(set('microplans'));
 
-			q.campaign_start = (self.start ?
-				moment(self.start) :
-				moment()).subtract(4, 'months').format('YYYY-MM-DD');
+			q.campaign_start = start.clone().subtract(4, 'months').format('YYYY-MM-DD');
+
+			console.log(q.campaign_start);
 
 			fetchBullets('capacity', q);
 			fetchBullets('supply', q);
