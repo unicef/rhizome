395b2e35b848249faf61fdfcde7ac375bbd57182
John Dingee <dingee@seedscientific.com>
2015-01-05 16:40:17 -0500

    add odk ingest script

---
+++ source_data/ingest_odk.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# find settings... script wont fail if file doesnt exists so try dev than prod
+source /Users/johndingee_seed/code/UF04/polio/source_data/dev_odk_settings.py
+source /var/www/clients.seedscientific.com/uf/UF04/polio/source_data
+
+java -jar $JAR_FILE \
+--form_id $FORM_ID \
+--export_filename $FORM_ID + '.csv' \
+--aggregate_url $AGGREGATE_URL \
+--storage_directory $STORAGE_DIRECTORY \
+--export_directory $EXPORT_DIRECTORY \
+--odk_username $USERNAME \
+--odk_password $PASSWORD \
+--overwrite_csv_export \
+--exclude_media_export \
