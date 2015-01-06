923dac992c4001a86d0b5fb21dd9f5453749db84
John Dingee <dingee@seedscientific.com>
2015-01-06 09:54:39 -0500

    prepping ingest_odk shell script for prod

--- source_data/ingest_odk.sh
+++ source_data/ingest_odk.sh
@@ -4,11 +4,9 @@
 source /Users/johndingee_seed/code/UF04/polio/source_data/dev_odk_settings.py
 source /var/www/clients.seedscientific.com/uf/UF04/polio/source_data/prod_odk_settings.py
 
+## STARTING JAR FILE PROCESS ##
 wget -O/dev/null http://localhost:8000/api/v1/etl?task=start_odk_jar&username=$POLIO_USERNAME&password=$POLIO_PASSWORD
-
 pid[0]=$!
-trap INT " kill ${pid[0]}; exit 1"
-wait
 
 
 java -jar $JAR_FILE \
@@ -22,10 +20,21 @@ java -jar $JAR_FILE \
 --overwrite_csv_export \
 --exclude_media_export \
 
-
+## DONE WITH THE JAR FILE PROCESS ##
 wget -O/dev/null http://localhost:8000/api/v1/etl?task=finish_odk_jar&username=$POLIO_USERNAME&password=$POLIO_PASSWORD
+pid[1]=$!
 
-## this peice of code kills the wget process we just ran ( same as cntr-c )
-pid[0]=$!
-trap INT " kill ${pid[0]}; exit 1"
+
+# ## populate data into vcm summary table ##
+# wget -O/dev/null http://localhost:8000/api/v1/etl?task=odk_refresh_vcm_summary_work_table&username=$POLIO_USERNAME&password=$POLIO_PASSWORD
+# pid[2]=$!
+#
+# ## move data from vcm summary table into source datapoints ##
+# wget -O/dev/null http://localhost:8000/api/v1/etl?task=odk_vcm_summary_to_source_datapoints&username=$POLIO_USERNAME&password=$POLIO_PASSWORD
+# pid[3]=$!
+
+## kill all the wget spids
+# trap INT " kill ${pid[0]} ${pid[1]} ${pid[2]} ${pid[3]}; exit 1"
+trap INT " kill ${pid[0]} ${pid[1]}; exit 1"
+
 wait
