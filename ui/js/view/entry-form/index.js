4d7910ccfb093a8cc74497a19d7c9e2d9cb7cb6c
Edward Lee <lee@seedscientific.com>
2015-01-12 15:33:10 -0500

    initial modifications to the data entry form

--- ui/js/view/entry-form/index.js
+++ ui/js/view/entry-form/index.js
@@ -14,6 +14,10 @@ module.exports = {
 		return {
 			regions: [],
 			indicators: [],
+			indicatorSet: [
+				{ 'id': 5 },
+				{ 'id': 51 }
+			],
 			pagination: {
 				the_limit: 20,
 				the_offset: 0,
@@ -25,10 +29,16 @@ module.exports = {
 				rows: []
 			},
 			campaign: {
-				start: '',
-				end: ''
+				start: '2013-06-01',
+				end: '2013-06-30'
 			}
 		};
+	},
+
+	ready: function() {
+
+		this.load();
+
 	},
 
 	attached: function () {
@@ -48,18 +58,18 @@ module.exports = {
 			self.regions = items;
 		});
 
-		this._indicators = new Dropdown({
-			el     : '#indicators',
-			source : api.indicators,
-			mapping: {
-				'short_name': 'title',
-				'id'        : 'value'
-			}
-		});
+		// this._indicators = new Dropdown({
+		// 	el     : '#indicators',
+		// 	source : api.indicators,
+		// 	mapping: {
+		// 		'short_name': 'title',
+		// 		'id'        : 'value'
+		// 	}
+		// });
 
-		this._indicators.$on('dropdown-value-changed', function (items) {
-			self.indicators = items;
-		});
+		// this._indicators.$on('dropdown-value-changed', function (items) {
+		// 	self.indicators = items;
+		// });
 
 		this.$on('page-changed', function (data) {
 			this.refresh(data);
@@ -69,12 +79,28 @@ module.exports = {
 	computed: {
 
 		hasSelection: function () {
-			return this.regions.length > 0 && this.indicators.length > 0;
+			return this.regions.length > 0;
+			// return this.regions.length > 0 && this.indicators.length > 0;
 		}
 
 	},
 
 	methods: {
+
+		load: function() {
+			var self = this;
+
+			// load all indicators metadata
+			api.indicators().done(function(data) {
+				self.$data.indicators = {};
+				if (data.objects) {
+					data.objects.forEach(function(d) {
+						self.$data.indicators[d.id] = d;
+					});
+				}
+			});
+
+		},
 
 		refresh: function (pagination) {
 			if (!this.hasSelection) {
@@ -85,17 +111,22 @@ module.exports = {
 
 			var regionNames = _.indexBy(this.regions, 'value');
 			var regions     = _.map(this.regions, 'value');
-			var options     = { indicator__in : [] };
-			var columns     = [{
-					prop: 'region',
-					display: 'Region',
-					format: function (v) {
-						return regionNames[v].title;
-					}
-				}, {
-					prop: 'campaign',
-					display: 'Campaign'
-				}];
+
+			var options     = { 
+				indicator__in : [],
+				region__in: []
+			};
+
+			// var columns     = [{
+			// 		prop: 'region',
+			// 		display: 'Region',
+			// 		format: function (v) {
+			// 			return regionNames[v].title;
+			// 		}
+			// 	}, {
+			// 		prop: 'campaign',
+			// 		display: 'Campaign'
+			// 	}];
 
 			if (pagination) {
 				// Prepend "the_" to the pagination options (typically limit and offset)
@@ -106,9 +137,15 @@ module.exports = {
 				});
 			}
 
+			// add regions to request
 			if (regions.length > 0) {
 				options.region__in = regions;
+				// sort region order
+				options.region__in = options.region__in.sort(function(a,b) {
+					return regionNames[a].title > regionNames[b].title ? 1 : -1;
+				});
 			}
+			// options.region__in = [12929, 12939, 12942];
 
 			if (this.campaign.start) {
 				options.campaign_start = this.campaign.start;
@@ -118,17 +155,28 @@ module.exports = {
 				options.campaign_end = this.campaign.end;
 			}
 
-			this.indicators.forEach(function (indicator) {
-				options.indicator__in.push(indicator.value);
+			// add indicators to request
+			this.indicatorSet.forEach(function (row) {
+				if (row.id) {
+					options.indicator__in.push(row.id);
+				}
+			});
+
+			// define columns
+			var columns = [
+				{ prop: 'indicator', display: 'Indicator' }
+			];
+			// add region names as columns
+			options.region__in.forEach(function(region_id) {
 				columns.push({
-					prop   : indicator.value,
-					display: indicator.title,
+					prop: region_id,
+					display: regionNames[region_id].title,
 					classes: 'numeric',
-					format : function (v) {
+					format: function (v) {
 						return (isNaN(v) || _.isNull(v)) ? '' : d3.format('n')(v);
 					}
 				});
-			});
+			});		
 
 			_.defaults(options, this.pagination);
 
@@ -147,16 +195,28 @@ module.exports = {
 					return;
 				}
 
-				var datapoints = data.objects.map(function (v) {
-					var d = _.pick(v, 'region');
+				console.log(data);
 
-					d.campaign = moment(v.campaign.start_date).format('MMM YYYY');
-
-					v.indicators.forEach(function (ind) {
-						d[ind.indicator] = ind.value;
+				// pivot data so that each row contains all region datapoints for one indicator
+				// TO DO: this may be broken by pagination (?) and so would need to be grouped differently on the back end
+				var byIndicator = {};
+				data.objects.forEach(function (d) {
+					d.indicators.forEach(function (ind) {
+						if (!byIndicator[ind.indicator]) { byIndicator[ind.indicator] = {}; }
+						byIndicator[ind.indicator][d.region] = ind;						
 					});
+				});
 
-					return d;
+				// assemble data points into rows for table
+				var datapoints = [];
+				options.indicator__in.forEach(function(ind) {
+					var row = {
+						'indicator': self.$data.indicators[ind] ? self.$data.indicators[ind].name : 'Missing Data for Indicator'+ind
+					};
+					options.region__in.forEach(function(reg) {
+						row[reg] = byIndicator[ind] && byIndicator[ind][reg] ? byIndicator[ind][reg].value : null;
+					});
+					datapoints.push(row);
 				});
 
 				self.table.rows = datapoints;
@@ -164,32 +224,32 @@ module.exports = {
 		},
 
 		download: function () {
-			if (!this.hasSelection) {
-				return;
-			}
+			// if (!this.hasSelection) {
+			// 	return;
+			// }
 
-			this.downloading = true;
+			// this.downloading = true;
 
-			var indicators   = _.map(this.indicators, 'value');
-			var regions      = _.map(this.regions, 'value');
-			var query        = {
-				// FIXME: Hack to get around no way of setting no limit for the 12/9 demo.
-				'the_limit'  : 10000000,
-				'format'     : 'csv',
-				'uri_display': 'slug'
-			};
+			// var indicators   = _.map(this.indicators, 'value');
+			// var regions      = _.map(this.regions, 'value');
+			// var query        = {
+			// 	// FIXME: Hack to get around no way of setting no limit for the 12/9 demo.
+			// 	'the_limit'  : 10000000,
+			// 	'format'     : 'csv',
+			// 	'uri_display': 'slug'
+			// };
 
-			if (indicators.length < 1) {
-				this.$data.src = '';
-				return;
-			}
+			// if (indicators.length < 1) {
+			// 	this.$data.src = '';
+			// 	return;
+			// }
 
-			query.indicator__in = indicators;
-			if (regions.length > 0) {
-				query.region__in = regions;
-			}
+			// query.indicator__in = indicators;
+			// if (regions.length > 0) {
+			// 	query.region__in = regions;
+			// }
 
-			this.$set('src', api.datapoints.toString(query));
+			// this.$set('src', api.datapoints.toString(query));
 		},
 
 		previous: function () {
