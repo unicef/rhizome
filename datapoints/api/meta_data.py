e683103c706129f4d96f3dba74710f68ed9b1a81
John Dingee <dingee@seedscientific.com>
2015-01-15 15:40:24 -0500

    trying to return geo json to api

--- datapoints/api/meta_data.py
+++ datapoints/api/meta_data.py
@@ -38,22 +38,86 @@ class RegionResource(ModelResource):
         bundle.data.pop("resource_uri",None)# = bundle.obj.region.id
         return bundle
 
-class RegionPolygonResource(BaseApiResource):
+class GeoJsonResult(object):
+    '''
+    This is the same as a row in the CSV export in which one row has a distinct
+    region / campaign combination, and the remaing columns represent the
+    indicators requested.  Indicators are a list of IndicatorObjects.
+    '''
+    geo_json = dict()
 
-    region_id = fields.ForeignKey(RegionResource,'region')
+class RegionPolygonResource(Resource):
+
+    # region_id = fields.ForeignKey(RegionResource,'region')
+    geo_json = fields.DictField()
 
     class Meta(BaseApiResource.Meta):
-        queryset = RegionPolygon.objects.all()
+        # queryset = RegionPolygon.objects.all()
+        object_class = GeoJsonResult
         resource_name = 'polygon'
         filtering = {
             "region_id": ALL,
         }
 
-    def dehydrate(self, bundle):
+    # def obj_get(self):
+    #     # get one object from data source
+    #     pk = int(kwargs['pk'])
+    #     try:
+    #         return data[pk]
+    #     except KeyError:
+    #         raise NotFound("Object not found")
 
-        bundle.data['region_id'] = bundle.obj.region.id
 
-        return bundle
+    def obj_get_list(self,bundle,**kwargs):
+        '''
+        Outer method for get_object_list... this calls get_object_list and
+        could be a point at which additional build_agg_rc_dfing may be applied
+        '''
+
+        return self.get_object_list(bundle.request)
+
+    def get_object_list(self,request):
+
+        print 'wuddup\n' * 10
+        # results = RegionPolygon.objects.all()
+        # region_values_list = RegionPolygon.objects.all().values()
+        # pp.pprint(region_values_list)
+
+        # { "type": "FeatureCollection",
+        # "features": [
+        features = []
+
+        f_1 = { "type": "Feature",
+             "geometry": {
+               "type": "Polygon",
+               "coordinates": [
+                 [ [100.0, 0.0], [101.0, 0.0], [101.0, 1.0],
+                   [100.0, 1.0], [100.0, 0.0] ]
+                 ]
+             },
+             "properties": { "region_id": 211 }
+        }
+        f_2 = { "type": "Feature",
+             "geometry": {
+               "type": "Polygon",
+               "coordinates": [
+                 [ [100.0, 0.0], [101.0, 0.0], [101.0, 1.0],
+                   [100.0, 1.0], [100.0, 0.0] ]
+                 ]
+             },
+             "properties": { "region_id": 122 }
+        }
+
+        f1_obj = GeoJsonResult()
+        f1_obj.geo_json = f_1
+
+        f2_obj = GeoJsonResult()
+        f2_obj.geo_json = f_2
+
+        features.append(f1_obj)
+        features.append(f2_obj)
+
+        return features
 
 
 class IndicatorResource(BaseApiResource):
