235d160ff997ca6e20f4d491be2c812e98ab7c9a
John Dingee <dingeej@gmail.com>
2015-01-19 21:38:35 -0500

    uncomment top part of db_refresh script

--- prod_db_to_local.sh
+++ prod_db_to_local.sh
@@ -7,29 +7,29 @@ DB=polio
 HOST=50.57.77.252
 USER=djangoapp
 
-# # FIXME: It would be nice to have command-line switches for skipping back-up and
-# # skipping the sync
-#
-# echo Backing up $DB database to $BACKUPDIR$DATE-$DB.sql.tgz ...
-#
-# if [ ! -d $BACKUPDIR ]; then
-#   mkdir $BACKUPDIR
-# fi
-#
-# pg_dump --verbose --format=t -f "$BACKUPDIR$DATE.sql.tar" $DB
-#
-# echo done.
-#
-# # FIXME: It would be better to prompt for the password upfront, not midway
-# # through syncing
-# echo
-# echo Downloading data from $HOST:$DB...
-# pg_dump --verbose -C -h $HOST -U $USER -f db.sql $DB
-# echo done.
-#
-# echo
-# echo Deleting data from $DB database...
-#
+# FIXME: It would be nice to have command-line switches for skipping back-up and
+# skipping the sync
+
+echo Backing up $DB database to $BACKUPDIR$DATE-$DB.sql.tgz ...
+
+if [ ! -d $BACKUPDIR ]; then
+  mkdir $BACKUPDIR
+fi
+
+pg_dump --verbose --format=t -f "$BACKUPDIR$DATE.sql.tar" $DB
+
+echo done.
+
+# FIXME: It would be better to prompt for the password upfront, not midway
+# through syncing
+echo
+echo Downloading data from $HOST:$DB...
+pg_dump --verbose -C -h $HOST -U $USER -f db.sql $DB
+echo done.
+
+echo
+echo Deleting data from $DB database...
+
 # FIXME: Should exit with an error status if we fail to drop the database
 psql -c"select pg_terminate_backend(pid)
    from pg_stat_activity
