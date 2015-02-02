fe5c7045a50ea234bca1bc0362e266a765741889
John Dingee <dingee@seedscientific.com>
2015-02-02 00:18:51 -0500

    create dataframe for region/campaign tuples

--- datapoints/cache_tasks/pivot_datapoint.py
+++ datapoints/cache_tasks/pivot_datapoint.py
@@ -1,3 +1,4 @@
+from pprint import pprint
 from pandas import DataFrame
 
 from datapoints.models import DataPoint
@@ -5,12 +6,26 @@ from datapoints.models import DataPoint
 
 def full_cache_refresh():
 
-    indicators = set(list(DataPoint.objects.all()\
+    indicator_ids = set(list(DataPoint.objects.all()\
         .values_list('indicator_id',flat=True)))
 
 
-    indicator_df = DataFrame(columns = indicators)
+    indicator_df = DataFrame(columns = indicator_ids)
 
-    print indicator_df
+    distict_region_campaign_list = DataPoint.objects.raw("\
+        SELECT \
+             max(id) as id \
+             ,region_id \
+            ,campaign_id \
+        FROM datapoint \
+        GROUP BY region_id,campaign_id")
 
-    print indicators
+
+    rc_tuple_list = []
+    for rc in distict_region_campaign_list:
+        # print r
+        r_c_tuple = (rc.region_id,rc.campaign_id)
+        rc_tuple_list.append(r_c_tuple)
+
+    rc_df = DataFrame(rc_tuple_list,columns=['region_id','campaign_id'])
+    pprint(rc_df)
