cf4ba85659d725e82e3e803d402739d0fa1e7295
W. Evan Sheehan <wallace.sheehan@gmail.com>
2015-02-04 14:00:12 -0500

    Set proper indicators for the NCO dashboard
     - Set the correct indicator IDs for the NCO dashboard charts so that once
       data is in the system, the dashboard will display correctly
     - Resize some of the columns to accommodate the scatter plots better
     - Set the aspect on the scatter plots to be wider so that they don't take as
       much vertical space

--- ui/js/dashboard/nco.js
+++ ui/js/dashboard/nco.js
@@ -10,27 +10,27 @@ module.exports = {
 
 			overview: [{
 				title     : 'Influencer',
-				indicators: [164,165,166,167],
+				indicators: [287,288,289,290,291,292,293,294],
 				chart     : 'chart-bar'
 			}, {
 				title     : 'Information Source',
-				indicators: [164,165,166,167],
+				indicators: [307,308,309,310,311,312,313,314,315,316,317],
 				chart     : 'chart-bar'
 			}, {
 				title     : 'Reasons for Missed',
-				indicators: [164,165,166,167],
+				indicators: [318,319,320,321,322],
 				chart     : 'chart-bar'
 			}, {
 				title     : 'Reasons for Absence',
-				indicators: [164,165,166,167],
+				indicators: [323,324,325,326,327],
 				chart     : 'chart-bar'
 			}, {
 				title     : 'Reasons for NC',
-				indicators: [164,165,166,167],
+				indicators: [328,329,330,331,332,333,334],
 				chart     : 'chart-bar'
 			}, {
 				title     : 'NC Resolved by',
-				indicators: [164,165,166,167],
+				indicators: [345,346,347,348],
 				chart     : 'chart-bar'
 			}]
 		};
