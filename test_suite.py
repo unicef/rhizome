71c3253e40c576bf489b956988d81f8b5968e871
John Dingee <dingee@seedscientific.com>
2015-02-09 12:28:15 -0500

    setting up my test suite

--- test_suite.py
+++ test_suite.py
@@ -1,14 +1,12 @@
 import unittest
-from unittest import TestResult
 
-from datapoints.tests.test_api_filters import CampaignDateFilterTestCase
-from datapoints.tests.test_agg_api import CalcPctSoloRegionSoloCampaign
-from datapoints.tests.test_agg_api import CalcPctParentRegionSoloCampaign
+# from datapoints.tests.test_api_filters import CampaignDateFilterTestCase
+# from datapoints.tests.test_agg_api import CalcPctSoloRegionSoloCampaign
+# from datapoints.tests.test_agg_api import CalcPctParentRegionSoloCampaign
 # from source_data.tests.test_odk import OdkTestCase
-from source_data.tests.test_master_refresh import NewDPTestCase
+# from source_data.tests.test_master_refresh import NewDPTestCase
 
 # python manage.py test test_suite.run_all --settings=polio.settings_test
-
 
 
 def run_all():
@@ -17,50 +15,52 @@ def run_all():
     python manage.py test test_suite.run_all --settings=polio.settings_test
 
     '''
-    result = TestResult()
-    my_suite = build_suite()
-    my_suite.run(result)
-
-    print 'WHAT FAILED\n' * 10
+    result = unittest.TestResult()
+    # my_suite = build_suite()
+    suite = unittest.TestSuite()
+    suite.run(result)
 
     print result.failures
-    return my_suite
+    return suite
 
 
+# def build_suite():
+#     """
+#     Gather all the tests from this module in a test suite.
+#     """
+#     test_suite = unittest.TestSuite()
+#
+#     ###############
+#     ## BASIC API ##
+#     ###############
+#
+#     ####################
+#     ## CUSTOM FILTERS ##
+#     ####################
+#     # test_suite.addTest(unittest.makeSuite(CampaignDateFilterTestCase))
+#
+#
+#     #######################
+#     ## AGGREGATE QUERIES ##
+#     #######################
+#     # test_suite.addTest(unittest.makeSuite(CalcPctSoloRegionSoloCampaign))
+#     # test_suite.addTest(unittest.makeSuite(CalcPctParentRegionSoloCampaign))
+#
+#     #########
+#     ## ODK ##
+#     #########
+#     # OdkTestCase
+#
+#     ###########################
+#     ## REFRESH MASTER METHOD ##
+#     ###########################
+#     # test_suite.addTest(unittest.makeSuite(NewDPTestCase))
+#
+#
+#     print '=\n' * 100
+#
+#     return test_suite
 
-def build_suite():
-    """
-    Gather all the tests from this module in a test suite.
-    """
-    test_suite = unittest.TestSuite()
 
-    ###############
-    ## BASIC API ##
-    ###############
-
-    ####################
-    ## CUSTOM FILTERS ##
-    ####################
-    # test_suite.addTest(unittest.makeSuite(CampaignDateFilterTestCase))
-
-
-    #######################
-    ## AGGREGATE QUERIES ##
-    #######################
-    # test_suite.addTest(unittest.makeSuite(CalcPctSoloRegionSoloCampaign))
-    # test_suite.addTest(unittest.makeSuite(CalcPctParentRegionSoloCampaign))
-
-    #########
-    ## ODK ##
-    #########
-    # OdkTestCase
-
-    ###########################
-    ## REFRESH MASTER METHOD ##
-    ###########################
-    # test_suite.addTest(unittest.makeSuite(NewDPTestCase))
-
-
-    print '=\n' * 100
-
-    return test_suite
+if __name__ == "__main__":
+    run_all()
