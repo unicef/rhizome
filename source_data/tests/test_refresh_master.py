d5c375b19d28cf53483f7b627ee8be3796afe9b2
John Dingee <dingee@seedscientific.com>
2015-02-10 16:19:55 -0500

    create sourcedatapoint object list

--- source_data/tests/test_refresh_master.py
+++ source_data/tests/test_refresh_master.py
@@ -2,7 +2,7 @@ from django.test import TestCase
 from django.contrib.auth.models import User
 from pandas import read_csv
 
-from source_data.models import Source, Document
+from source_data.models import Source, Document, SourceDataPoint
 
 class RefreshMasterTestCase(TestCase):
 
@@ -25,14 +25,35 @@ class RefreshMasterTestCase(TestCase):
 
         self.source_datapoints = self.build_source_datapoint_list()
 
+        print self.source_datapoints
+
     def build_source_datapoint_list(self):
 
         sdp_df = read_csv('datapoints/tests/_data/source_datapoint_msd_chd.csv')
 
         print sdp_df[:5]
 
-        # for sdp in sdp_df.iterrows():
-        #     print sdp
+        sdp_list = []
+
+        for sdp in sdp_df.iterrows():
+
+            sdp_ix, sdp_data = sdp[0],sdp[1]
+
+            sdp_dict = {
+                'region_string': sdp_data['region_string'],
+                'campaign_string': sdp_data['campaign_string'],
+                'indicator_string': sdp_data['indicator_string'],
+                'cell_value' : sdp_data['cell_value'],
+                'row_number' : sdp_data['row_number'],
+                'source_guid': sdp_data['guid'],
+                'source_id' : self.source.id,
+                'document_id': self.document.id,
+            }
+
+            sdp_obj = SourceDataPoint(**sdp_dict)
+            sdp_list.append(sdp_obj)
+
+        return sdp_list
 
     # def test_refresh_master(self, indicator_id):
     def test_refresh_master(self):
