cec01ea8da211c708756d061a164d5cf44a0ecf9
John Dingee <dingee@seedscientific.com>
2015-02-10 16:45:30 -0500

    creating indicators campaigns and regions to prep for refresh master

--- source_data/tests/test_refresh_master.py
+++ source_data/tests/test_refresh_master.py
@@ -2,7 +2,11 @@ from django.test import TestCase
 from django.contrib.auth.models import User
 from pandas import read_csv
 
-from source_data.models import Source, Document, SourceDataPoint
+from datapoints.models import Indicator, Campaign, CampaignType,\
+    Region, DataPoint, Office, RegionType
+from source_data.models import Source, Document, SourceDataPoint, SourceRegion,\
+    SourceCampaign, SourceIndicator
+
 
 class RefreshMasterTestCase(TestCase):
 
@@ -12,26 +16,80 @@ class RefreshMasterTestCase(TestCase):
 
     def set_up(self):
 
+        self.region_1_name = 'Pakistan - Balochistan'
+        self.region_2_name ='Pakistan - Lakki Marwat'
+        self.indicator_string = 'Number of all missed children'
+        self.campaign_string = 'Pakistan July 2014'
+
         self.source = Source.objects.create(
             source_name = 'test',
             source_description = 'test')
 
-        self.user = User.objects.create(username='test_user')
+        self.user = User.objects.create(username = 'test_user')
 
         self.document = Document.objects.create(
             doc_text = 'test_text',
             created_by_id  = self.user.id,
             guid = 'refresh_master_test')
 
+        # self.source_indicator = SourceIndicator.objects.create(
+        #     indicator_string = self.indicator_string)
+
+
+        # self.source_campaign= SourceCampaign.objects.create(
+        #     campaign_string = self.campaign_string,
+        #     document_id = self.document.id,
+        #     office_id = self.office.id)
+
+        # self.source_region_1 = SourceRegion.objects.create(
+        #     region_string = self.region_1_name,
+        #     document_id = self.document.id)
+        #
+        # self.source_region_2 = SourceRegion.objects.create(
+        #     region_string = self.region_2_name,
+        #     document_id = self.document.id)
+
+
+        self.indicator = Indicator.objects.create(
+                name = self.indicator_string,
+                source_id = self.source.id)
+
+        self.office = Office.objects.create(name = 'Pakistan')
+
+        self.campaign_type = CampaignType.objects.create(name='global')
+
+
+        ## need a source_campaign_id column in here ##
+        self.campaign = Campaign.objects.create(
+            office_id = self.office.id,
+            start_date = '2014-07-01',
+            end_date = '2014-07-01',
+            campaign_type_id = self.campaign_type.id)
+
+        self.region_type = RegionType.objects.create(name='district')
+
+        self.region_1 = Region.objects.create(
+            name = self.region_1_name,
+            region_type_id = self.region_type.id,
+            office_id = self.office.id,
+            source_id = self.source.id,
+            # source_region_id = self.source_region_1.id,
+            region_code = self.source_region_1.region_string.replace(' ','-'))
+
+        self.region_1 = Region.objects.create(
+            name = self.region_2_name,
+            region_type_id = self.region_type.id,
+            office_id = self.office.id,
+            source_id = self.source.id,
+            # source_region_id = self.source_region_2.id,
+            region_code = self.source_region_2.region_string.replace(' ','-'))
+
         self.source_datapoints = self.build_source_datapoint_list()
 
-        print self.source_datapoints
 
     def build_source_datapoint_list(self):
 
         sdp_df = read_csv('datapoints/tests/_data/source_datapoint_msd_chd.csv')
-
-        print sdp_df[:5]
 
         sdp_list = []
 
