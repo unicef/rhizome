cbedc13dd6756b1eeb9991682747cfcaf8871927
John Dingee <dingee@seedscientific.com>
2015-02-11 11:45:38 -0500

    cleanup source data model test

--- datapoints/tests/test_models.py
+++ datapoints/tests/test_models.py
@@ -11,33 +11,25 @@ class MasterModelTestCase(TestCase):
 
     def set_up(self):
 
-        set_up_dict = {}
-
         self.source = Source.objects.create(
             source_name = 'test',
             source_description = 'test')
 
-        ##
+        self.user = User.objects.create(
+            username='john')
 
-        set_up_dict['user_id'] = User.objects.create(
-            username='john').id
-
-        ##
-
-        set_up_dict['document_id'] = Document.objects.create(
+        self.document = Document.objects.create(
             doc_text = 'test',
-            created_by_id = set_up_dict['user_id'],
-            guid = 'test').id
-
-
-        return set_up_dict
+            created_by_id = self.user.id,
+            guid = 'test')
 
 
 class IndicatorTest(MasterModelTestCase):
 
     def test_datapoint_indicator_creation(self):
 
-        set_up_dict = self.set_up()
+        self.set_up()
+
         dpi = Indicator.objects.create(
             name = 'test',
             description = 'test',
@@ -46,8 +38,8 @@ class IndicatorTest(MasterModelTestCase):
 
         self.assertTrue(isinstance,(dpi,Indicator))
         self.assertEqual(dpi.__unicode__(),dpi.name)
-        print '...Done Testing Indicator Model...'
 
+        print '...Done Testing Indicator Model...'
 
 
 class RegionTest(MasterModelTestCase):
@@ -58,22 +50,17 @@ class RegionTest(MasterModelTestCase):
             source_name = 'test',
             source_description = 'test')
 
-    def create_region_type(self):
-
-        region_type_id = RegionType.objects.create(name='test').id
-
-        return region_type_id
+        self.region_type_id = RegionType.objects.create(name='test').id
 
     def create_region(self, name = "test", office_id=1):
 
         self.set_up()
 
-        region_type_id = self.create_region_type()
         # source_region_id = self.create_source_region()
 
         region = Region.objects.create(name = name\
             ,office_id = office_id
-            ,region_type_id = region_type_id\
+            ,region_type_id = self.region_type_id\
             ,source_id = self.source.id)
 
         return region
@@ -88,21 +75,30 @@ class RegionTest(MasterModelTestCase):
 
 class DataPointTest(MasterModelTestCase):
 
-    def prep(self):
+    def set_up(self):
 
         self.status = ProcessStatus.objects.create(
             status_text = 'test',
             status_description = 'test')
 
-        self.set_up_dict = self.set_up()
+        self.source = Source.objects.create(
+            source_name = 'test',
+            source_description = 'test')
 
+        self.user = User.objects.create(
+            username='john')
+
+        self.document = Document.objects.create(
+            doc_text = 'test',
+            created_by_id = self.user.id,
+            guid = 'test')
 
     def create_source_datapoint(self):
 
-        self.prep()
+        self.set_up()
 
         sdp_id = SourceDataPoint.objects.create(
-            document_id = self.set_up_dict['document_id'],
+            document_id = self.document.id,
             row_number = 0,
             source_id =self.source.id,
             status_id = self.status.id).id
