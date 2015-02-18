310e7097c45ee6efcfe484f9fd95de711051e27c
John Dingee <dingee@seedscientific.com>
2015-02-18 12:46:39 -0500

    calc datapoint via end point

--- datapoints/views.py
+++ datapoints/views.py
@@ -306,7 +306,117 @@ def search(request):
 
 def calc_datapoint(request):
 
-    pass
+    curs = DataPoint.objects.raw("""
+        DROP TABLE IF EXISTS datapoint_with_computed;
+
+        CREATE TABLE datapoint_with_computed AS
+
+        SELECT
+        id
+        ,indicator_id
+        ,region_id
+        ,campaign_id
+        ,value
+        ,is_agg
+        ,CAST(0 as BOOLEAN) as is_calc
+        FROM agg_datapoint;
+
+        -- make ID column auto increment
+        DROP SEQUENCE IF EXISTS dwc_seq;
+        CREATE SEQUENCE dwc_seq;
+        ALTER TABLE datapoint_with_computed ALTER COLUMN id SET DEFAULT nextval('dwc_seq');
+        --ALTER TABLE datapoint_with_computed ALTER COLUMN id SET NOT NULL;
+        ALTER SEQUENCE dwc_seq OWNED BY datapoint_with_computed.id;
+
+        ----- PART / WHOLE ------
+        INSERT INTO datapoint_with_computed
+        (indicator_id,region_id,campaign_id,value,is_calc)
+
+        SELECT
+        part.indicator_id as master_indicator_id
+        ,d_part.region_id
+        ,d_part.campaign_id
+        ,d_part.value / NULLIF(d_whole.value,0) as value
+        ,CAST(1 as BOOLEAN) as is_calc
+        FROM(
+          SELECT max(id) as max_dp_id FROM datapoint_with_computed
+        ) x
+        INNER JOIN calculated_indicator_component part
+        ON 1=1
+        INNER JOIN calculated_indicator_component whole
+        ON part.indicator_id = whole.indicator_id
+        AND whole.calculation = 'WHOLE'
+        AND part.calculation = 'PART'
+        INNER JOIN agg_datapoint d_part
+        ON part.indicator_component_id = d_part.indicator_id
+        INNER JOIN agg_datapoint d_whole
+        ON whole.indicator_component_id = d_whole.indicator_id
+        AND d_part.campaign_id = d_whole.campaign_id
+        AND d_part.region_id = d_whole.region_id;
+
+
+        ----- SUM OF PARTS ------
+        INSERT INTO datapoint_with_computed
+        (indicator_id,region_id,campaign_id,value,is_calc)
+
+        SELECT
+        i_part.indicator_id
+        ,region_id
+        ,campaign_id
+        ,SUM(ad.value) as value
+        ,CAST(1 as BOOLEAN) as is_calc
+        FROM calculated_indicator_component i_part
+        INNER JOIN agg_datapoint ad
+        ON i_part.indicator_component_id = ad.indicator_id
+        WHERE i_part.calculation = 'PART_TO_BE_SUMMED'
+        GROUP BY i_part.indicator_id,region_id,campaign_id;
+
+        GRANT SELECT ON datapoint_with_computed TO djangoapp;
+
+        INSERT INTO datapoint_with_computed
+        (indicator_id,region_id,campaign_id,value,is_calc)
+        SELECT
+        x.indicator_id
+        ,x.region_id
+        ,x.campaign_id
+        ,x.calc_value
+        ,CAST(1 as BOOLEAN) as is_calc
+        FROM (
+          SELECT
+          part.to_calc_ind_id  as indicator_id
+          ,part.region_id
+          ,part.campaign_id
+          ,(whole.value - part.value) / NULLIF(whole.value,0) as calc_value
+          FROM (
+            SELECT d.value, d.region_id, d.campaign_id, d.indicator_id, cic.calculation, cic.indicator_id as to_calc_ind_id
+            FROM calculated_indicator_component cic
+            INNER JOIN datapoint d
+            ON cic.indicator_component_id = d.indicator_id
+            WHERE calculation = 'PART_OF_DIFFERENCE'
+          ) part
+          INNER JOIN (
+            SELECT d.value, d.region_id, d.campaign_id, d.indicator_id, cic.calculation, cic.indicator_id as to_calc_ind_id
+            FROM calculated_indicator_component cic
+            INNER JOIN datapoint d
+            ON cic.indicator_component_id = d.indicator_id
+            WHERE calculation = 'WHOLE_OF_DIFFERENCE'
+          ) whole
+          ON part.to_calc_ind_id = whole.to_calc_ind_id
+          AND part.region_id = whole.region_id
+          AND part.campaign_id = whole.campaign_id
+        )x
+        WHERE x.calc_value IS NOT NULL;
+
+        SELECT id FROM agg_datapoint LIMIT 1;
+
+    """)
+
+    for x in curs:
+        print x
+
+    return HttpResponseRedirect('/datapoints/regions/')
+
+
 
 def agg_datapoint(request):
 
