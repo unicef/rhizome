b3d2285bb533591039827e006b8b75d6471d36ed
W. Evan Sheehan <wallace.sheehan@gmail.com>
2015-02-24 12:18:00 -0500

    Manually generate tick values for x-axis
    - Implement custom tick generation in the xTicks property of the
      line component
    - Add a special case for formatting dates in the x-axis

--- ui/js/component/chart/line.js
+++ ui/js/component/chart/line.js
@@ -2,6 +2,7 @@
 
 var _         = require('lodash');
 var d3        = require('d3');
+var moment    = require('moment');
 
 var colors    = require('colors/coolgray');
 var lineChart = require('./renderer/line');
@@ -141,7 +142,23 @@ module.exports = {
 		},
 
 		xTicks: function () {
-			return this.xScale.ticks(d3.time.months, 3);
+			var months = [0];
+			var domain = this.xScale.domain();
+			var dt = moment(domain[0]).clone().startOf('month');
+			var end = moment(domain[1]);
+			var ticks = [];
+
+			while (dt.isBefore(end)) {
+				if (months.indexOf(dt.month()) >= 0) {
+					ticks.push(dt.clone().toDate());
+				}
+
+				dt.add(1, 'months');
+			}
+
+			ticks.push(dt.clone().toDate());
+
+			return ticks;
 		},
 
 		yFmt: function () {
@@ -209,9 +226,21 @@ module.exports = {
 				.data(this.labels)
 				.call(label().addClass('series').width(this.contentWidth).height(this.contentHeight));
 
+			var xFmt = this.xFmt;
+
 			var gx = svg.select('.x.axis')
 				.call(d3.svg.axis()
-					.tickFormat(this.xFmt)
+					.tickFormat(function (d) {
+						if (d instanceof Date) {
+							if (d.getMonth() === 0) {
+								return moment(d).format('MMM YYYY');
+							}
+
+							return moment(d).format('MMM');
+						}
+
+						return xFmt(d);
+					})
 					.tickValues(this.xTicks)
 					.scale(xScale)
 					.orient('bottom'));
