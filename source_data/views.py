4bdf55bfd073d83ec1fed7bbc273f1b911c97745
John Dingee <dingee@seedscientific.com>
2015-03-03 17:15:58 -0500

    add master datapoint counts to review page

--- source_data/views.py
+++ source_data/views.py
@@ -128,8 +128,6 @@ def document_review(request,document_id):
         .transpose().to_dict()
     region_breakdown =  [v for k,v in region_dict.iteritems()]
 
-    print region_breakdown
-
     return render_to_response(
         'upload/document_review.html',
         {'source_indicator_breakdown': ind_breakdown,
@@ -144,168 +142,202 @@ def populate_document_metadata(document_id):
 
     raw_qs = Document.objects.raw(
     '''
-    DROP TABLE IF EXISTS _doc_meta;
-    CREATE TABLE _doc_meta AS
+        DROP TABLE IF EXISTS _doc_data;
+        CREATE TABLE _doc_data AS
 
-    SELECT
-    	document_id
-    	,indicator_string
-    	,campaign_string
-    	,region_code
-    FROM source_datapoint
-    WHERE document_id = 974;
+        SELECT
+        	id as source_datapoint_id
+        	,document_id
+        	,indicator_string
+        	,campaign_string
+        	,region_code
+        FROM source_datapoint
+        WHERE document_id = 974;
 
-    DROP TABLE IF EXISTS _doc_meta_cnt;
-    CREATE TABLE _doc_meta_cnt AS
-    SELECT
-    	*
-    	,CAST(NULL AS INT) AS source_object_id
-    	,CAST(-1 AS INT) as master_object_id
-    	,CAST(-1 AS INT) as master_object_cnt
-    FROM (
-    	SELECT
-    		'source_indicator' as db_model
-    		,indicator_string as source_string
-    		,COUNT(1) AS source_object_cnt
-    	FROM _doc_meta
-    	GROUP BY indicator_string
+        DROP TABLE IF EXISTS _doc_meta_cnt;
+        CREATE TABLE _doc_meta_cnt AS
+        SELECT
+        	*
+        	,CAST(NULL AS INT) AS source_object_id
+        	,CAST(-1 AS INT) as master_object_id
+        	,CAST(-1 AS INT) as master_object_cnt
+        FROM (
+        	SELECT
+        		'source_indicator' as db_model
+        		,indicator_string as source_string
+        		,COUNT(1) AS source_object_cnt
+        	FROM _doc_data
+        	GROUP BY indicator_string
 
-    	UNION ALL
+        	UNION ALL
 
-    	SELECT
-    		'source_campaign' as db_model
-    		,campaign_string
-    		,COUNT(1) AS c
-    	FROM _doc_meta
-    	GROUP BY campaign_string
+        	SELECT
+        		'source_campaign' as db_model
+        		,campaign_string
+        		,COUNT(1) AS c
+        	FROM _doc_data
+        	GROUP BY campaign_string
 
-    	UNION ALL
+        	UNION ALL
 
-    	SELECT
-    		'source_region' as db_model
-    		,region_code
-    		,COUNT(1) AS c
-    	FROM _doc_meta
-    	GROUP BY region_code
-    )x
-    INNER JOIN (
-    	SELECT document_id
-    	FROM _doc_meta LIMIT 1
-    )y
-    ON 1=1;
+        	SELECT
+        		'source_region' as db_model
+        		,region_code
+        		,COUNT(1) AS c
+        	FROM _doc_data
+        	GROUP BY region_code
+        )x
+        INNER JOIN (
+        	SELECT document_id
+        	FROM _doc_meta LIMIT 1
+        )y
+        ON 1=1;
 
+        -----------------------------
+        -- insert source meta data --
+        -----------------------------
 
-    -----					-----
-    -- insert source meta data --
-    -----					------
-    ----------------
-    -- indicators --
-    ----------------
-    INSERT INTO source_indicator
-    (indicator_string, document_id,source_guid)
+        ----------------
+        -- indicators --
+        ----------------
+        INSERT INTO source_indicator
+        (indicator_string, document_id,source_guid)
 
-    SELECT dmc.source_string, dmc.document_id, dmc.source_string || '-' || dmc.document_id
-    FROM _doc_meta_cnt dmc
-    WHERE db_model = 'source_indicator'
-    AND NOT EXISTS (
-    	SELECT 1 FROM source_indicator si
-    	WHERE dmc.db_model = 'source_indicator'
-    	AND dmc.source_string = si.indicator_string
-    );
+        SELECT dmc.source_string, dmc.document_id, dmc.source_string || '-' || dmc.document_id
+        FROM _doc_meta_cnt dmc
+        WHERE db_model = 'source_indicator'
+        AND NOT EXISTS (
+        	SELECT 1 FROM source_indicator si
+        	WHERE dmc.db_model = 'source_indicator'
+        	AND dmc.source_string = si.indicator_string
+        );
 
-    UPDATE _doc_meta_cnt dmc
-    SET
-    	source_object_id = si.id
-    FROM source_indicator si
-    WHERE dmc.db_model = 'source_indicator'
-    AND dmc.source_string = si.indicator_string;
+        UPDATE _doc_meta_cnt dmc
+        SET
+        	source_object_id = si.id
+        FROM source_indicator si
+        WHERE dmc.db_model = 'source_indicator'
+        AND dmc.source_string = si.indicator_string;
 
-    -- MASTER REGION ID --
-    UPDATE _doc_meta_cnt dmc
-    SET
-    	master_object_id = im.master_indicator_id
-    FROM indicator_map im
-    WHERE dmc.db_model = 'source_indicator'
-    AND dmc.source_object_id = im.source_indicator_id;
-
-
-    -------------
-    -- REGIONS --
-    -------------
-
-    INSERT INTO source_region
-    (region_code,document_id,source_guid)
-
-    SELECT dmc.source_string, dmc.document_id, dmc.source_string || '-' || dmc.document_id
-    FROM _doc_meta_cnt dmc
-    WHERE db_model = 'source_region'
-    AND NOT EXISTS (
-    	SELECT 1 FROM source_region sr
-    	WHERE dmc.db_model = 'source_region'
-    	AND dmc.source_string = sr.region_code
-    );
-
-    UPDATE _doc_meta_cnt dmc
-    SET
-    	source_object_id = sr.id
-    FROM source_region sr
-    WHERE dmc.db_model = 'source_region'
-    AND dmc.source_string = sr.region_code;
+        -- MASTER REGION ID --
+        UPDATE _doc_meta_cnt dmc
+        SET
+        	master_object_id = im.master_indicator_id
+        FROM indicator_map im
+        WHERE dmc.db_model = 'source_indicator'
+        AND dmc.source_object_id = im.source_indicator_id;
 
 
-    -- MASTER REGION ID --
-    UPDATE _doc_meta_cnt dmc
-    SET
-    	master_object_id = rm.master_region_id
-    FROM region_map rm
-    WHERE dmc.db_model = 'source_region'
-    AND dmc.source_object_id = rm.source_region_id;
+        -------------
+        -- REGIONS --
+        -------------
 
-    -------------
-    -- CAMPAIGNS --
-    -------------
+        INSERT INTO source_region
+        (region_code,document_id,source_guid)
 
-    INSERT INTO source_campaign
-    (campaign_string,document_id,source_guid)
+        SELECT dmc.source_string, dmc.document_id, dmc.source_string || '-' || dmc.document_id
+        FROM _doc_meta_cnt dmc
+        WHERE db_model = 'source_region'
+        AND NOT EXISTS (
+        	SELECT 1 FROM source_region sr
+        	WHERE dmc.db_model = 'source_region'
+        	AND dmc.source_string = sr.region_code
+        );
 
-    SELECT dmc.source_string, dmc.document_id, dmc.source_string || '-' || dmc.document_id
-    FROM _doc_meta_cnt dmc
-    WHERE db_model = 'source_campaign'
-    AND NOT EXISTS (
-    	SELECT 1 FROM source_campaign sc
-    	WHERE dmc.db_model = 'source_campaign'
-    	AND dmc.source_string = sc.campaign_string
-    );
-
-    -- SOURCE CAMPAIGN ID --
-    UPDATE _doc_meta_cnt dmc
-    SET
-    	source_object_id = sc.id
-    FROM source_campaign sc
-    WHERE dmc.db_model = 'source_campaign'
-    AND dmc.source_string = sc.campaign_string;
-
-    -- MASTER CAMPAIGN ID --
-    UPDATE _doc_meta_cnt dmc
-    SET
-    	master_object_id = cm.master_campaign_id
-    FROM campaign_map cm
-    WHERE dmc.db_model = 'source_campaign'
-    AND dmc.source_object_id = cm.source_campaign_id;
+        UPDATE _doc_meta_cnt dmc
+        SET
+        	source_object_id = sr.id
+        FROM source_region sr
+        WHERE dmc.db_model = 'source_region'
+        AND dmc.source_string = sr.region_code;
 
 
-    --- RETURN TO RAW QUERYSET -----
-    -- DROP TABLE IF EXISTS _results;
-    -- CREATE TABLE _results AS
-    SELECT
-    	document_id as id
-    	,db_model
-    	,source_string
-    	,source_object_id
-    	,master_object_id
-    	,master_object_cnt
-    	,source_object_cnt
-    FROM _doc_meta_cnt;''',[document_id,document_id])
+        -- MASTER REGION ID --
+        UPDATE _doc_meta_cnt dmc
+        SET
+        	master_object_id = rm.master_region_id
+        FROM region_map rm
+        WHERE dmc.db_model = 'source_region'
+        AND dmc.source_object_id = rm.source_region_id;
+
+        -------------
+        -- CAMPAIGNS --
+        -------------
+
+        INSERT INTO source_campaign
+        (campaign_string,document_id,source_guid)
+
+        SELECT dmc.source_string, dmc.document_id, dmc.source_string || '-' || dmc.document_id
+        FROM _doc_meta_cnt dmc
+        WHERE db_model = 'source_campaign'
+        AND NOT EXISTS (
+        	SELECT 1 FROM source_campaign sc
+        	WHERE dmc.db_model = 'source_campaign'
+        	AND dmc.source_string = sc.campaign_string
+        );
+
+        -- SOURCE CAMPAIGN ID --
+        UPDATE _doc_meta_cnt dmc
+        SET
+        	source_object_id = sc.id
+        FROM source_campaign sc
+        WHERE dmc.db_model = 'source_campaign'
+        AND dmc.source_string = sc.campaign_string;
+
+        -- MASTER CAMPAIGN ID --
+        UPDATE _doc_meta_cnt dmc
+        SET
+        	master_object_id = cm.master_campaign_id
+        FROM campaign_map cm
+        WHERE dmc.db_model = 'source_campaign'
+        AND dmc.source_object_id = cm.source_campaign_id;
+
+        DROP TABLE IF EXISTS _synced_datapoints;
+        CREATE TEMP TABLE _synced_datapoints  as
+        SELECT
+        	dd.region_code
+        	,dd.indicator_string
+        	,dd.campaign_string
+        FROM _doc_data dd
+        INNER JOIN datapoint d
+        ON dd.source_datapoint_id = d.source_datapoint_id;
+
+
+        UPDATE _doc_meta_cnt dmc
+        SET master_object_cnt = x.cnt
+        FROM (
+        	SELECT 'source_indicator' as db_model,indicator_string as source_string ,COUNT(1) as cnt
+        	FROM _synced_datapoints
+        	GROUP BY indicator_string
+
+        	UNION ALL
+
+        	SELECT 'source_region' as db_model,region_code, COUNT(1) as cnt
+        	FROM _synced_datapoints
+        	GROUP BY region_code
+
+        	UNION ALL
+
+        	SELECT 'source_campaign' as db_model, campaign_string, COUNT(1) as cnt
+        	FROM _synced_datapoints
+        	GROUP BY campaign_string
+        )x
+        WHERE dmc.db_model = x.db_model
+        AND dmc.source_string = x.source_string;
+
+
+        --- RETURN TO RAW QUERYSET -----
+        SELECT
+        	document_id as id
+        	,db_model
+        	,source_string
+        	,source_object_id
+        	,master_object_id
+        	,master_object_cnt
+        	,source_object_cnt
+        FROM _doc_meta_cnt;
+
+    ''',[document_id,document_id])
 
     for row in raw_qs:
         row_dict = {
@@ -314,6 +346,7 @@ def populate_document_metadata(document_id):
             'source_string':row.source_string,
             'master_object_id':row.master_object_id,
             'source_object_count':row.source_object_cnt,
+            'master_object_count':row.master_object_cnt,
         }
 
         meta_breakdown.append(row_dict)
@@ -461,8 +494,6 @@ def refresh_master(request):
     t = EtlTask('refresh_master',job_guid)
 
     task_data = t.data
-
-    print task_data
 
     return render_to_response('map/master_refresh.html'
         ,{'task_data': task_data})
