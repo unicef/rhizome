4f78a0dd7e3f10ed5d4b4b50884b70ed99f9128a
John Dingee <dingee@seedscientific.com>
2015-03-04 15:24:59 -0500

    round to 3 decimal pts

--- datapoints/views.py
+++ datapoints/views.py
@@ -444,6 +444,9 @@ def calc_datapoint(request):
           AND num_whole.master_indicator_id = denom.master_indicator_id
          AND num_whole.campaign_id = denom.campaign_id;
 
+        UPDATE datapoint_with_computed
+        SET value = ROUND(value,3);
+
         SELECT id FROM datapoint_with_computed LIMIT 1;
     """)
 
