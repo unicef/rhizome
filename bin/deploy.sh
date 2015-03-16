96ae4c2efe08969a4b1b9013413a6c13672fc31c
John Dingee <dingee@seedscientific.com>
2015-03-16 16:54:44 -0400

    use prod settings in deploy.sh

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -2,6 +2,6 @@
 
 git pull origin development
 pip install -r requirements.txt
-python manage.py syncdb
-python manage.py migrate
+python manage.py syncdb --settings=polio.prod_settings
+python manage.py migrate --settings=polio.prod_settings
 bash bin/build_db.sh
