c8065dd207950f7c16f8764e8d9ae110a12ae541
John Dingee <dingee@seedscientific.com>
2015-03-16 15:25:21 -0400

    adding cache refresh class

--- datapoints/cache_tasks/cache_refresh.py
+++ datapoints/cache_tasks/cache_refresh.py
@@ -10,17 +10,48 @@ class CacheRefresh(object):
     def __init__(self,datapoint_id_list=None):
 
         if datapoint_id_list is None:
-            self.datapoint_id_list = self.get_datapoints_to_cache(self)
+            self.datapoint_id_list = self.get_datapoints_to_cache()
 
 
-        self.sdp_df = DataFrame(list(SourceDataPoint.objects\
-            .filter(document_id = self.document_id).values()))
+        self.rc_loop = self.get_rc_loop()
 
+        # self.sdp_df = DataFrame(list(SourceDataPoint.objects\
+        #     .filter(document_id = self.document_id).values()))
 
 
     def get_datapoints_to_cache(self):
+        '''
+        Called on __init__ to find the unprocessed datapoints that need to be
+        cached.  If the datapoint_id_list parameter is provided when the class
+        is instantiated, this method need not be called.
+        '''
 
         dps = DataPoint.objects.raw('''
             SELECT id from datapoint
             WHERE is_cached = 'f'
+            LIMIT 1000
         ''')
+
+        dp_ids = [row.id for row in dps]
+
+        return dp_ids
+
+    def get_rc_loop(self):
+        '''
+        Being as that the cache table the API uses to provide data to the site
+        is stored with a unique key of region_id, campaign_id we need this data
+        in order to appropriate loop through the cache process.
+        '''
+        rc_raw = DataPoint.objects.raw('''
+            SELECT
+                  MIN(id) as id
+                , region_id
+                , campaign_id
+            FROM datapoint
+            WHERE id = ANY (%s)
+            GROUP BY region_id, campaign_id
+        ''',[self.datapoint_id_list])
+
+        rc_list_of_tuples = [(rc.region_id,rc.campaign_id) for rc in rc_raw]
+
+        return rc_list_of_tuples
