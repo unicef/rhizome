64ed14c72c5f616da36786663e0951c7269e1681
Elliot Winard <winard@seedscientific.com>
2015-03-19 17:28:07 -0400

    progress on the fabfile to push to rackspace
    * add emacs & temporary files to gitighore
    * successfully build zip files via gulp on local machine, push &
      explode to app1
    (not yet tested from jenkins machine)

--- fabfile.py
+++ fabfile.py
@@ -3,12 +3,7 @@
 # depends on installation of fabric - pip install fabric
 #
 # example invocation
-# $ fab -H jenkins@uf04.seedscientific.com fabfile.py
-
-
-# fab hello:name=Elliot
-def hello(name="world"):
-    print("Hello %s!" % name)
+# $ fab -H jenkins@uf04.seedscientific.com deploy
 
 from fabric.api import local, run, cd, put
 from fabvenv import virtualenv, make_virtualenv
@@ -25,16 +20,23 @@ def test():
 # build-machine dependencies - node, gulp, bower, sass, compass, ruby, virtualenv, fabric-virtualenv
 def deploy():
     # /var/www/clients.seedscientific.com/uf/UF04
-    code_dir = '~/polio'
+    work_path = '~/polio'
+    venv_path = '/tmp/venv'
+
+    backend_path = '/tmp/polio-backend/'
+    frontend_path = '/tmp/polio-frontenv/'
+
+    ###
+    ### on build machine...
+    ###
 
     # set up dependencies
     print ("TODO: confirm build machine has dependencies. i.e. node, gulp.")
+    # e.g.
     # sudo gem install sass
     # sudo gem install compass
 
     # enter virtual environment
-    # local ("virtualenv /tmp/venv") # we may be able to save space and time
-    venv_path = '/tmp/venv'
     make_virtualenv(venv_path)
     with virtualenv(venv_path):
         local ("pip install -r requirements.txt")
@@ -43,19 +45,46 @@ def deploy():
         local("./node_modules/.bin/bower install")
         local("./node_modules/.bin/gulp dist")
 
-    # put dist
-    put ('dist/uf04-frontend.zip', code_dir)
-    put ('dist/uf04-backend.zip', code_dir)
+    ###
+    ### on target machine...
+    ###
 
     # make folder if it doesn't exist
-    # run ("mkdir -p %s" % code_dir)
+    run ("mkdir -p %s" % work_path)
 
+    # push to remote server
+    put ('dist/uf04-frontend.zip', work_path)
+    put ('dist/uf04-backend.zip', work_path)
 
-    # with cd(code_dir):
-        # run("touch fab.test")
+    # unzip stuff
+    with cd(work_path):
+        run("unzip uf04-frontend.zip -d %s" % frontend_path)
+        run("unzip uf04-backend.zip -d %s" % backend_path)
 
-# deploy -
-def
+    # in front-end path
+    # with cd(frontend_path):
+    #     run("chown -R www-data:www-data .")
+    #
+    # # in server path -
+    # with cd(backend_path):
+    #     run("pip install -r requirements.txt")
+    #
+    #     # echo "== SYNCDB / MIGRATE =="
+    #     run("python manage.py syncdb --settings=polio.prod_settings")
+    #     run("python manage.py migrate --settings=polio.prod_settings")
+    #
+    #     # echo "== BUILDING DATABASE =="
+    #     run("bash bin/build_db.sh")
+
+    # bounce apache??
+    # customize any other configuration?
+    #
+    # echo "== BUILDING DOCUMENTATION ==" # maybe...
+    # make clean -C docs
+    # make html -C docs
+    #
+    # echo "== RUNNING TESTS =="
+    # python manage.py test datapoints.tests.test_cache --settings=polio.settings_test
 
 # def prepare_deploy():
     # local("pip install -r requirements.txt")
