24d56d8265c39c5d0cbb8a13ab920f9ef664617e
John Dingee <dingee@seedscientific.com>
2015-03-19 01:59:46 -0400

    ensure that raw datapoints also are deleted before re-inserted

--- datapoints/sql/functions/fn_calc_datapoint.sql
+++ datapoints/sql/functions/fn_calc_datapoint.sql
@@ -7,7 +7,8 @@ RETURNS TABLE(id int) AS $$
 	DELETE FROM datapoint_with_computed dwc
 	USING agg_datapoint ad
 	INNER JOIN ( -- a table with all of the idicators needed for a calculation
-		SELECT 
+
+		SELECT  -- FOR INDICATORS NEEDED FOR CALCULATION --
 			cic.indicator_component_id as input_indicator_id
 			,cic_all.indicator_component_id as output_indicator_id
 		FROM calculated_indicator_component cic
@@ -16,10 +17,21 @@ RETURNS TABLE(id int) AS $$
 
 		UNION ALL
 		
-		SELECT 
+		SELECT -- FOR THE CALCULATED INDICATORS --
 			cic.indicator_component_id as input_indicator
 			,cic.indicator_id as output_indicator
 		FROM calculated_indicator_component cic
+
+		UNION ALL -- FOR RAW INDICATORS --
+
+		SELECT 
+			id
+			,id
+		FROM indicator i
+		WHERE EXISTS ( 
+			SELECT 1 FROM datapoint d 
+			WHERE i.id = d.indicator_id
+		)
 	)x
 	ON ad.indicator_id = x.input_indicator_id
 	WHERE dwc.campaign_id = ad.campaign_id
@@ -89,67 +101,67 @@ RETURNS TABLE(id int) AS $$
             AND d_part.region_id = d_whole.region_id
             WHERE (d_part.cache_job_id = $1 or d_whole.cache_job_id = $1);
 
---         INSERT INTO datapoint_with_computed
---         (indicator_id,region_id,campaign_id,value,cache_job_id)
--- 
---         SELECT
--- 		denom.master_indicator_id
--- 		,denom.region_id
--- 		,denom.campaign_id
--- 		,(CAST(num_whole.value as FLOAT) - CAST(num_part.value as FLOAT)) / NULLIF(CAST(denom.value AS FLOAT),0) as calculated_value
---                , $1
---           FROM (
---           	SELECT
---           		cic.indicator_id as master_indicator_id
---           		,ad.region_id
---           		,ad.indicator_id
---           		,ad.campaign_id
---           		,ad.value
--- 			,ad.cache_job_id
---           	FROM agg_datapoint ad
---           	INNER JOIN calculated_indicator_component cic
---           	ON cic.indicator_component_id = ad.indicator_id
---           	AND calculation = 'PART_OF_DIFFERENCE'
---           	AND ad.cache_job_id = $1
---           )num_part
--- 
---           INNER JOIN (
---           	SELECT
---           		cic.indicator_id as master_indicator_id
---           		,ad.region_id
---           		,ad.indicator_id
---           		,ad.campaign_id
---           		,ad.value
--- 			,ad.cache_job_id
---           	FROM agg_datapoint ad
---           	INNER JOIN calculated_indicator_component cic
---           	ON cic.indicator_component_id = ad.indicator_id
---           	AND calculation = 'WHOLE_OF_DIFFERENCE'
--- 		AND ad.cache_job_id = $1
--- 
---           )num_whole
---           ON num_part.master_indicator_id = num_whole.master_indicator_id
---           AND num_part.region_id = num_whole.region_id
---           AND num_part.campaign_id = num_whole.campaign_id
--- 
---           INNER JOIN
---           (
---           	SELECT
---           		cic.indicator_id as master_indicator_id
---           		,ad.region_id
---           		,ad.indicator_id
---           		,ad.campaign_id
---           		,ad.value
---           		,ad.cache_job_id
---           	FROM agg_datapoint ad
---           	INNER JOIN calculated_indicator_component cic
---           	ON cic.indicator_component_id = ad.indicator_id
---           	AND calculation = 'WHOLE_OF_DIFFERENCE_DENOMINATOR'
--- 		AND ad.cache_job_id = $1
---           )denom
---           ON num_whole.region_id = denom.region_id
---           AND num_whole.master_indicator_id = denom.master_indicator_id
---           AND num_whole.campaign_id = denom.campaign_id;
+        INSERT INTO datapoint_with_computed
+        (indicator_id,region_id,campaign_id,value,cache_job_id)
+
+        SELECT
+		denom.master_indicator_id
+		,denom.region_id
+		,denom.campaign_id
+		,(CAST(num_whole.value as FLOAT) - CAST(num_part.value as FLOAT)) / NULLIF(CAST(denom.value AS FLOAT),0) as calculated_value
+               , $1
+          FROM (
+          	SELECT
+          		cic.indicator_id as master_indicator_id
+          		,ad.region_id
+          		,ad.indicator_id
+          		,ad.campaign_id
+          		,ad.value
+			,ad.cache_job_id
+          	FROM agg_datapoint ad
+          	INNER JOIN calculated_indicator_component cic
+          	ON cic.indicator_component_id = ad.indicator_id
+          	AND calculation = 'PART_OF_DIFFERENCE'
+          	AND ad.cache_job_id = $1
+          )num_part
+
+          INNER JOIN (
+          	SELECT
+          		cic.indicator_id as master_indicator_id
+          		,ad.region_id
+          		,ad.indicator_id
+          		,ad.campaign_id
+          		,ad.value
+			,ad.cache_job_id
+          	FROM agg_datapoint ad
+          	INNER JOIN calculated_indicator_component cic
+          	ON cic.indicator_component_id = ad.indicator_id
+          	AND calculation = 'WHOLE_OF_DIFFERENCE'
+		AND ad.cache_job_id = $1
+
+          )num_whole
+          ON num_part.master_indicator_id = num_whole.master_indicator_id
+          AND num_part.region_id = num_whole.region_id
+          AND num_part.campaign_id = num_whole.campaign_id
+
+          INNER JOIN
+          (
+          	SELECT
+          		cic.indicator_id as master_indicator_id
+          		,ad.region_id
+          		,ad.indicator_id
+          		,ad.campaign_id
+          		,ad.value
+          		,ad.cache_job_id
+          	FROM agg_datapoint ad
+          	INNER JOIN calculated_indicator_component cic
+          	ON cic.indicator_component_id = ad.indicator_id
+          	AND calculation = 'WHOLE_OF_DIFFERENCE_DENOMINATOR'
+		AND ad.cache_job_id = $1
+          )denom
+          ON num_whole.region_id = denom.region_id
+          AND num_whole.master_indicator_id = denom.master_indicator_id
+          AND num_whole.campaign_id = denom.campaign_id;
 
 	-- FIX ME --
 	UPDATE datapoint_with_computed SET value = 0.00 
