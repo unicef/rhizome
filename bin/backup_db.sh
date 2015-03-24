14c3c121d0b1f943269552a5d857d935891662a7
John Dingee <dingee@seedscientific.com>
2015-03-24 17:23:05 -0400

    use dumpall

--- bin/backup_db.sh
+++ bin/backup_db.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 
+# http://serverfault.com/questions/59838/whats-the-best-way-to-automate-backing-up-of-postgresql-databases
+
 BACKUPDIR=/var/lib/postgresql/sql_backups/
-COMPRESSION=4
 DATE=`date +%Y-%m-%dT%H:%M:%S`
 DB=polio
 USER=djangoapp
 
+DUMPALL="/usr/bin/pg_dumpall"
+PGDUMP="/usr/bin/pg_dump"
+PSQL="/usr/bin/psql"
+
 echo Backing up $DB database to $BACKUPDIR$DATE-$DB.sql.tgz ...
 
-pg_dump --verbose --format=t -f "$BACKUPDIR$DATE.sql.tar" $DB -U $USER
+DUMPALL$ --verbose --format=t -f "$BACKUPDIR$DATE.sql.tar" $DB -U $USER
+
+# delete backup files older than 30 days
+OLD=$(find $BACKUPDIR -type d -mtime +30)
+if [ -n "$OLD" ] ; then
+        echo deleting old backup files: $OLD
+        echo $OLD | xargs rm -rfv
+fi
