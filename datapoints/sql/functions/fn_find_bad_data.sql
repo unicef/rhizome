958b89b6c4f89506e0c9fd3104d14c78502d3f04
John Dingee <dingee@seedscientific.com>
2015-03-26 10:47:23 -0400

    add sproc for bad data

---
+++ datapoints/sql/functions/fn_find_bad_data.sql
@@ -0,0 +1,13 @@
+﻿DROP FUNCTION IF EXISTS fn_find_bad_data(cache_job_id INT);
+CREATE FUNCTION fn_find_bad_data(cache_job_id INT)
+RETURNS TABLE(id int) AS $$
+
+	
+	SELECT DISTINCT ID
+	FROM datapoint limit 1
+		
+$$
+
+LANGUAGE SQL;
+
+
