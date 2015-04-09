13fc54015af37168bea5f5fdb293fd4950d20388
John Dingee <dingee@seedscientific.com>
2015-04-09 11:03:02 -0400

    add recursive cte for sum of parts

--- datapoints/sql/functions/fn_calc_sum_of_parts.sql
+++ datapoints/sql/functions/fn_calc_sum_of_parts.sql
@@ -43,6 +43,44 @@ WHERE cic.indicator_id = 265
 GROUP BY tcd.campaign_id, tcd.region_id, cic.indicator_id;
 
 
+/*
+WITH RECURSIVE ind_graph AS
+(
+    -- non-recursive term ( rows where the components aren't
+    -- master_indicators in another calculation )
+
+	SELECT
+		id
+		,indicator_id
+		,indicator_component_id
+		,0 as lvl
+	FROM calculated_indicator_component cic
+	WHERE NOT EXISTS (
+		SELECT 1 FROM calculated_indicator_component cic_leaf
+		WHERE cic.indicator_component_id = cic_leaf.indicator_id
+	)
+	AND calculation = 'PART_TO_BE_SUMMED'
+
+	UNION ALL
+
+	-- recursive term
+	SELECT
+		cic_recurs.id
+		,cic_recurs.indicator_id
+		,cic_recurs.indicator_component_id
+		,ig.lvl + 1
+	FROM calculated_indicator_component AS cic_recurs
+	INNER JOIN ind_graph AS ig
+	ON (cic_recurs.indicator_component_id = ig.indicator_id)
+	AND calculation = 'PART_TO_BE_SUMMED'
+	)
+
+SELECT *
+FROM ind_graph
+ORDER BY lvl DESC;
+*/
+
+
 
 	RETURN QUERY
 
