d8485ebe7d11f18e067ef2ca2e2fb51769449a2b
Edward Lee <lee@seedscientific.com>
2015-04-10 11:38:07 -0400

    Updates to indicator sets json.

--- ui/js/view/entry-form/structure/indicator_sets.js
+++ ui/js/view/entry-form/structure/indicator_sets.js
@@ -27,6 +27,7 @@ module.exports = [
 				{ 'id': 432, 'name': 'Non-polio AFP cases with 1-3 doses of OPV' },
 				{ 'id': 433, 'name': 'Non-polio AFP cases with 4+ doses of OPV' },
 
+				{ 'type': 'section-header', 'title': 'Polio+' },
 				{ 'id': 29, 'name': 'Number of caregivers in high risk districts' },
 				{ 'id': 50, 'name': 'Number of caregivers in HRD who know times needed to visit RI site before 1 yo' },
 
