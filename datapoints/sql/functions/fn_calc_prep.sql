4e5859e7422ba6cce825a6de807c68acb5c8e1c5
John Dingee <dingee@seedscientific.com>
2015-04-16 15:48:37 -0400

    refactor calc_prep sproc to only select relevant indicator_ids

--- datapoints/sql/functions/fn_calc_prep.sql
+++ datapoints/sql/functions/fn_calc_prep.sql
@@ -19,24 +19,12 @@ BEGIN
 				DROP TABLE IF EXISTS _region_campaign;
 				DROP TABLE IF EXISTS _raw_indicators;
 				DROP TABLE IF EXISTS _indicators_needed_to_calc;
+				DROP TABLE IF EXISTS _tmp_calc_datapoint ;
 
-				CREATE TABLE _region_campaign
-				AS
-
-				SELECT DISTINCT
-					region_id
-					, campaign_id
-				FROM agg_datapoint ad
-				WHERE ad.cache_job_id = $1;
-
-				-----
-
-		 		CREATE TABLE _raw_indicators AS
-
-		  		SELECT DISTINCT
-		  			indicator_id
-		  		FROM agg_datapoint ad
-		  		WHERE ad.cache_job_id = $1;
+				CREATE TABLE _raw_indicators AS
+				SELECT 43 as indicator_id -- LEAF LEVEL MISSED CHD
+				--SELECT 265 as indicator_id -- MID LEVEL MISSED CHD
+				;
 
 				-----
 
@@ -48,53 +36,56 @@ BEGIN
 				-- non-recursive term ( rows where the components aren't
 				-- master_indicators in another calculation )
 
-				SELECT
-						 cic.id
-					,cic.indicator_id
-					,cic.indicator_component_id
-					--,0 as lvl
-				FROM calculated_indicator_component cic
-				WHERE NOT EXISTS (
-					SELECT 1 FROM calculated_indicator_component cic_leaf
-					WHERE cic.indicator_component_id = cic_leaf.indicator_id
-				)
-
-				UNION ALL
-
-				-- recursive term --
-				SELECT
-					cic_recurs.id
-					,cic_recurs.indicator_id
-					,ig.indicator_component_id
-					--,ig.lvl + 1
-				FROM calculated_indicator_component AS cic_recurs
-				INNER JOIN ind_graph AS ig
-				ON (cic_recurs.indicator_component_id = ig.indicator_id)
-
-				)
-
-				SELECT DISTINCT indicator_id FROM (
-					SELECT DISTINCT ig2.indicator_id
-					FROM ind_graph ig
-					INNER JOIN _raw_indicators ri
-						ON ig.indicator_component_id = ri.indicator_id
-					INNER JOIN ind_graph ig2
-						ON ig.indicator_id = ig2.indicator_id
+					SELECT
+							cic.id
+						,cic.indicator_id
+						,cic.indicator_component_id
+						--,0 as lvl
+					FROM calculated_indicator_component cic
+					WHERE NOT EXISTS (
+						SELECT 1 FROM calculated_indicator_component cic_leaf
+						WHERE cic.indicator_component_id = cic_leaf.indicator_id
+					)
+					--AND calculation = 'PART_TO_BE_SUMMED'
 
 					UNION ALL
 
-					SELECT ig2.indicator_component_id
-					FROM ind_graph ig
-					INNER JOIN _raw_indicators ri
-						ON ig.indicator_component_id = ri.indicator_id
-					INNER JOIN ind_graph ig2
-						ON ig.indicator_id = ig2.indicator_id
-				)x;
+					-- recursive term --
+					SELECT
+						cic_recurs.id
+						,cic_recurs.indicator_id
+						,ig.indicator_component_id
+						--,ig.lvl + 1
+					FROM calculated_indicator_component AS cic_recurs
+					INNER JOIN ind_graph AS ig
+					ON (cic_recurs.indicator_component_id = ig.indicator_id)
+					--AND calculation = 'PART_TO_BE_SUMMED'
+				)
+
+				-- ALL MASTER INDICATORS ( AT ALL LEVELS ) --
+				SELECT DISTINCT ig.indicator_id
+				FROM ind_graph ig
+				INNER JOIN _raw_indicators ri
+				ON ri.indicator_id = indicator_component_id;
 
 				CREATE UNIQUE INDEX uq_ind_ix ON _indicators_needed_to_calc (indicator_id);
 
+				-- ALL COMPONENT INDICATORS NEEDED TO MAKE THE CALC --
 
-				DROP TABLE IF EXISTS _tmp_calc_datapoint ;
+				INSERT INTO _indicators_needed_to_calc
+				(indicator_id)
+
+				SELECT DISTINCT
+					cic.indicator_component_id
+				FROM calculated_indicator_component cic
+				INNER JOIN _indicators_needed_to_calc intc
+					ON cic.indicator_id = intc.indicator_id
+				WHERE NOT EXISTS (
+					SELECT 1 FROM _indicators_needed_to_calc tc
+					WHERE cic.indicator_component_id = tc.indicator_id
+				);
+
+
 				CREATE TABLE _tmp_calc_datapoint AS
 
 				SELECT DISTINCT
