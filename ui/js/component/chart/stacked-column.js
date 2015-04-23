7f7f23c31c90bc264b3b21a4deb19f5023e205f3
W. Evan Sheehan <wallace.sheehan@gmail.com>
2015-04-23 11:50:23 -0400

    Add a label for the x-axis to column charts on hover
    This makes it easier to navigate the charts

--- ui/js/component/chart/stacked-column.js
+++ ui/js/component/chart/stacked-column.js
@@ -1,3 +1,4 @@
+/* global window */
 'use strict';
 
 var _      = require('lodash');
@@ -242,15 +243,43 @@ module.exports = {
 				.value();
 
 			var svg = d3.select(this.$$.svg);
+			var annotation = svg.select('.annotation');
 
-			svg.select('.annotation')
-				.selectAll('.series.label')
+			annotation.selectAll('.series.label')
 				.data(labels)
 				.call(label()
 					.addClass('series')
 					.width(this.contentWidth)
 					.height(this.contentHeight)
 					.align(true));
+
+			svg.selectAll('.x.axis text')
+				.transition()
+				.duration(300)
+				.style('opacity', 0);
+
+			var xLabel = annotation.selectAll('.axis.label')
+				.data([target]);
+
+			xLabel.enter()
+				.append('text')
+				.attr({
+					'class' : 'axis label',
+					'dy'    : '1.2em',
+					'y'     : this.contentHeight,
+					'x'     : function (d) { return xScale(d); }
+				});
+
+			var labelFmt = this.xLabel;
+			xLabel
+				.text(function (d) {
+					return moment(d).format(labelFmt);
+				})
+				.transition()
+				.duration(300)
+				.attr('x', function (d) {
+					return xScale(d);
+				});
 
 			svg.select('.data')
 				.selectAll('rect')
@@ -270,14 +299,26 @@ module.exports = {
 
 					self._currentHover = null;
 
-					svg.select('.annotation')
-						.selectAll('.series.label')
+					var annotation = svg.select('.annotation');
+
+					annotation.selectAll('.series.label')
 						.data(self.labels)
 						.call(label()
 							.addClass('series')
 							.width(self.contentWidth)
 							.height(self.contentHeight)
 							.align(false));
+
+					annotation.selectAll('.axis.label')
+						.transition()
+						.duration(300)
+						.style('opacity', 0)
+						.remove();
+
+					svg.selectAll('.x.axis text')
+						.transition()
+						.duration(300)
+						.style('opacity', 1);
 
 					svg.select('.data')
 						.selectAll('rect')
