49df3f8497168a363c546cb2a5bd9694b5205675
John Dingee <dingee@seedscientific.com>
2015-04-24 14:26:07 -0400

    dont join to datapoint_with_computed in sum_of_parts calculation

--- datapoints/sql/functions/fn_calc_sum_of_parts.sql
+++ datapoints/sql/functions/fn_calc_sum_of_parts.sql
@@ -47,15 +47,9 @@ BEGIN
       , dwc.campaign_id
       , SUM(dwc.value) as agg_value
     FROM ind_graph ig
-    INNER JOIN datapoint_with_computed dwc
+    INNER JOIN _tmp_calc_datapoint dwc
         ON 1 = 1
         AND ig.indicator_component_id = dwc.indicator_id
-    WHERE EXISTS (
-        SELECT 1 FROM _tmp_calc_datapoint tcd
-        WHERE 1 = 1
-        AND tcd.region_id = dwc.region_id
-        AND tcd.campaign_id = dwc.campaign_id
-    )
     AND NOT EXISTS (
       SELECT 1 FROM _tmp_calc_datapoint tcd
       WHERE dwc.region_id = tcd.region_id
