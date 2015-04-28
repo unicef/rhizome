9656b10af8fd818ef997a573ae28974f4d3d5204
W. Evan Sheehan <sheehan@seedscientific.com>
2015-04-28 14:14:19 -0400

    Add formatted immunity gap data to the District dashboard VM
    * Add a new indexIndicators data transform that replaces the indicators
      array on a datum with a dictionary that maps indicator id to value
    * Uncurry the variables function so that it can be called directly; we
      can use _.curryRight to replicate the old behavior
    * Add an accessor argument to the data.defined utility for convenience
    * Add an immunityGap matrix to the District dashboard VM with one array
      for each indicator, where each is an array of series with each series
      corresponding to a single region and transformed so that the x
      property is the campaign start and the y is the indicator value

--- ui/js/dashboard/district.js
+++ ui/js/dashboard/district.js
@@ -1,9 +1,13 @@
 /* global window */
 'use strict';
 
-var _   = require('lodash');
+var _      = require('lodash');
+var moment = require('moment');
 
-var api = require('data/api');
+var api             = require('data/api');
+var indexIndicators = require('data/transform/indexIndicators');
+var variables       = require('data/transform/variables');
+var util            = require('util/data');
 
 module.exports = {
 	template : require('./district.html'),
@@ -15,13 +19,55 @@ module.exports = {
 			currentSection : null,
 			highlights     : [],
 			region         : null,
-			sections       : []
+			sections       : [],
+
+			immunityGap    : new Array(3)
 		};
 	},
 
 	methods : {
 		dataReceived : function (data) {
+			function createDataPoint(d, indicator) {
+				return _.assign(_.pick(d, 'campaign', 'region'), {
+					indicator : indicator,
+					x         : startDate(d),
+					y         : d.indicators[indicator]
+				});
+			}
 
+			function createDataSeries(arr, indicator) {
+				return _(arr)
+					.map(datumTransform(indicator, _, _))
+					.filter(defined)
+					.value();
+			}
+
+			var startDate       = util.accessor('campaign.start_date');
+			var sort            = _.curryRight(_.sortBy)('x');
+			var defined         = _.curryRight(util.defined)(util.accessor('y'), _, _);
+			var datumTransform  = _.curryRight(createDataPoint);
+			var seriesTransform = _.curryRight(createDataSeries);
+
+			var series = _(data.objects)
+				.map(indexIndicators)
+				.groupBy('region')
+				.map(sort)
+				.value();
+
+			this.immunityGap[0] = _(series)
+				.map(seriesTransform(431, _, _))
+				.reject(_.isEmpty)
+				.value();
+
+			this.immunityGap[1] = _(series)
+				.map(seriesTransform(432, _, _))
+				.reject(_.isEmpty)
+				.value();
+
+			this.immunityGap[2] = _(series)
+				.map(seriesTransform(433, _, _))
+				.reject(_.isEmpty)
+				.value()
 		},
 
 		error : function () {
@@ -29,13 +75,17 @@ module.exports = {
 		},
 
 		load : function () {
+			if (!(this.campaign && this.region)) {
+				return;
+			}
+
 			var q = {
-				parent_region__in : this.region,
+				parent_region__in : this.region.id,
 				level             : 'district'
 			};
 
 			var start = moment(this.campaign.start_date)
-				.subtract('1 year')
+				.subtract(1, 'year')
 				.format('YYYY-MM-DD');
 
 			var end = moment(this.campaign.start_date).format('YYYY-MM-DD');
