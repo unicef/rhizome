ce81bde664b8f45ee1aa8a6e53626dcea67e0cba
John Dingee <dingee@seedscientific.com>
2015-05-01 10:30:27 -0400

    bring back mapping screens for old document_index

--- source_data/urls.py
+++ source_data/urls.py
@@ -7,6 +7,17 @@ urlpatterns = [
     url(r'^map_header/(?P<document_id>[0-9]+)/',map_header\
         , name='map_header'),
 
+    url(r'^map_indicator/(?P<pk>[0-9]+)/$', IndicatorMapCreateView.as_view()\
+        ,name='map_indicator'),
+
+    url(r'^map_region/(?P<pk>[0-9]+)/$', RegionMapCreateView.as_view()\
+        ,name='map_region'),
+
+    url(r'^map_campaign/(?P<pk>[0-9]+)/$', CampaignMapCreateView.as_view(),\
+        name='map_campaign'),
+
+    url(r'^un_map/(?P<source_object_id>[0-9]+)/(?P<db_model>\w+)/(?P<document_id>[0-9]+)/$',un_map,name='un_map'),
+
     url(r'^refresh_master/$', refresh_master,name='refresh_master'),
 
     url(r'^mark_doc_as_processed/(?P<document_id>[0-9]+)/$'\
