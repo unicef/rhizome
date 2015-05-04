04f5fdda79bb98eb3cd1dcc661093bd7db49094c
W. Evan Sheehan <sheehan@seedscientific.com>
2015-05-04 15:24:29 -0400

    Remove indicators that aren't collected at district level

--- ui/js/dashboard/district.js
+++ ui/js/dashboard/district.js
@@ -50,7 +50,6 @@ module.exports = {
 			}
 
 			var indicators = [
-				431,432,433,  // Immunity gap
 				475,166,164,167,165, // Missed Children
 				222, // Microplans
 				187,189, // Conversions
@@ -58,7 +57,6 @@ module.exports = {
 				178,228,179,184,180,185,230,226,239, // Capacity to Perform
 				194,219,173,172, // Supply
 				245,236,192,193,191, // Polio+
-				169,233, // Resources
 				174, // Access plan
 				442,443,444,445,446,447,448,449,450 // Inaccessibility
 			];
