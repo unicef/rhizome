0cab3d3f561d8ee94e61c0608cd903c7b0f2828c
John Dingee <dingee@seedscientific.com>
2015-05-06 17:23:11 -0400

    recursively filter regions

--- datapoints/sql/functions/fn_get_authorized_regions_by_user.sql
+++ datapoints/sql/functions/fn_get_authorized_regions_by_user.sql
@@ -1,14 +1,33 @@
-DROP FUNCTION IF EXISTS fn_get_authorized_regions_by_user(cache_job_id int);
-CREATE FUNCTION fn_get_authorized_regions_by_user(cache_job_id int)
-RETURNS TABLE(id int) AS
+DROP FUNCTION IF EXISTS fn_get_authorized_regions_by_user(user_id int);
+CREATE FUNCTION fn_get_authorized_regions_by_user(user_id int)
+RETURNS TABLE(
+  id INT
+  ,parent_region_id INT
+  ,office_id INT
+  ,region_type_id INT
+  ,name VARCHAR
+) AS
 $func$
 BEGIN
 
-	RETURN QUERY
+  RETURN QUERY
 
-	SELECT r.id FROM region r
-	--WHERE dwc.cache_job_id = $1
-	LIMIT 1;
+
+	SELECT
+		r.id
+		,r.parent_region_id
+		,r.office_id
+		,r.region_type_id
+		,r.name
+	FROM region r
+	WHERE EXISTS (
+		SELECT 1
+		FROM region_permission rm
+		WHERE rm.user_id = 1
+		AND r.id = rm.region_id
+	);
+
+
 
 END
 $func$ LANGUAGE PLPGSQL;
