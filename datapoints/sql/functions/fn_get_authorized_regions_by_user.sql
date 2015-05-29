958fbfab07cf6b011b67422f69cf76d48df6a170
John Dingee <dingee@seedscientific.com>
2015-05-29 12:12:00 -0400

    add depth to region_auth sproc

--- datapoints/sql/functions/fn_get_authorized_regions_by_user.sql
+++ datapoints/sql/functions/fn_get_authorized_regions_by_user.sql
@@ -1,5 +1,5 @@
-DROP FUNCTION IF EXISTS fn_get_authorized_regions_by_user(user_id int, list_of_region_ids integer[], read_write varchar(1));
-CREATE FUNCTION fn_get_authorized_regions_by_user(user_id int, list_of_region_ids integer[], read_write varchar(1))
+DROP FUNCTION IF EXISTS fn_get_authorized_regions_by_user(user_id int, list_of_region_ids integer[], read_write varchar(1),depth_level INT);
+CREATE FUNCTION fn_get_authorized_regions_by_user(user_id int, list_of_region_ids integer[], read_write varchar(1),depth_level INT)
 
 RETURNS TABLE(
 
@@ -21,56 +21,67 @@ BEGIN
 
 	RETURN QUERY
 
-	WITH RECURSIVE region_tree AS
-	(
-	-- non-recursive term ( rows where the components aren't
-	-- master_indicators in another calculation )
+  DROP TABLE IF EXISTS _permitted_regions;
 
-	SELECT
-		rg.parent_region_id
-		,rg.parent_region_id as immediate_parent_id
-		,rg.id as region_id
-		,0 as lvl
-	FROM region rg
+  CREATE TABLE _permitted_regions AS
+  WITH RECURSIVE region_tree(parent_region_id, immediate_parent_id, region_id, lvl) AS
+  	(
+  	-- non-recursive term ( rows where the components aren't
+  	-- master_indicators in another calculation )
 
-	UNION ALL
+  	SELECT
+  		rg.parent_region_id
+  		,rg.parent_region_id as immediate_parent_id
+  		,rg.id as region_id
+  		,1 as lvl
+  	FROM region rg
 
-	-- recursive term --
-	SELECT
-		r_recurs.parent_region_id
-		,rt.parent_region_id as immediate_parent_id
-		,rt.region_id
-		,rt.lvl + 1
-	FROM region AS r_recurs
-	INNER JOIN region_tree AS rt
-	ON (r_recurs.id = rt.parent_region_id)
-	AND r_recurs.parent_region_id IS NOT NULL
-	)
+  	UNION ALL
 
-	SELECT
-		r.*
-  FROM region_tree rt
-	INNER JOIN region r
-	ON rt.region_id = r.id
-	WHERE EXISTS (
-		SELECT 1
-		FROM region_permission rm
-		WHERE rm.user_id = $1
-		AND rt.parent_region_id = rm.region_id
-		AND rm.read_write = $3
-	)
-  AND r.id = ANY(COALESCE($2,ARRAY[r.id]))
+  	-- recursive term --
+  	SELECT
+  		r_recurs.parent_region_id
+  		,rt.parent_region_id as immediate_parent_id
+  		,rt.region_id
+  		,rt.lvl + 1
+  	FROM region AS r_recurs
+  	INNER JOIN region_tree AS rt
+  	ON (r_recurs.id = rt.parent_region_id)
+  	AND r_recurs.parent_region_id IS NOT NULL
+  	)
 
-  UNION ALL
+  	SELECT * FROM (
 
-  SELECT
-    r.*
-  FROM region r
-  INNER JOIN region_permission rp
-    ON r.id = rp.region_id
-    AND rp.user_id = $1
-    AND r.id = ANY(COALESCE($2,ARRAY[r.id]))
-    AND rp.read_write = $3;
+  		SELECT rt.lvl,r.*
+  		FROM region_tree rt
+  		INNER JOIN region r
+  		ON rt.region_id = r.id
+  		WHERE EXISTS (
+  			SELECT 1
+  			FROM region_permission rm
+  			WHERE rm.user_id = $1
+  			AND rm.read_write = $3
+  			AND rt.parent_region_id = rm.region_id
+  		)
+
+  		UNION ALL
+  		SELECT
+  			0 as lvl
+  			,r.*
+  		FROM region r
+  		INNER JOIN region_permission rp
+  		ON r.id = rp.region_id
+  		AND rp.user_id = $1
+  		AND rp.read_write = $3
+
+  	)x
+  	WHERE x.id = ANY(COALESCE($2,ARRAY[x.id]));
+
+  	SELECT
+  		*
+  	FROM _permitted_regions
+  	WHERE lvl <= $4
+  	ORDER BY lvl;
 
 END
 $func$ LANGUAGE PLPGSQL;
