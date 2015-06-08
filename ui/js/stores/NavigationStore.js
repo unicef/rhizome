31d3f134c78aa4d6fd66fb3e6e20cf1eff72bb02
W. Evan Sheehan <sheehan@seedscientific.com>
2015-06-08 15:22:01 -0400

    Basic initial data in the NavigationStore
    * Hard-code the three available dashboards for now
    * Add a getInitialState method to the store for initializing the
      components that subscribe

--- ui/js/stores/NavigationStore.js
+++ ui/js/stores/NavigationStore.js
@@ -3,19 +3,26 @@
 var Reflux = require('reflux/src');
 
 var NavigationStore = Reflux.createStore({
-	data : {
-		dashboards : [{
-			'name' : 'Management: Country',
-			'url'  : '/datapoints/management-dashboard',
-		}, {
-			'name' : 'Management: Districts',
-			'url'  : '/datapoints/district'
-		}, {
-			'name'    : 'NGA Campaign Monitoring',
-			'url'     : '/datapoints/nga-campaign-monitoring',
-			'region'  : 'Nigeria',
-			'offices' : [1]
-		}]
+	init : function () {
+		// FIXME: This should fetch data from the server...
+		this.data = {
+			dashboards : [{
+					'name' : 'Management: Country',
+					'url'  : '/datapoints/management-dashboard'
+				}, {
+					'name' : 'Management: Districts',
+					'url'  : '/datapoints/district'
+				}, {
+					'name'    : 'NGA Campaign Monitoring',
+					'url'     : '/datapoints/nga-campaign-monitoring',
+					'region'  : 'Nigeria',
+					'offices' : [1]
+				}]
+			};
+	},
+
+	getInitialState : function () {
+		return this.data;
 	}
 });
 
