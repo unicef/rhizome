e8c7f2cbf16c34ce503afeed32e8b665f020d544
Dan Delany <dan.delany@gmail.com>
2015-06-15 14:30:47 -0400

    rename dev config to sample

--- webpack.config.dev.sample.js
+++ webpack.config.dev.sample.js
@@ -8,6 +8,8 @@ var devConfig = _.merge(config, {
 		contentBase: "./static",
 		hot: true,
 		proxy: {
+			// Copy this file as webpack.config.dev.js
+			// Then change this to the location of your local backend/VM
 			'*': 'http://localhost:8000'
 		}
 	}
