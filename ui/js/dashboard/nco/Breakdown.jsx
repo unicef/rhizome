302e37fb30512e80f317feb7db31a3a20207bf52
W. Evan Sheehan <sheehan@seedscientific.com>
2015-06-16 09:59:34 -0400

    Implement all the stacked bars in the NCO dashboard
    * Allow options to be passed through the ToggleableStackedBar
    * Add a simple method for processing all of the data points for the
      NCO regional breakdown
    * Define the stacked bar charts for the NCO dashboard

--- ui/js/dashboard/nco/Breakdown.jsx
+++ ui/js/dashboard/nco/Breakdown.jsx
@@ -1,13 +1,23 @@
 'use strict';
 
-var _ = require('lodash');
+var _     = require('lodash');
 var React = require('react');
 
-// var BarChartToggleSection = require('component/BarChartToggleSection.jsx');
+var ToggleableStackedBar = require('dashboard/ToggleableStackedBar.jsx');
+
+function prep (data) {
+  return _(data)
+    .groupBy('region.id')
+    .filter(v => _(v).pluck('value').some(_.isFinite))
+    .flatten()
+    .groupBy('indicator.short_name')
+    .map((v, n) => { return { name : n, values : v }; })
+    .value();
+}
 
 var Breakdown = React.createClass({
   propTypes : {
-    data : React.PropTypes.object.isRequired,
+    data    : React.PropTypes.object.isRequired,
     loading : React.PropTypes.bool
   },
 
@@ -18,7 +28,78 @@ var Breakdown = React.createClass({
   },
 
   render : function () {
-    return (<h4>Coming Soon!</h4>);
+    var data    = this.props.data;
+    var loading = this.props.loading;
+
+    var options = {
+      x : _.property('value'),
+      y : _.property('region.name'),
+      margin : {
+        top    : 0,
+        right  : 80,
+        bottom : 18,
+        left   : 80
+      }
+    }
+
+    return (
+      <div>
+        <div className='row'>
+          <div className='medium-8 columns'>
+            <ToggleableStackedBar
+              title='Missed Children'
+              loading={loading}
+              options={options}
+              data={prep(data.missedChildren)} />
+          </div>
+        </div>
+
+        <div className='row'>
+          <div className='medium-6 columns'>
+            <ToggleableStackedBar
+              title='Absences'
+              loading={loading}
+              options={options}
+              data={prep(data.absences)} />
+          </div>
+
+          <div className='medium-6 columns'>
+            <ToggleableStackedBar
+              title='Non-Compliance'
+              loading={loading}
+              options={options}
+              data={prep(data.nonCompliance)} />
+          </div>
+        </div>
+
+        <div className='row'>
+          <div className='medium-4 columns'>
+            <ToggleableStackedBar
+              title='Non-Compliance Resolutions'
+              loading={loading}
+              options={options}
+              data={prep(data.nonComplianceResolutions)} />
+          </div>
+
+          <div className='medium-4 columns'>
+            <ToggleableStackedBar
+              title='Influencers'
+              loading={loading}
+              options={options}
+              data={prep(data.influencers)} />
+          </div>
+
+          <div className='medium-4 columns'>
+            <ToggleableStackedBar
+              title='Sources of Information'
+              loading={loading}
+              options={options}
+              data={prep(data.sourcesOfInformation)} />
+          </div>
+        </div>
+
+      </div>
+    );
   },
 
 });
