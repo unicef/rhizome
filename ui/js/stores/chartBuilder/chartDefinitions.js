20cbf6b95d38df3e99658d1e921ed710b15a99d3
W. Evan Sheehan <sheehan@seedscientific.com>
2015-06-23 13:56:14 -0400

    Remove "all time" option for column charts
    The implementation is buggy and the line charts are better suited
    for long time series anyway

--- ui/js/stores/chartBuilder/chartDefinitions.js
+++ ui/js/stores/chartBuilder/chartDefinitions.js
@@ -1,6 +1,6 @@
 module.exports = [{name:"LineChart",groupBy:true,chooseAxis:false,timeRadios:["allTime","pastYear","3Months"]},
 			{name:"PieChart",groupBy:false,chooseAxis:false,timeRadios:["current"]},
 			{name:"ChoroplethMap",groupBy:false,chooseAxis:false,timeRadios:["current"]},
-			{name:"ColumnChart",groupBy:true,chooseAxis:false,timeRadios:["allTime","pastYear","3Months","current"]},
+			{name:"ColumnChart",groupBy:true,chooseAxis:false,timeRadios:["pastYear","3Months","current"]},
 			{name:"ScatterChart",groupBy:false,chooseAxis:true,timeRadios:["allTime","pastYear","3Months","current"]},
 			{name:"BarChart",groupBy:true,chooseAxis:false,timeRadios:["current"]}];
