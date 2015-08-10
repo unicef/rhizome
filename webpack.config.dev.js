af7cbcdeca18dd748a50b91758fe3ad98b3a2cd7
John Dingee <dingeej@gmail.com>
2015-08-10 13:52:18 -0400

    creating a new bundle with each webpack build in order to set up .js environment

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,16 +1,25 @@
 var webpack = require('webpack');
 var _ = require('lodash');
 var config = require('./webpack.config.base');
+var path = require("path");
+
+// var devConfig = _.merge(config, {
+
+// });
 
 var devConfig = _.merge(config, {
-	devServer: {
-		port: 8080,
-		contentBase: "./static",
-		hot: true,
-		proxy: {
-			'*': 'http://172.16.252.128'
-		}
-	}
-});
+	output: {
+			 path: path.resolve('./assets/bundles/'),
+			 filename: "[name]-[hash].js",
+	 },
+ // 	devServer: {
+ // 		port: 8080,
+ // 		contentBase: "./static",
+ // 		hot: true,
+ // 		proxy: {
+ // 			'*': 'http://172.16.252.128'
+ // 		}
+ // 	}
+})
 
 module.exports = devConfig;
