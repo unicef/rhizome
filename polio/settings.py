91a72fadd287b7018abb296e18d21125a9ba0618
John Dingee <dingeej@gmail.com>
2015-08-10 22:35:27 -0400

    base_dir not project_dir

--- polio/settings.py
+++ polio/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'assets/bundles/'),
 )
