e84bb3090abf1396141f10a8f4bd3e48cf3b688d
John Dingee <dingeej@gmail.com>
2015-08-17 12:06:34 -0400

    get region drop down on doc_review page - need to populate with region_data

--- ui/js/doc-review/DocOverview.js
+++ ui/js/doc-review/DocOverview.js
@@ -2,8 +2,12 @@
 	var _ = require('lodash');
 	var API = require('../data/api');
 	var DropdownMenu     = require('component/DropdownMenu.jsx');
+	var RegionTitleMenu     = require('component/RegionTitleMenu.jsx');
 
 	var DocOverview = React.createClass({
+		// _setRegion : function (id) {
+	  //   var region    = _.find(this.state.regions, r => r.id === id)
+
 		getInitialState: function() {
 				// https://facebook.github.io/react/tips/initial-ajax.html
 				return {doc_overview: {'docfile':null}};
@@ -28,7 +32,18 @@
 					<a className="button" href={createUrl}>Refresh Master</a>
 				</div> : null;
 
+			var regionDropDown = <div>
+			<h1>
+				<RegionTitleMenu
+					regions={this.state.regions}
+					selected={12907}
+					/>
+			</h1>
+			</div>
+
+
 			return <div>
+			{regionDropDown}
 			<h2> Document ID : {this.state.doc_overview.id} </h2>
 			<h2> Document Name: {this.state.doc_overview.docfile} </h2>
 
--- ui/js/doc-review/DocValidate.js
+++ ui/js/doc-review/DocValidate.js
@@ -58,6 +58,7 @@ var DocValidate = React.createClass({
 		};
 
 
+
 		return <ReviewPage
 			title="Validation Page"
 			getMetadata={API.admin.docValidateMeta}
