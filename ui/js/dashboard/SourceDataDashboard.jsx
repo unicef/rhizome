9f3bfb8557a33958f89881a7cf302d8aa2b55140
John Dingee <dingeej@gmail.com>
2015-08-18 07:59:50 -0400

    going to make source data a dashboard

---
+++ ui/js/dashboard/SourceDataDashboard.jsx
@@ -0,0 +1,34 @@
+'use strict';
+
+var _     = require('lodash');
+var React = require('react');
+
+var Overview  = require('dashboard/nco/Overview.jsx');
+var Breakdown = require('dashboard/nco/Breakdown.jsx');
+
+var SourceDataDashboard = React.createClass({
+  propTypes : {
+    dashboard : React.PropTypes.object.isRequired,
+    data      : React.PropTypes.object.isRequired,
+    region    : React.PropTypes.object.isRequired,
+
+    loading   : React.PropTypes.bool
+  },
+
+  getDefaultProps : function () {
+    return {
+      loading : false
+    };
+  },
+
+  render : function () {
+    var data    = this.props.data;
+    var loading = this.props.loading;
+
+    return (<div>
+      </div>
+    );
+  }
+});
+
+module.exports = SourceDataDashboard;
