7090f87531778903d22ee5188db27690c7da9bf2
John Dingee <dingeej@gmail.com>
2015-08-18 08:29:51 -0400

    render table on dashboard

--- ui/js/dashboard/SourceDataDashboard.jsx
+++ ui/js/dashboard/SourceDataDashboard.jsx
@@ -2,9 +2,19 @@
 
 var _     = require('lodash');
 var React = require('react');
+var api = require('../data/api.js')
 
 var Overview  = require('dashboard/nco/Overview.jsx');
 var Breakdown = require('dashboard/nco/Breakdown.jsx');
+var ReviewPage = require('../doc-review/ReviewPage');
+
+var {
+	Datascope, LocalDatascope,
+	SimpleDataTable, SimpleDataTableColumn,
+	Paginator, SearchBar,
+	FilterPanel, FilterDateRange
+	} = require('react-datascope');
+
 
 var SourceDataDashboard = React.createClass({
   propTypes : {
@@ -25,7 +35,40 @@ var SourceDataDashboard = React.createClass({
     var data    = this.props.data;
     var loading = this.props.loading;
 
-    return (<h1> hi this is the document dashboard </h1>
+    console.log(data)
+    console.log(this.props)
+
+    const fields = {
+    	map_link: {
+    		title: 'Master Object Name',
+    		key: 'id',
+    		renderer: (id) => {
+    				return MapButtonFunction(id)
+    			}
+    	},
+    };
+
+    var doc_id = 72;
+    const fieldNamesOnTable = ['id','content_type','source_object_code','master_object_id'];
+
+    var data_fn = function(){
+      return api.admin.docMap({document:doc_id},null,{'cache-control':'no-cache'})
+    };
+
+    return (<ReviewPage
+  			title="ToMap"
+  			getMetadata={api.admin.docMapMeta}
+  			getData={data_fn}
+  			fields={fields}
+  			>
+  				<Paginator />
+  				<SimpleDataTable>
+  					{fieldNamesOnTable.map(fieldName => {
+  						return <SimpleDataTableColumn name={fieldName} />
+  					})}
+  				</SimpleDataTable>
+  		</ReviewPage>
+
     );
   }
 });
--- ui/js/dashboard/builtin/sourcedata.js
+++ ui/js/dashboard/builtin/sourcedata.js
@@ -3,10 +3,12 @@ module.exports = {
 	'title'             : 'Source Data',
   'builtin': true,
 	'default_office_id' : 1,
-	'charts' : [{
-		'title': 'Inside',
-		'section': 'overview',
-		'indicators': [272],
-    'timeRange': 0
-	}]
+	'charts' : [
+	// 	{
+	// 	'title': 'Inside',
+	// 	'section': 'overview',
+	// 	'indicators': [272],
+  //   'timeRange': 0
+	// }
+]
 };
