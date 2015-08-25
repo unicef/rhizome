ecca242f12be21a75f5cbab203bf9fa33dc5d540
John Dingee <dingeej@gmail.com>
2015-08-25 14:25:53 -0400

    setting up a fuction to render mappign drop down based on this.state.content_type

--- webapp/src/dashboard/sd/MapForm.js
+++ webapp/src/dashboard/sd/MapForm.js
@@ -31,7 +31,6 @@ var MapForm = React.createClass({
 	},
 
   openModal: function() {
-    console.log('opening modal..')
     this.setState({ modalIsOpen: true });
 
     api.get_source_object_map({id: this.props.source_object_map_id})
@@ -39,8 +38,6 @@ var MapForm = React.createClass({
 				source_object_code: response.objects[0].source_object_code,
 				content_type: response.objects[0].content_type
 		}));
-    console.log(this.state)
-
   },
 
   closeModal: function() {
@@ -48,27 +45,33 @@ var MapForm = React.createClass({
 
   },
 
-// postMetaMap : function(source_object_map_id) {
-//   console.log('posting')
-//   console.log(source_object_map_id)
-// },
+postMetaMap : function(source_object_map_id) {
+  console.log('posting')
+  console.log(source_object_map_id)
+},
+
+renderDropDown : function(content_type) {
+  var defaultSelected = {'name':'please map..'}
+
+  if (content_type == 'region') {
+    return <h2> REEEEGIon </h2>;
+   }
+   else if (content_type == 'indicator') {
+     return <h2> indICATOR </h2>;
+   }
+   else if (content_type == 'campaign') {
+      return <h2> CAMPAIIIIGGGNNN </h2>;
+   }
+   else {
+     return <h2> error </h2>;
+   }
+},
+
 
 render : function(){
-  console.log('rendering')
+
   var source_object_map_id = this.props.source_object_map_id
-
-  // var source_object_name = source_object_map_id
-  var content_type = 'region'
-
-  var defaultSelected = {'name':'please map:'}
-
-
-  var dropDown = <RegionTitleMenu
-                         regions={DashboardStore.regions}
-                         selected={defaultSelected}
-                         sendValue={this.postMetaMap} />
-
-  var modalStyle = {width:400, height:300, marginLeft:400}; // rendered as "height:10px"
+  var modalStyle = {width:400, height:300, marginLeft:400}; // rendered as "height:10px"f
 
   return <div><button className="tiny" onClick={this.openModal}> map! </button>
           <Modal
@@ -80,7 +83,7 @@ render : function(){
               <form>
               <h2> Content Type: {this.state.content_type} </h2>
               <h2> Source Code: {this.state.source_object_code} </h2>
-              <h2> {dropDown} </h2>
+              <h2> {this.renderDropDown(this.state.content_type)} </h2>
               </form>
           </Modal></div>
 
