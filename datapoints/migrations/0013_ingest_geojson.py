95d631c066b7b01f2a057fda75969cc258f28a59
John Dingee <dingeej@gmail.com>
2015-09-04 16:06:21 -0400

    looking up region_id from the ge0_json result

--- datapoints/migrations/0013_ingest_geojson.py
+++ datapoints/migrations/0013_ingest_geojson.py
@@ -1,19 +1,21 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
-from django.db import models, migrations
 import os
 import json
 from pprint import pprint
 
-# def ingest_geo(apps, schema_editor):
-def ingest_geo():
+from django.db import models, migrations
+from django.core.exceptions import ObjectDoesNotExist
+
+from datapoints.models import Region, RegionPolygon
+
+def ingest_geo(apps, schema_editor):
 
     GEO_JSON_DIR = '/Users/john/data/geo'
 
     for root, dirs, files in os.walk(GEO_JSON_DIR, topdown=False):
         for name in files:
-            if name.endswith('adm1.geojson'):
+            if name.endswith('adm0.geojson'):
                 file_results = process_geo_json_file((os.path.join(root, name)))
 
 def process_geo_json_file(file_path):
@@ -23,24 +25,29 @@ def process_geo_json_file(file_path):
     with open(file_path) as data_file:
         data = json.load(data_file)
 
-    pprint(data['features'][0]['properties']['ADM0_CODE'])
-    pprint(data.keys())
+    features = data['features']
+    for feature in features:
+        region_result = process_region(feature)
+    # pprint(data.keys())
 
 def process_region(geo_json):
 
-    print '--'
+    print '-'
+
+    try:
+        region_id = Region.objects.get(region_code = geo_json['properties']['ADM0_CODE'] ).id
+        print region_id
+    except ObjectDoesNotExist:
+        print 'DOES NOT EXISTS'
 
 
-# class Migration(migrations.Migration):
-#
-#     dependencies = [
-#         ('datapoints', '0012_afg_pak_regions'),
-#     ]
-#
-#     operations = [
-#         migrations.RunPython(ingest_geo)
-#     ]
 
+class Migration(migrations.Migration):
 
-if __name__ == "__main__":
-    ingest_geo()
+    dependencies = [
+        ('datapoints', '0012_afg_pak_regions'),
+    ]
+
+    operations = [
+        migrations.RunPython(ingest_geo)
+    ]
