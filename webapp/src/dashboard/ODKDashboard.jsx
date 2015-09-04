1282b913fd4cc7a0afc2b82b2b5aaac0bb414254
John Dingee <dingeej@gmail.com>
2015-09-04 12:33:45 -0400

    get donut chart up on odk dashboard

--- webapp/src/dashboard/ODKDashboard.jsx
+++ webapp/src/dashboard/ODKDashboard.jsx
@@ -2,8 +2,7 @@
 
 var _     = require('lodash');
 var React = require('react');
-
-var Overview  = require('dashboard/odk/Overview.jsx');
+var DonutChart = require('component/DonutChart.jsx');
 
 var NCODashboard = React.createClass({
   propTypes : {
@@ -28,6 +27,32 @@ var NCODashboard = React.createClass({
 
     var loading = this.props.loading;
 
+    var options = {
+      innerRadius : 0.6,
+      domain      : _.constant([0, 1]),
+      labelStyle  : {
+        lineHeight : 1
+      }
+    };
+    
+    // var label = ..
+
+    var caregiver_donut = <div className='row'>
+      <div className='small-12 columns'>
+        <h4 style={{ textAlign : 'center' }}>Caregiver Awareness</h4>
+      </div>
+
+      <div className='medium-6 push-3 end columns'>
+        <DonutChart
+          loading={loading}
+          data={data.caregiverAwareness}
+          options={options} />
+      </div>
+
+    </div>
+
+
+
     return (
       <div id='nco-dashboard'>
         <section>
@@ -36,7 +61,12 @@ var NCODashboard = React.createClass({
               <h3>Overview for {this.props.region.name}</h3>
             </div>
           </div>
-          <Overview data={data.overview} loading={loading} />
+          <div className='row'>
+            <div className='medium-6 columns'>
+
+            {caregiver_donut}
+            </div>
+          </div>
         </section>
       </div>
     );
