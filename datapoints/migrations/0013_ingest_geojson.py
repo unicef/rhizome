b68276bee1ba466aead0265f4f11862951440235
John Dingee <dingeej@gmail.com>
2015-09-04 16:37:50 -0400

    insert 3 levels of polygons in 3 countries

--- datapoints/migrations/0013_ingest_geojson.py
+++ datapoints/migrations/0013_ingest_geojson.py
@@ -13,12 +13,20 @@ def ingest_geo(apps, schema_editor):
 
     GEO_JSON_DIR = '/Users/john/data/geo'
 
-    for root, dirs, files in os.walk(GEO_JSON_DIR, topdown=False):
-        for name in files:
-            if name.endswith('adm0.geojson'):
-                file_results = process_geo_json_file((os.path.join(root, name)))
+    for x in range(0,3):
+        print x
+        process_geo_level(x,GEO_JSON_DIR)
 
-def process_geo_json_file(file_path):
+def process_geo_level(lvl,data_dir):
+
+    for root, dirs, files in os.walk(data_dir, topdown=False):
+        for name in files:
+
+            if name.endswith('adm%s.geojson' % lvl):
+                file_results = process_geo_json_file(\
+                    (os.path.join(root, name)),lvl)
+
+def process_geo_json_file(file_path,lvl):
 
     print '==%s==' % file_path
 
@@ -27,12 +35,14 @@ def process_geo_json_file(file_path):
 
     features = data['features']
     for feature in features:
-        process_region(feature)
+        process_region(feature,lvl)
 
-def process_region(geo_json):
+def process_region(geo_json, lvl):
+
+    region_code = geo_json['properties']['ADM%s_CODE' %  lvl]
 
     try:
-        region_id = Region.objects.get(region_code = geo_json['properties']['ADM0_CODE'] ).id
+        region_id = Region.objects.get(region_code = region_code).id
     except ObjectDoesNotExist:
         print geo_json['properties']['ADM0_CODE'] + 'DOES NOT EXISTS'
         return
