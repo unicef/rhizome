614e11e26351193264e71f4c30aac6e0710b8c47
John Dingee <dingeej@gmail.com>
2015-09-19 10:44:07 -0400

    run tests in the build

--- fabfile.py
+++ fabfile.py
@@ -35,9 +35,9 @@ def start_apache():
     run("sudo /etc/init.d/apache2 start")
 
 def run_tests():
-    pass
-    # local("coverage run manage.py test --settings=polio.settings.test")
-    # local("coverage html --omit='venv/*,*migrations/*,*admin*,*manage*,*wsgi*,*__init__*,*test*,*settings*,*url*' -i")
+    
+    local("coverage run manage.py test --settings=polio.settings.test")
+    local("coverage html --omit='venv/*,*migrations/*,*admin*,*manage*,*wsgi*,*__init__*,*test*,*settings*,*url*' -i")
 
 
 # build dependencies
