7db7a08b7dc74204bece4488ea1407b442d9a621
Ruoran Liu <rrliu@thoughtworks.com>
2015-09-25 11:21:05 +0800

    fix rhizome logo rendering

--- webapp/tasks/config/copy.js
+++ webapp/tasks/config/copy.js
@@ -10,7 +10,7 @@ export default {
     },
     {
       src: [
-        `${gulp.config('base.src')}/static/**/*.*`
+        `${gulp.config('base.src')}/assets/**/*.*`
       ],
       dest: `${gulp.config('base.dist')}/static`
     },
