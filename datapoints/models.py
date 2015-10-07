4cb72dadec968d68307c31d4f27fcd6672f80074
John Dingee <dingeej@gmail.com>
2015-10-07 13:49:42 -0400

    add dashboard json field back to fix migratino

--- datapoints/models.py
+++ datapoints/models.py
@@ -512,7 +512,8 @@ class CustomDashboard(models.Model):
     description = models.CharField(max_length=1000)
     owner = models.ForeignKey('auth.User')
     default_office = models.ForeignKey(Office,null=True)
-
+    dashboard_json = JSONField()
+    
     class Meta:
         db_table = 'custom_dashboard'
 
