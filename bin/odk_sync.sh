4ab3b06a1f62d220bf5ea13af8444b7c2dbfe789
John Dingee <dingeej@gmail.com>
2015-10-20 13:25:57 -0400

    first step of odk script is to get the odk forms to process from the api

--- bin/odk_sync.sh
+++ bin/odk_sync.sh
@@ -4,13 +4,14 @@
 source /Users/johndingee_seed/code/UF04/polio/source_data/dev_odk_settings.py
 
 JAR_FILE="/Users/johndingee_seed/code/UF04/polio/source_data/Briefcase_v1.4.4.jar"
-AGGREGATE_URL="https://vcm-ng.appspot.com/"
+AGGREGATE_URL=""
 STORAGE_DIRECTORY="/Users/johndingee_seed/code/UF04/polio/static/odk_source/"
 EXPORT_DIRECTORY="/Users/johndingee_seed/code/UF04/polio/static/odk_source/csv_exports/"
-USERNAME="admin"
-PASSWORD="P@ssword"
+USERNAME=""
+PASSWORD=""
 
-
+RHIZOME_USERNAME=""
+RHIZOME_KEY=""
 
 # create an id that we can use to identify one cron execution
 UUID=$(uuidgen)
@@ -18,26 +19,28 @@ UUID=$(uuidgen)
 
 API_ROOT='http://localhost:8000'
 
+
+
 # ## STARTING JAR FILE PROCESS ##
-wget -O/dev/null $API_ROOT/api/v1/etl/?task=start_odk_jar\&username=$POLIO_USERNAME\&password=$POLIO_KEY\&cron_guid=$UUID
-wget -O/dev/null $API_ROOT/api/v1/etl/?task=start_odk_jar\&cron_guid=$UUID
+wget -O/dev/null $API_ROOT/api/v1/doc_detail/?doc_detail_type=odk_form_name\&username=$RHIZOME_USERNAME\&api_key=$RHIZOME_KEY\&cron_guid=$UUID
+# wget -O/dev/null $API_ROOT/api/v1/etl/?task=start_odk_jar\&cron_guid=$UUID
 
 
-java -jar $JAR_FILE \
---form_id $FORM_ID \
---export_filename $FORM_ID + '.csv' \
---aggregate_url $AGGREGATE_URL \
---storage_directory $STORAGE_DIRECTORY \
---export_directory $EXPORT_DIRECTORY \
---odk_username $USERNAME \
---odk_password $PASSWORD \
---overwrite_csv_export \
---exclude_media_export \
+# java -jar $JAR_FILE \
+# --form_id $FORM_ID \
+# --export_filename $FORM_ID + '.csv' \
+# --aggregate_url $AGGREGATE_URL \
+# --storage_directory $STORAGE_DIRECTORY \
+# --export_directory $EXPORT_DIRECTORY \
+# --odk_username $USERNAME \
+# --odk_password $PASSWORD \
+# --overwrite_csv_export \
+# --exclude_media_export \
 
 # JAR_PID=$!
 
 echo DONE WITH THE JAR FILE PROCESS ##
-wget -O/dev/null $API_ROOT/api/v1/etl/?task=finish_odk_jar\&username=$POLIO_USERNAME\&password=$POLIO_KEY\&cron_guid=$UUID
+# wget -O/dev/null $API_ROOT/api/v1/etl/?task=finish_odk_jar\&username=$POLIO_USERNAME\&password=$POLIO_KEY\&cron_guid=$UUID
 
 sleep 2
 # wget -O/dev/null $API_ROOT/api/v1/etl/?task=odk_refresh_vcm_summary_work_table\&username=$POLIO_USERNAME\&password=$POLIO_KEY\&cron_guid=$UUID
