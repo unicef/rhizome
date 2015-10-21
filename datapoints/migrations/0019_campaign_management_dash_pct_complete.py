52c5461fa3c463280628c4ee67b7a41f0038486d
John Dingee <dingeej@gmail.com>
2015-10-21 12:39:24 -0400

    remove CampaignAbstracted model and put the pct_complete attribute in the campaign model directly

---
+++ datapoints/migrations/0019_campaign_management_dash_pct_complete.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('datapoints', '0018_delete_campaignabstracted'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='campaign',
+            name='management_dash_pct_complete',
+            field=models.FloatField(default=0),
+            preserve_default=False,
+        ),
+    ]
