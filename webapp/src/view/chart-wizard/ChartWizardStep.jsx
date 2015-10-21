46e86771c915bcdc8b6a40ce655386ec55e9506f
Ruoran Liu <rrliu@thoughtworks.com>
2015-10-21 14:21:41 -0400

    add chart wizard step lists

---
+++ webapp/src/view/chart-wizard/ChartWizardStep.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+let ChartWizardStep = React.createClass({
+  getInitialState() {
+    return {
+      title: this.props.title
+    }
+  },
+
+  activeStep() {
+    this.props.onToggle(this.props.refer)
+  },
+
+  render() {
+    let isActive = this.props.active == this.props.refer ? 'active' : ''
+    return (
+      <li className={'chart-wizard__step ' + isActive} onClick={this.activeStep}>
+        <h2>{this.state.title}</h2>
+        <div className='chart-wizard__expandable'>
+          {this.props.children}
+        </div>
+      </li>
+    )
+  }
+})
+
+export default ChartWizardStep
