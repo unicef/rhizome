8a84ef15810578f6f0c49d1fbc2e01b829d6aa6f
Sichu <zhangsichu@gmail.com>
2015-10-22 21:54:59 +0800

    modified the api test case file. separate the two api test module.

--- datapoints/tests/test_api_Indicator.py
+++ datapoints/tests/test_api_Indicator.py
@@ -6,10 +6,8 @@ from django.contrib.auth.models import User
 from datapoints.models import Indicator, IndicatorTag, CustomDashboard, CustomChart, CalculatedIndicatorComponent, \
     IndicatorToTag
 from source_data.models import Document
-
 from tastypie.test import TestApiClient
 import json
-
 
 class IndicatorResourceTest(ResourceTestCase):
     def setUp(self):
@@ -33,45 +31,6 @@ class IndicatorResourceTest(ResourceTestCase):
     def test_auth_valid(self):
         resp = self.api_client.get('/api/v1/', format='json')
         self.assertValidJSONResponse(resp)
-
-    def test_dashboard_post(self):
-        post_data = {'title': 'this is the title'}
-
-        resp = self.api_client.post('/api/v1/custom_dashboard/', format='json', \
-                                    data=post_data, authentication=self.get_credentials())
-
-        response_data = self.deserialize(resp)
-
-        self.assertHttpCreated(resp)
-        self.assertEqual(post_data['title'], response_data['title'])
-
-    def test_chart_create(self):
-        dash = CustomDashboard.objects.create(title='test', owner_id=self.user.id)
-
-        post_data = {'dashboard_id': dash.id, 'chart_json': json.dumps({'foo': 'bar'})}
-
-        resp = self.api_client.post('/api/v1/custom_chart/', format='json', \
-                                    data=post_data, authentication=self.get_credentials())
-
-        response_data = self.deserialize(resp)
-
-        self.assertHttpCreated(resp)
-        self.assertEqual(post_data['dashboard_id'], response_data['dashboard_id'])
-        self.assertEqual(post_data['chart_json'], response_data['chart_json'])
-
-    # def test_chart_delete(self):
-    #     d = CustomDashboard.objects.create(owner_id = self.user.id,title='test')
-    #     c1 = CustomChart.objects.create(dashboard_id = d.id,chart_json={'hello':'world'})
-    #     c2 = CustomChart.objects.create(dashboard_id = d.id,chart_json={'goodnight':'moon'})
-    #
-    #     delete_url = '/api/v1/custom_chart/'
-    #     params = {'id':c1.id}
-    #
-    #
-    #     self.api_client.delete(delete_url,format='json',data=params,
-    #         authentication=self.get_credentials())
-    #
-    #     self.assertEqual(CustomChart.objects.count(), 1)
 
     def test_create_calculation(self):
 
@@ -104,6 +63,9 @@ class IndicatorResourceTest(ResourceTestCase):
         self.assertEqual(indicator_2.id, response_data['component_id'])
         self.assertEqual(indicator_calculation.calculation, response_data['typeInfo'])
 
+    def test_remove_calculation(self):
+        pass
+
     def test_create_tag(self):
         Indicator.objects.create(short_name='Test Indicator',\
                                  name= 'Test Indicator for the Tag',\
@@ -125,6 +87,10 @@ class IndicatorResourceTest(ResourceTestCase):
 
         self.assertHttpCreated(resp)
         self.assertEqual(indicator_tag.id, response_data['id'])
-        self.assertEqual(post_data['indicator_id'], response_data['indicator_id'])
-        self.assertEqual(post_data['indicator_tag_id'], response_data['indicator_tag_id'])
+        self.assertEqual(indicatior.id, response_data['indicator_id'])
+        self.assertEqual(tag.id, response_data['indicator_tag_id'])
+
+    def test_remove_tag(self):
+        pass
+
 
---
+++ datapoints/tests/test_api_chart.py
@@ -0,0 +1,66 @@
+from django.test import TestCase
+from tastypie.test import ResourceTestCase
+from tastypie.models import ApiKey
+from django.contrib.auth.models import User
+from datapoints.models import CustomDashboard, CustomChart
+from source_data.models import Document
+from tastypie.test import TestApiClient
+
+import json
+
+
+class ChartResourceTest(ResourceTestCase):
+    def setUp(self):
+        super(ChartResourceTest, self).setUp()
+
+        # Create a user.
+        self.username = 'john'
+        self.password = 'pass'
+        self.user = User.objects.create_user(self.username,
+                                             'john@john.com', self.password)
+
+        self.get_credentials()
+
+        # create their api_key
+
+    def get_credentials(self):
+        result = self.api_client.client.login(username=self.username,
+                                              password=self.password)
+        return result
+
+    def test_dashboard_post(self):
+        post_data = {'title': 'this is the title'}
+
+        resp = self.api_client.post('/api/v1/custom_dashboard/', format='json', \
+                                    data=post_data, authentication=self.get_credentials())
+
+        response_data = self.deserialize(resp)
+
+        self.assertHttpCreated(resp)
+        self.assertEqual(post_data['title'], response_data['title'])
+
+    def test_chart_create(self):
+        dash = CustomDashboard.objects.create(title='test', owner_id=self.user.id)
+
+        post_data = {'dashboard_id': dash.id, 'chart_json': json.dumps({'foo': 'bar'})}
+
+        resp = self.api_client.post('/api/v1/custom_chart/', format='json', \
+                                    data=post_data, authentication=self.get_credentials())
+
+        response_data = self.deserialize(resp)
+
+        self.assertHttpCreated(resp)
+        self.assertEqual(post_data['dashboard_id'], response_data['dashboard_id'])
+        self.assertEqual(post_data['chart_json'], response_data['chart_json'])
+
+    def test_chart_delete(self):
+        d = CustomDashboard.objects.create(owner_id=self.user.id, title='test')
+        c1 = CustomChart.objects.create(dashboard_id=d.id, chart_json={'hello': 'world'})
+        c2 = CustomChart.objects.create(dashboard_id=d.id, chart_json={'goodnight': 'moon'})
+
+        delete_url = '/api/v1/custom_chart/?id=' + str(c1.id)
+
+        self.api_client.delete(delete_url, format='json', data={},
+                               authentication=self.get_credentials())
+
+        self.assertEqual(CustomChart.objects.count(), 1)
