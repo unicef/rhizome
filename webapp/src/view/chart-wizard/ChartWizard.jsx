4b77e8b0c69a669d1864c98251735003d65b7ad5
Ruoran Liu <rrliu@thoughtworks.com>
2015-10-22 09:52:06 -0400

    change wizard screen to static preview

--- webapp/src/view/chart-wizard/ChartWizard.jsx
+++ webapp/src/view/chart-wizard/ChartWizard.jsx
@@ -2,8 +2,7 @@ import React from 'react'
 
 import ChartWizardStep from './ChartWizardStep.jsx'
 import ChartWizardStepList from './ChartWizardStepList.jsx'
-import ChartWizardScreen from './ChartWizardScreen.jsx'
-import ChartWizardScreenList from './ChartWizardScreenList.jsx'
+import PreviewScreen from './PreviewScreen.jsx'
 
 let ChartWizard = React.createClass({
   propTypes: {
@@ -71,29 +70,7 @@ let ChartWizard = React.createClass({
             {previewStep}
           </ChartWizardStep>
         </ChartWizardStepList>
-        <ChartWizardScreenList active={this.state.refer}>
-          <ChartWizardScreen referTo='country'>
-            <h1>Select country page</h1>
-          </ChartWizardScreen>
-          <ChartWizardScreen referTo='indicator'>
-            <h1>Select indicator page</h1>
-          </ChartWizardScreen>
-          <ChartWizardScreen referTo='location'>
-            <h1>Select location page</h1>
-          </ChartWizardScreen>
-          <ChartWizardScreen referTo='campaign'>
-            <h1>Select campaign page</h1>
-          </ChartWizardScreen>
-          <ChartWizardScreen referTo='chart-type'>
-            <h1>Select Chart type page</h1>
-          </ChartWizardScreen>
-          <ChartWizardScreen referTo='style'>
-            <h1>Customise style page</h1>
-          </ChartWizardScreen>
-          <ChartWizardScreen referTo='preview'>
-            <h1>Preview page</h1>
-          </ChartWizardScreen>
-        </ChartWizardScreenList>
+        <PreviewScreen />
         <a className='chart-wizard__cancel' href="#" onClick={this.props.cancel}>Cancel without saving chart</a>
       </div>
     )
---
+++ webapp/src/view/chart-wizard/PreviewScreen.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+let PreviewScreen = React.createClass({
+  render() {
+    return (
+      <div className='preview-screen'>
+        <h1>Preview</h1>
+        <p>Chart preview will be generated after country, indicator, campaign, and chart type are selected.</p>
+      </div>
+    )
+  }
+})
+
+export default PreviewScreen
