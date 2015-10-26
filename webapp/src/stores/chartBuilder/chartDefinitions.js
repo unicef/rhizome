623d49403e5d67d2bac286532a7338f3b625d53a
Ruoran Liu <rrliu@thoughtworks.com>
2015-10-26 16:32:03 -0400

    add group-by radio

--- webapp/src/stores/chartBuilder/chartDefinitions.js
+++ webapp/src/stores/chartBuilder/chartDefinitions.js
@@ -1,8 +1,15 @@
-module.exports = [
-  {name:"LineChart",groupBy:true,chooseAxis:false,timeRadios:["allTime","pastYear","3Months"]},
-	{name:"PieChart",groupBy:false,chooseAxis:false,timeRadios:["current"]},
-	{name:"ChoroplethMap",groupBy:false,chooseAxis:false,timeRadios:["current"]},
-	{name:"ColumnChart",groupBy:true,chooseAxis:false,timeRadios:["pastYear","3Months","current"]},
-	{name:"ScatterChart",groupBy:false,chooseAxis:true,timeRadios:["current"]},
-	{name:"BarChart",groupBy:true,chooseAxis:false,timeRadios:["current"]}
-];
+export default {
+  charts: [
+    {name: "LineChart", groupBy: true, chooseAxis: false, timeRadios: ["allTime","pastYear","3Months"]},
+    {name: "PieChart", groupBy: false, chooseAxis: false, timeRadios :["current"]},
+    {name: "ChoroplethMap" ,groupBy:false, chooseAxis:false, timeRadios: ["current"]},
+    {name: "ColumnChart", groupBy: true, chooseAxis: false, timeRadios: ["pastYear","3Months","current"]},
+    {name: "ScatterChart", groupBy: false, chooseAxis: true, timeRadios: ["current"]},
+    {name: "BarChart", groupBy: true, chooseAxis: false, timeRadios: ["current"]}
+  ],
+  groups: [
+    {value: "indicator", title: "Indicators"},
+    {value: "location", title: "Locations"}
+  ]
+
+}
