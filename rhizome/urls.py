fa27bef9ac23318b6f82205cab235d40fa900251
Qiu Juntao <juntao.qiu@gmail.com>
2015-10-26 19:18:23 +1100

    add debug toolbar

--- rhizome/urls.py
+++ rhizome/urls.py
@@ -82,7 +82,11 @@ urlpatterns = patterns('',
 
     ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
+from django.conf import settings
+from django.conf.urls import include, patterns, url
+
 if settings.DEBUG:
+    import debug_toolbar
     urlpatterns += patterns('',
-        url(r'^debug/', debug),
+        url(r'^debug/', include(debug_toolbar.urls)),
     )
