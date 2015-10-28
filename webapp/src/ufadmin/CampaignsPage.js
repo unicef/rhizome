992cf4aff5bd6eb9dee386ff88dd6ed4aaee68ad
Sichu <zhangsichu@gmail.com>
2015-10-28 23:29:39 +0800

    just finish the html inject logic to render the react component. need polish the css style. and check the post back value, but the react component is rendered to the page.

--- webapp/src/ufadmin/CampaignsPage.js
+++ webapp/src/ufadmin/CampaignsPage.js
@@ -1,13 +1,12 @@
 var React = require('react');
 var _ = require('lodash');
-var DateRangePicker = require('component/DateTimePicker.jsx');
+var DatePicker = require('component/DatePicker.jsx');
 var api = require('../data/api');
 
 var CampaignsPage = (function () {
   return {
     render: function (start_date_dom_id, end_date_dom_id) {
       var createControl = function (id) {
-        var dateRangePickerProps = {};
         var input_dom = document.getElementById(id);
         input_dom.type = "hidden";
 
@@ -16,7 +15,17 @@ var CampaignsPage = (function () {
         input_dom.id = id + "_date_picker";
         parent_dom.appendChild(input_dom);
 
-        React.render(React.createElement(DateRangePicker, dateRangePickerProps), input_dom);
+        var sendValue = function (date, dateStr) {
+          console.log("date", date);
+          console.log("string", dateStr);
+        }
+
+        var dateRangePickerProps = {
+          date: new Date(),
+          sendValue: sendValue
+        };
+
+        React.render(React.createElement(DatePicker, dateRangePickerProps), input_dom);
       };
 
       createControl(start_date_dom_id);
