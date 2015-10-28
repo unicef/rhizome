3d40067b87e1000066dd7d7781f4b968ebfd758d
Sichu <zhangsichu@gmail.com>
2015-10-29 03:34:41 +0800

    code update for the data picker.

--- webapp/src/ufadmin/CampaignsPage.js
+++ webapp/src/ufadmin/CampaignsPage.js
@@ -15,23 +15,23 @@ var CampaignsPage = (function () {
         var input_dom = document.createElement("div");
         input_dom.id = id + "_date_picker";
         parent_dom.appendChild(input_dom);
-        input_dom.className ="admin_campaigns_date_picker"
+        input_dom.className = "admin_campaigns_date_picker";
 
         var sendValue = function (date, dateStr) {
           original_dom.value = dateStr;
-        }
+        };
 
         var date_value = original_dom.value;
         var set_date_value = null;
         if (date_value != null && date_value != '')
           set_date_value = moment(date_value).toDate();
 
-        var dateRangePickerProps = {
+        var datePickerProps = {
           date: set_date_value,
           sendValue: sendValue
         };
 
-        React.render(React.createElement(DatePicker, dateRangePickerProps), input_dom);
+        React.render(React.createElement(DatePicker, datePickerProps), input_dom);
       };
 
       createControl(start_date_dom_id);
