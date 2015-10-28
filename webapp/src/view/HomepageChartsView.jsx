6ab78f3d1cbe4dec9e88b6e979d92383fe418aca
Qiu Juntao <juntao.qiu@gmail.com>
2015-10-28 20:03:50 +1100

    code clear up

--- webapp/src/view/HomepageChartsView.jsx
+++ webapp/src/view/HomepageChartsView.jsx
@@ -10,11 +10,6 @@ var api = require('data/api');
 var dashboardInit = require('data/dashboardInit');
 var builtins = require('dashboard/builtin');
 
-var TitleMenu = require('component/TitleMenu.jsx');
-var RegionTitleMenu = require('component/RegionTitleMenu');
-var CampaignTitleMenu = require('component/CampaignTitleMenu.jsx');
-var MenuItem = require('component/MenuItem.jsx');
-
 var CustomDashboard = require('dashboard/CustomDashboard.jsx');
 
 var DashboardStore = require('stores/DashboardStore');
@@ -45,7 +40,7 @@ var HomepageChartsView = React.createClass({
         };
     },
 
-    getallDashboards: function() {
+    getAllDashboards: function() {
         var self = this;
         api.get_dashboard().then(function(response) {
             var customDashboards = _(response.objects).sortBy('title').value();
@@ -56,11 +51,7 @@ var HomepageChartsView = React.createClass({
     },
 
     componentWillMount: function () {
-        this.getallDashboards();
-        //page('/datapoints/:dashboard/:location/:year/:month/:doc_tab/:doc_id', this._showSourceData);
-        //page('/datapoints/:dashboard/:location/:year/:month', this._show);
-        //page('/', this._showDefault);
-        //this._showDefault();
+        this.getAllDashboards();
         AppActions.init();
     },
 
@@ -82,12 +73,7 @@ var HomepageChartsView = React.createClass({
     },
 
     componentDidMount: function () {
-        console.log("component Did Mount");
-        // Reflux.ListenerMixin will unmount listeners
         this.listenTo(DashboardStore, this._onDashboardChange);
-        this.listenTo(NavigationStore, this._onNavigationChange);
-
-        this.listenTo(DashboardActions.navigate, this._navigate);
 
         this.listenTo(IndicatorStore, () => this.forceUpdate());
         this.listenTo(GeoStore, () => this.forceUpdate());
@@ -95,14 +81,12 @@ var HomepageChartsView = React.createClass({
 
     _onDashboardChange: function (state) {
         var fetchData = this.state.loaded;
-        console.log(state);
 
         this.setState(state);
 
 
         if (this.state.loaded) {
             var q = DashboardStore.getQueries();
-            console.log(q);
 
             if (_.isEmpty(q)) {
                 DataActions.clear();
@@ -118,44 +102,6 @@ var HomepageChartsView = React.createClass({
                 click: false
             });
         }
-    },
-
-    _onNavigationChange: function (nav) {
-        if (NavigationStore.loaded && DashboardStore.loaded) {
-            page({
-                click: false
-            });
-        }
-    },
-
-    _setCampaign: function (id) {
-        var campaign = _.find(this.state.campaigns, c => c.id === id);
-
-        if (!campaign) {
-            return;
-        }
-
-        this._navigate({
-            campaign: moment(campaign.start_date, 'YYYY-MM-DD').format('YYYY/MM')
-        });
-    },
-
-    _setlocation: function (id) {
-        var location = _.find(this.state.locations, r => r.id === id);
-
-        if (!location) {
-            return;
-        }
-
-        this._navigate({
-            location: location.name
-        });
-    },
-
-    _setDashboard: function (slug) {
-        this._navigate({
-            dashboard: slug
-        });
     },
 
     _getDashboard: function (slug) {
@@ -179,17 +125,6 @@ var HomepageChartsView = React.createClass({
         }
     },
 
-    _navigate: function (params) {
-        var slug = _.get(params, 'dashboard', _.kebabCase(this.state.dashboard.title));
-        var location = _.get(params, 'location', this.state.location.name);
-        var campaign = _.get(params, 'campaign', moment(this.state.campaign.start_date, 'YYYY-MM-DD').format('YYYY/MM'));
-        if (_.isNumber(location)) {
-            location = _.find(this.state.locations, r => r.id === location).name;
-        }
-
-        page('/datapoints/' + [slug, location, campaign].join('/'));
-    },
-
     _showDefault: function () {
         this._getDashboard('management-dashboard').then(dashboard => {
             DashboardActions.setDashboard({
@@ -198,33 +133,6 @@ var HomepageChartsView = React.createClass({
         })
     },
 
-    _show: function (ctx) {
-        NavigationStore.getDashboard(ctx.params.dashboard).then(dashboard => {
-            DashboardActions.setDashboard({
-                dashboard,
-                location: ctx.params.location,
-                date: [ctx.params.year, ctx.params.month].join('-')
-            });
-        })
-    },
-
-    _showSourceData: function (ctx) {
-        NavigationStore.getDashboard(ctx.params.dashboard).then(dashboard => {
-            var doc_tab = ctx.params.doc_tab;
-
-            this.setState({
-                doc_id: ctx.params.doc_id,
-                doc_tab: doc_tab
-            });
-
-            DashboardActions.setDashboard({
-                dashboard,
-                location: ctx.params.location,
-                date: [ctx.params.year, ctx.params.month].join('-')
-            });
-        })
-
-    },
 
     render: function () {
         if (!(this.state.loaded && this.state.dashboard)) {
