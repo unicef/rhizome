f13a40d55bc3cfef2e78345ad33ef67695681dab
Sichu <zhangsichu@gmail.com>
2015-10-30 03:16:56 +0800

    fix the 411 After Edit User return to user index.

--- datapoints/views.py
+++ datapoints/views.py
@@ -13,7 +13,7 @@ from django.core.exceptions import ObjectDoesNotExist
 from django.core import serializers
 from django.views import generic
 from django.views.decorators.cache import cache_control as django_cache_control
-from django.contrib.auth.models import User,Group
+from django.contrib.auth.models import User, Group
 from django.contrib.auth.decorators import login_required
 
 from django.template import RequestContext
@@ -27,44 +27,44 @@ from datapoints.forms import *
 from datapoints import cache_tasks
 from datapoints.mixins import PermissionRequiredMixin
 
+
 class IndexView(generic.ListView):
     paginate_by = 20
 
     def get_queryset(self):
         return self.model.objects.order_by('-created_at')
 
-    ###################
-    ###################
-    ### DATA POINTS ###
-    ###################
-    ###################
+        ###################
+        ###################
+        ### DATA POINTS ###
+        ###################
+        ###################
+
 
 class DataPointIndexView(IndexView):
-
-    model=DataPoint
+    model = DataPoint
     template_name = 'datapoints/index.html'
     context_object_name = 'top_datapoints'
 
 
 def data_entry(request):
-
     return render_to_response('data-entry/index.html',
-        context_instance=RequestContext(request))
+                              context_instance=RequestContext(request))
+
 
 def dashboard_list(request):
-
     return render_to_response('dashboard-builder/list.html',
-        context_instance=RequestContext(request))
+                              context_instance=RequestContext(request))
 
-def dashboard_builder(request,dashboard_id=None):
 
-    return render_to_response('dashboard-builder/index.html', {'dashboard_id': dashboard_id },
-        context_instance=RequestContext(request))
+def dashboard_builder(request, dashboard_id=None):
+    return render_to_response('dashboard-builder/index.html', {'dashboard_id': dashboard_id},
+                              context_instance=RequestContext(request))
 
-def chart_builder(request,dashboard_id):
 
-    return render_to_response('dashboard-builder/chart_builder.html', {'dashboard_id': dashboard_id },
-        context_instance=RequestContext(request))
+def chart_builder(request, dashboard_id):
+    return render_to_response('dashboard-builder/chart_builder.html', {'dashboard_id': dashboard_id},
+                              context_instance=RequestContext(request))
 
 
 class DashBoardView(IndexView):
@@ -77,21 +77,19 @@ class DashBoardView(IndexView):
         return DataPoint.objects.all()[:1]
 
 
-    #################
-    ### CAMPAIGNS ###
-    #################
+        #################
+        ### CAMPAIGNS ###
+        #################
 
 
-class CampaignCreateView(PermissionRequiredMixin,generic.CreateView):
-
+class CampaignCreateView(PermissionRequiredMixin, generic.CreateView):
     model = Campaign
     success_url = '/ufadmin/campaigns'
     template_name = 'campaigns/create.html'
-    fields = ['office','campaign_type','start_date','end_date']
+    fields = ['office', 'campaign_type', 'start_date', 'end_date']
 
 
-class CampaignUpdateView(PermissionRequiredMixin,generic.UpdateView):
-
+class CampaignUpdateView(PermissionRequiredMixin, generic.UpdateView):
     model = Campaign
     success_url = '/ufadmin/campaigns'
     template_name = 'campaigns/update.html'
@@ -102,13 +100,13 @@ class CampaignUpdateView(PermissionRequiredMixin,generic.UpdateView):
     ### locationS ###
     ###############
 
-class LocationCreateView(PermissionRequiredMixin,generic.CreateView):
 
+class LocationCreateView(PermissionRequiredMixin, generic.CreateView):
     model = Location
-    template_name='regions/create.html'
+    template_name = 'regions/create.html'
     permission_required = 'datapoints.add_location'
     form_class = LocationForm
-    success_url= '/ufadmin/locations'
+    success_url = '/ufadmin/locations'
 
     def form_valid(self, form):
         # this inserts into the changed_by field with  the user who made the insert
@@ -120,8 +118,7 @@ class LocationCreateView(PermissionRequiredMixin,generic.CreateView):
         return HttpResponseRedirect(self.success_url)
 
 
-class LocationUpdateView(PermissionRequiredMixin,generic.UpdateView):
-
+class LocationUpdateView(PermissionRequiredMixin, generic.UpdateView):
     model = Location
     success_url = '/ufadmin/locations'
     template_name = 'regions/update.html'
@@ -134,23 +131,21 @@ class LocationUpdateView(PermissionRequiredMixin,generic.UpdateView):
     ##############################
     ##############################
 
-def manage_data_refresh(request):
 
-    cache_jobs = CacheJob.objects.all().\
+def manage_data_refresh(request):
+    cache_jobs = CacheJob.objects.all(). \
         exclude(response_msg='NOTHING_TO_PROCESS').order_by('-id')
 
-    return render_to_response('manage_data_refresh.html',{'cache_jobs':cache_jobs},
-    context_instance=RequestContext(request))
+    return render_to_response('manage_data_refresh.html', {'cache_jobs': cache_jobs},
+                              context_instance=RequestContext(request))
 
 
 def refresh_cache(request):
-
     cr = cache_tasks.CacheRefresh()
     return HttpResponseRedirect(reverse('datapoints:manage_data_refresh'))
 
 
-def parse_url_args(request,keys):
-
+def parse_url_args(request, keys):
     request_meta = {}
 
     for k in keys:
@@ -178,109 +173,102 @@ def refresh_metadata(request):
 
 
 class GroupCreateView(PermissionRequiredMixin, generic.CreateView):
-
     model = Group
     template_name = 'group_create.html'
 
     form_class = GroupForm
 
     def form_valid(self, form):
-
         new_group = form.save()
 
         return HttpResponseRedirect(reverse('datapoints:create_group', \
-            kwargs={'pk':new_group.id}))
+                                            kwargs={'pk': new_group.id}))
 
 
-class GroupEditView(PermissionRequiredMixin,generic.UpdateView):
-
+class GroupEditView(PermissionRequiredMixin, generic.UpdateView):
     model = Group
     template_name = 'group_update.html'
 
     form_class = GroupForm
 
     def get_success_url(self):
-
         requested_group_id = self.get_object().id
 
-        return reverse_lazy('datapoints:group_update',kwargs={'pk':
-            requested_group_id})
+        return reverse_lazy('datapoints:group_update', kwargs={'pk':
+                                                                   requested_group_id})
 
     def get_context_data(self, **kwargs):
-
         context = super(GroupEditView, self).get_context_data(**kwargs)
         group_obj = self.get_object()
         context['group_id'] = group_obj.id
 
         return context
 
-class UserCreateView(PermissionRequiredMixin,generic.CreateView):
 
+class UserCreateView(PermissionRequiredMixin, generic.CreateView):
     model = User
     template_name = 'user_create.html'
     form_class = UserCreateForm
-    success_url= '/ufadmin/users'
+    success_url = '/ufadmin/users'
 
     def form_valid(self, form):
         new_user = form.save()
         return HttpResponseRedirect(self.success_url)
 
 
-class UserEditView(PermissionRequiredMixin,generic.UpdateView):
-
+class UserEditView(PermissionRequiredMixin, generic.UpdateView):
     model = User
     template_name = 'user_edit.html'
     form_class = UserEditForm
 
     def get_success_url(self):
-
         requested_user_id = self.get_object().id
 
-        return reverse_lazy('datapoints:user_update',kwargs={'pk':
-            requested_user_id})
+        return reverse_lazy('datapoints:user_update', kwargs={'pk':
+                                                                  requested_user_id})
 
     def get_context_data(self, **kwargs):
-
         context = super(UserEditView, self).get_context_data(**kwargs)
         user_obj = self.get_object()
         context['user_id'] = user_obj.id
 
         return context
 
-class IndicatorCreateView(PermissionRequiredMixin,generic.CreateView):
+    def form_valid(self, form):
+        form.save()
+        return HttpResponseRedirect('/ufadmin/users')
 
+
+class IndicatorCreateView(PermissionRequiredMixin, generic.CreateView):
     model = User
     template_name = 'user_create.html'
     form_class = IndicatorForm
 
     def form_valid(self, form):
-
         new_indicator = form.save()
 
         return HttpResponseRedirect(reverse('datapoints:update_indicator', \
-            kwargs={'pk':new_indicator.id}))
+                                            kwargs={'pk': new_indicator.id}))
 
 
-class IndicatorEditView(PermissionRequiredMixin,generic.UpdateView):
-
+class IndicatorEditView(PermissionRequiredMixin, generic.UpdateView):
     model = Indicator
     template_name = 'indicators/upsert.html'
     form_class = IndicatorForm
 
     def get_success_url(self):
-
         new_indicator_id = self.get_object().id
 
-        return reverse_lazy('datapoints:update_indicator',kwargs={'pk':
-            new_indicator_id})
+        return reverse_lazy('datapoints:update_indicator', kwargs={'pk':
+                                                                       new_indicator_id})
 
     def get_context_data(self, **kwargs):
-
         context = super(IndicatorEditView, self).get_context_data(**kwargs)
         indicator_obj = self.get_object()
         context['pk'] = indicator_obj.id
 
         return context
+
 
 def html_decorator(func):
     """
