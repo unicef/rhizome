e96e79cfa10657d35a6407fbdf81b38404ceb4c6
Ruiwei Huang <rwhuang@CNrwhuang.local>
2015-11-02 11:42:45 +0800

    [#419]fix the bug that redesign page does not call datapoints api

--- webapp/src/component/DashboardNav.jsx
+++ webapp/src/component/DashboardNav.jsx
@@ -2,39 +2,42 @@
 
 var _ = require('lodash');
 var React = require('react');
+var Reflux = require('reflux');
+
+var NavMenu = require('component/NavMenu.jsx');
+var NavMenuItem = require('component/NavMenuItem.jsx');
+var NavigationStore = require('stores/NavigationStore');
+var MenuControl = require('mixin/MenuControl');
 
 module.exports = React.createClass({
+  mixins: [
+    Reflux.connect(NavigationStore),
+    require('mixin/MenuControl')
+  ],
 
-  render: function () {
+  render : function () {
+    var dashboards = NavMenuItem.fromArray(_(this.state.dashboards)
+      .filter(d => d.builtin || d.owned_by_current_user)
+      .map(function(d) {
+        return _.assign({
+          key: 'dashboard-nav-' + d.id
+        }, d);
+      })
+      .value()
+    );
 
     return (
-      <ul className="dashboards-nav">
-        <li className="medium-6 columns">
-          <a href="#">View My Dashboards</a>
-          <ul className="dashboard-menu">
-            <li>
-              <a href="/datapoints/management-dashboard">Management Dashboard</a>
-            </li>
-            <li>
-              <a href="/datapoints/district-dashboard">District Dashboard</a>
-            </li>
-            <li>
-              <a href="/datapoints/nga-campaign-monitoring">NGA Campaign Monitoring</a>
-            </li>
-            <li>
-              <a href="/datapoints/odk-dashboard">ODK Dashboard</a>
-            </li>
-            <li>
-                <a href="/datapoints/dashboards">See all custom dashboards</a>
-            </li>
-          </ul>
-        </li>
-        <li className="medium-6 columns">
-          <a href="/datapoints/dashboards/edit">Create a dashboard</a>
-        </li>
-      </ul>
-
+        <ul className="dashboards-nav">
+          <li className="medium-6 columns">
+          <a onClick={this._toggleMenu} tabIndex='-1'>View My Dashboards</a>
+            <ul className="dashboard-menu">
+              {dashboards}
+            </ul>
+          </li>
+          <li className="medium-6 columns">
+            <a href="/datapoints/dashboards/edit">Create a dashboard</a>
+          </li>
+        </ul>
     );
   }
-
 });
