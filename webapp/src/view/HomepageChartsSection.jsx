dd3c0b68106d21c0a1599bd8600beae6204d3fc1
Qiu Juntao <juntao.qiu@gmail.com>
2015-11-04 19:58:50 +1100

    add link to buttons on homepage

--- webapp/src/view/HomepageChartsSection.jsx
+++ webapp/src/view/HomepageChartsSection.jsx
@@ -4,23 +4,54 @@ var React = require('react');
 var HomepageCharts = require('dashboard/homepage/HomepageCharts.jsx');
 
 var HomepageChartsSection = React.createClass({
+    getManagementDashboardUrl: function() {
+      var [year, month] = this.props.date.split('-');
+      return `/datapoints/management-dashboard/${this.props.location}/${year}/${month}`;
+    },
+
+    getDistrictSummaryUrl: function() {
+      var [year, month] = this.props.date.split('-');
+      return `/datapoints/district-dashboard/${this.props.location}/${year}/${month}`;
+    },
+
+    getNGACampaignMonitoringUrl: function() {
+      var [year, month] = this.props.date.split('-');
+      return `/datapoints/nga-campaign-monitoring/${this.props.location}/${year}/${month}`;
+    },
+
+
     render: function () {
         var dashboard = React.createElement(HomepageCharts, this.props.data);
         var chartId = `${this.props.location.toLowerCase()}-chart`;
 
         var controls;
         if(this.props.location === 'Nigeria') {
-            controls = <div className="chart-button-group">
-                            <div className="chart-button"><span>Country overview</span></div>
-                            <div className="chart-button"><span>District summary</span></div>
-                            <div className="chart-button"><span>NGA Campaign Monitoring</span></div>
-                        </div>
+            controls =
+              <div className="chart-button-group">
+                <a href={this.getManagementDashboardUrl()}>
+                  <div className="chart-button"><span>Country overview</span></div>
+                </a>
+
+                <a href={this.getDistrictSummaryUrl()}>
+                  <div className="chart-button"><span>District summary</span></div>
+                </a>
+
+                <a href={this.getNGACampaignMonitoringUrl()}>
+                  <div className="chart-button"><span>NGA Campaign Monitoring</span></div>
+                </a>
+              </div>
         } else {
-            controls = <div className="chart-button-group">
-                            <div className="chart-button"><span>Country overview</span></div>
-                            <div className="chart-button"><span>District summary</span></div>
-                        </div>
+            controls =
+              <div className="chart-button-group">
+                <a href={this.getManagementDashboardUrl()}>
+                  <div className="chart-button"><span>Country overview</span></div>
+                </a>
+                <a href={this.getDistrictSummaryUrl()}>
+                  <div className="chart-button"><span>District summary</span></div>
+                </a>
+              </div>
         }
+
         return (
             <div>
                 <div className="large-4 columns chart-container" id={chartId}>
