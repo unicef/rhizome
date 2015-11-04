6cc126286e1df452adfd64fbfa167fa5980509e4
Qiu Juntao <juntao.qiu@gmail.com>
2015-11-04 19:38:29 +1100

    add the missing button to Nigeria

--- webapp/src/view/HomepageChartsSection.jsx
+++ webapp/src/view/HomepageChartsSection.jsx
@@ -4,24 +4,30 @@ var React = require('react');
 var HomepageCharts = require('dashboard/homepage/HomepageCharts.jsx');
 
 var HomepageChartsSection = React.createClass({
-
     render: function () {
-        console.log(this.props.data);
-
-
         var dashboard = React.createElement(HomepageCharts, this.props.data);
         var chartId = `${this.props.location.toLowerCase()}-chart`;
 
+        var controls;
+        if(this.props.location === 'Nigeria') {
+            controls = <div className="chart-button-group">
+                            <div className="chart-button"><span>Country overview</span></div>
+                            <div className="chart-button"><span>District summary</span></div>
+                            <div className="chart-button"><span>NGA Campaign Monitoring</span></div>
+                        </div>
+        } else {
+            controls = <div className="chart-button-group">
+                            <div className="chart-button"><span>Country overview</span></div>
+                            <div className="chart-button"><span>District summary</span></div>
+                        </div>
+        }
         return (
             <div>
                 <div className="large-4 columns chart-container" id={chartId}>
                     <div className="chart">
                         <h5>{this.props.location}</h5>
-                          {dashboard}
-                        <div className="chart-button-group">
-                            <div className="chart-button"><span>Country overview</span></div>
-                            <div className="chart-button"><span>District summary</span></div>
-                        </div>
+                        {dashboard}
+                        {controls}
                     </div>
                 </div>
 
