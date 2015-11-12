95a19555ca54cfe6f90f3615263824effad80666
Ruoran Liu <rrliu@thoughtworks.com>
2015-11-12 16:27:22 +0800

    remove old chart builder

--- webapp/src/view/chart-wizard/ChartSelect.jsx
+++ webapp/src/view/chart-wizard/ChartSelect.jsx
@@ -1,20 +1,21 @@
-var React = require('react')
+import React from 'react'
 
-module.exports = React.createClass({
+export default React.createClass({
   propTypes: {
     charts: React.PropTypes.array.isRequired,
     value: React.PropTypes.number.isRequired,
     onChange: React.PropTypes.func.isRequired
   },
-  _handleChange: function (event){
+
+  _handleChange (event) {
     this.props.onChange(event.target.value)
   },
-  render: function (){
-    var self = this
-    var chartBoxes = this.props.charts.map(function (chart, index){
+
+  render () {
+    var chartBoxes = this.props.charts.map((chart, index) => {
       return (
-        <div key={chart.name} className={'chart-box-wrapper ' + (chart.name === self.props.value ? 'active' : '')}>
-          <div className='chart-box' onClick={self.props.onChange.bind(null, chart.name)}>
+        <div key={chart.name} className={'chart-box-wrapper ' + (chart.name === this.props.value ? 'active' : '')}>
+          <div className='chart-box' onClick={this.props.onChange.bind(null, chart.name)}>
             <img className='chart-icon' src={'/static/img/chart-icons/' + chart.name + '.png'} />
             {chart.name}
           </div>
