23db9ff325c22906003da58e1f0eb29ac1bff3bc
Guoqing <zhaogq10@163.com>
2015-11-12 17:09:59 +0800

    hot fix standard style checking for comments

--- webapp/src/component/NavMenu.jsx
+++ webapp/src/component/NavMenu.jsx
@@ -1,29 +1,29 @@
-//'use strict'
+//  'use strict'
 //
-//var React = require('react')
+//  var React = require('react')
 //
-//var NavMenu = React.createClass({
-//  mixins: [
-//    require('mixin/MenuControl')
-//  ],
+//  var NavMenu = React.createClass({
+//    mixins: [
+//      require('mixin/MenuControl')
+//    ],
 //
-//  propTypes: {
-//    text: React.PropTypes.string.isRequired,
-//    icon: React.PropTypes.string
-//  },
+//    propTypes: {
+//      text: React.PropTypes.string.isRequired,
+//      icon: React.PropTypes.string
+//    },
 //
-//  render: function () {
-//    var display = this.props.icon
-//      ? (<span><i className={'fa fa-lg ' + this.props.icon}></i>&ensp;{this.props.text}</span>)
-//      : (<span>{this.props.text}</span>)
+//    render: function () {
+//      var display = this.props.icon
+//        ? (<span><i className={'fa fa-lg ' + this.props.icon}></i>&ensp;{this.props.text}</span>)
+//        : (<span>{this.props.text}</span>)
 //
-//    return (
-//      <a onClick={this._toggleMenu} onBlur={this._onBlur} tabIndex='-1'>
-//        {display}&emsp;<i className='fa fa-chevron-down'></i>
-//      </a>
-//    )
-//  }
+//      return (
+//        <a onClick={this._toggleMenu} onBlur={this._onBlur} tabIndex='-1'>
+//          {display}&emsp;<i className='fa fa-chevron-down'></i>
+//        </a>
+//      )
+//    }
 //
-//})
+//  })
 //
-//module.exports = NavMenu
+//  module.exports = NavMenu
