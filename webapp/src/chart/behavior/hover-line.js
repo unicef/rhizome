d727be84c01db66c98c7c0917f1f6eb2520dfb7e
rongliu <570899307@qq.com>
2015-11-17 09:39:02 +0800

    modify the total location

--- webapp/src/chart/behavior/hover-line.js
+++ webapp/src/chart/behavior/hover-line.js
@@ -309,7 +309,7 @@ function hoverLine () {
 
     if (type) {
       labelData.push({
-        x: 330,
+        x: 300,
         y: -10,
         text: 'TOTAL ' + yFormat(_.sum(labelData, function (d) {
           return d.value
