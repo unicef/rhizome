ed7784971369acacb290fe3f3d98583ad3c958e9
Ruoran Liu <rrliu@thoughtworks.com>
2015-11-24 13:57:39 +0800

    add palette for choropleth map

--- webapp/src/util/palettes.js
+++ webapp/src/util/palettes.js
@@ -4,6 +4,9 @@ export default {
     '#7F7F7F', '#8FB6BD', '#A5B9CC', '#A2AAB3', '#595959', '#7BA1A8', '#758E9E', '#87939E', '#333333',
     '#6493A6', '#436380', '#717B8B', '#222222', '#387EA3', '#334B61', '#4A5D70', '#000000'
   ],
+  orange: [
+    '#FEE7DC', '#FABAA2', '#F58667', '#D95449', '#AF373E', '#2D2525'
+  ],
   rainbow: [
     'indianred', 'coral', 'gold', 'limegreen', 'turquoise', 'royalblue', 'darkviolet', 'crimson', 'tomato'
   ]
