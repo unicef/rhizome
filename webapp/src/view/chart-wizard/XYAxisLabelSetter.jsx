16a7c40bc08c0daef9359d2f0f09a01eb9a8aaf7
Sichu <zhangsichu@gmail.com>
2015-12-01 13:19:06 +0800

    finished the component.

--- webapp/src/view/chart-wizard/XYAxisLabelSetter.jsx
+++ webapp/src/view/chart-wizard/XYAxisLabelSetter.jsx
@@ -27,15 +27,21 @@ export default React.createClass({
     this.setState({xLabel: nextProps.xLabel, yLabel: nextProps.yLabel})
   },
 
-  _updateText: function (e) {
-    this.setState({xLabel: e.currentTarget.value})
-    this.props.onChange(e.currentTarget.value)
+  _updateText: function () {
+    let xAxisLabel = this.refs.xAxisLabel.getDOMNode().value
+    let yAxisLabel = this.refs.yAxisLabel.getDOMNode().value
+    this.setState({xLabel: xAxisLabel, yLabel: yAxisLabel})
+    this.props.onChange(xAxisLabel, yAxisLabel)
   },
   render: function () {
     return (
       <div>
-        <input type='text' value={this.state.xLabel} onChange={this._updateText}/>
-        <input type='text' value={this.state.yLabel} onChange={this._updateText}/>
+        <label htmlFor='chart-wizard-y-axis-label'> X Axis Label
+          <input type='text' value={this.state.xLabel} onChange={this._updateText} ref='xAxisLabel' id='chart-wizard-y-axis-label' />
+        </label>
+        <label htmlFor='chart-wizard-x-axis-label'> Y Axis Label
+          <input type='text' value={this.state.yLabel} onChange={this._updateText} ref='yAxisLabel' id='chart-wizard-x-axis-label' />
+        </label>
       </div>
     )
   }
