fd0b357651bb9fd20c9c117ff01dfae491d10656
John Dingee <dingeej@gmail.com>
2015-12-02 12:17:56 -0500

    clean up user admin migration

--- datapoints/migrations/0033_indicatortooffice.py
+++ datapoints/migrations/0033_indicatortooffice.py
@@ -7,7 +7,7 @@ from django.db import models, migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('datapoints', '0032_useradminlevelpermissions'),
+        ('datapoints', '0031_adminlevelpermission'),
     ]
 
     operations = [
