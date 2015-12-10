4d9d85569a78e9d800442a5a052ae1804098a579
Ruoran Liu <rrliu@thoughtworks.com>
2015-12-10 13:18:31 +0800

    test for color.js

--- webapp/src/util/color.js
+++ webapp/src/util/color.js
@@ -5,28 +5,21 @@ var colors = ['#377EA4', '#B6D0D4']
 const defaultPalette = ['#334B61', '#222222', '#387EA3', '#436380', '#6493A6', '#7BA1A8', '#87939E',
   '#D5E5F2', '#8FB6BD', '#A5B9CC', '#A2AAB3', '#D7D9E1', '#C5D9DD', '#AFC5CD', '#E5E9EC']
 
-function scale (categories, palette) {
-  palette = palette || colors
+function scale (categories, palette = colors) {
   var interpolate = d3.interpolate(
     d3.rgb(palette[0]),
     d3.rgb(palette[palette.length - 1]))
 
   var s = d3.scale.linear().domain([0, categories.length - 1])
 
-  // Build up a range of colors for the ordinal scale by interpolating the
-  // two extremes of the colors from the coolgray array
-  var range = []
-  for (var i = 0, l = categories.length; i < l; i++) {
-    range.push(interpolate(s(i)))
-  }
+  let range = categories.map((category, idx) => {
+    return interpolate(s(idx))
+  })
 
-  return d3.scale.ordinal()
-    .domain(categories)
-    .range(range)
+  return d3.scale.ordinal().domain(categories).range(range)
 }
 
-function map (categories, palette) {
-  palette = palette || defaultPalette
+function map (categories, palette = defaultPalette) {
   let range = _.clone(palette)
   while (range.length < categories.length) {
     range = range.concat(range)
