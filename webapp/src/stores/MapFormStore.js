08abceeb98a993f11f3f3cb7853f0438eb22df13
rongliu <570899307@qq.com>
2015-12-24 12:05:09 +0800

    modify the mapformstore

--- webapp/src/stores/MapFormStore.js
+++ webapp/src/stores/MapFormStore.js
@@ -1,18 +1,48 @@
 import Reflux from 'reflux'
 import api from 'data/api'
 
-var MapFormStore = Reflux.createStore({
+import MapFormActions from 'actions/MapFormActions'
 
-  getSourceMap: function (id) {
-    return api.get_source_object_map(id, null, {'cache-control': 'no-cache'})
-      .then(function (response) {
-        return response.objects[0]
+var MapFormStore = Reflux.createStore({
+  listenables: MapFormActions,
+  data: {
+    modalIsOpen: false,
+    content_type: null,
+    master_object_id: null,
+    master_object_name: null,
+    source_object_code: null
+  },
+
+  getInitialState: function () {
+    return this.data
+  },
+
+  onGetSourceMap: function (id) {
+    api.get_source_object_map(id, null, {'cache-control': 'no-cache'})
+      .then(response => {
+        this.trigger({
+          modalIsOpen: true,
+          content_type: response.objects[0].content_type,
+          master_object_id: response.objects[0].mapped_by_id,
+          source_object_code: response.objects[0].source_object_code
+        })
       })
   },
 
-  updateMetaMap: function (info) {
-    return api.post_source_object_map(info).then(function (response) {
-      return response.objects
+  onUpdateMetaMap: function (info) {
+    api.post_source_object_map(info)
+      .then(response => {
+        this.trigger({
+          master_object_id: response.objects.mapped_by_id,
+          master_object_name: response.objects.master_object_name
+        })
+      })
+  },
+
+  onClear: function () {
+    this.trigger({
+      modalIsOpen: false,
+      content_type: null
     })
   }
 })
