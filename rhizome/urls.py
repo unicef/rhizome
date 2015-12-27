b4b1bb7b3df6beaf67e13ee3f50f85594b6c0ba6
vivihuang <rwhuang@thoughtworks.com>
2015-12-27 21:55:40 +0800

    add wafflejs for using waffle in js

--- rhizome/urls.py
+++ rhizome/urls.py
@@ -91,6 +91,8 @@ urlpatterns = patterns(
     # ABOUT PAGE
     url(r'^about$', TemplateView.as_view(template_name="about.html"), name='about'),
 
+    url(r'^', include('waffle.urls')),
+
 ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 from django.conf import settings
