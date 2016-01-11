9357892841b1284a923ee532674d52c568126f8e
vivihuang <rwhuang@thoughtworks.com>
2016-01-11 15:31:37 +0800

    remove some unused dependency in package.json

--- external/react-datascope/lib/index.js
+++ external/react-datascope/lib/index.js
@@ -8,9 +8,6 @@ module.exports = {
 
     DataList: require('./modules/DataList'),
 
-    // fixed-data-table - deprecated for now
-    //DataTable: require('./modules/DataTable'),
-
     SimpleDataTable: require('./modules/SimpleDataTable'),
     SimpleDataTableColumn: require('./modules/SimpleDataTableColumn'),
 
