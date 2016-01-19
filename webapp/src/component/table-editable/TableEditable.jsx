2843f90c3ae7b661b78eba429d44d9599fca5d80
rongliu <570899307@qq.com>
2016-01-19 12:02:45 +0800

    modify the table editable store

--- webapp/src/component/table-editable/TableEditable.jsx
+++ webapp/src/component/table-editable/TableEditable.jsx
@@ -2,23 +2,35 @@ import React from 'react'
 import Reflux from 'reflux'
 import _ from 'lodash'
 import d3 from 'd3'
-import api from '../../data/api'
+// import api from '../../data/api'
 
 import Cell from 'component/table-editable/Cell.jsx'
 import TableEditableStore from 'stores/TableEditableStore'
 import TableEditableActions from 'actions/TableEditableActions'
 
-var TableEditale = React.createClass({
+let formats = {
+  percent: d3.format('%')
+}
+
+let TableEditale = React.createClass({
   mixins: [Reflux.connect(TableEditableStore)],
 
   propTypes: {
-    data: React.PropTypes.object,
+    data: React.PropTypes.array,
     loaded: React.PropTypes.bool,
     indicatorSet: React.PropTypes.object,
     indicatorMap: React.PropTypes.object,
     locationMap: React.PropTypes.object,
     locations: React.PropTypes.array,
     campaignId: React.PropTypes.string
+  },
+
+  componentWillReceiveProps: function (nextProps) {
+    if (nextProps.loaded && nextProps.data) {
+      TableEditableActions.init(nextProps.data, nextProps.indicatorSet,
+        nextProps.indicatorMap, nextProps.locationMap,
+        nextProps.locations, nextProps.campaignId)
+    }
   },
 
   shouldComponentUpdate: function (nextProps, nextState) {
@@ -36,142 +48,16 @@ var TableEditale = React.createClass({
     return null
   },
 
-  _buildData: function () {
-    // define columns
-    let columns = [{
-      header: 'Indicator',
-      type: 'label',
-      headerClasses: 'medium-3'
-    }]
-
-    // add location names as columns
-    _.forEach(this.props.locations, locationId => {
-      columns.push({
-        header: this.props.locationMap[locationId].name,
-        type: 'value',
-        key: locationId,
-        children: null
-      })
-    })
-
-    // cell formatters
-    var numericFormatter = function (v) {
-      return (isNaN(v) || _.isNull(v)) ? v : d3.format('n')(v)
-    }
-
-    // arrange datapoints into an object of indicators > locations
-    var byIndicator = {}
-    this.props.data.forEach(function (d) {
-      if (!byIndicator[d.indicator_id]) { byIndicator[d.indicator_id] = {} }
-      byIndicator[d.indicator_id][d.location_id] = d
-    })
-
-    // assemble data points into rows for table
-    var rows = []
-
-    _.each(this.props.indicatorSet.indicators, rowInfo => {
-      var row = []
-      if (rowInfo.type && rowInfo.type === 'section-header') { // section header row
-        row.push({
-          isEditable: false,
-          type: 'label',
-          value: rowInfo.title,
-          class: 'section-header',
-          colspan: columns.length
-        })
-      } else { // normal indicator row
-        var indicator_id = rowInfo.id
-        // add columns
-        columns.forEach(column => {
-          var cell = {
-            isEditable: false,
-            type: column.type
-          }
-          switch (column.type) {
-            // editable value
-            case 'value':
-              cell.isEditable = true
-              cell.format = numericFormatter
-              cell.classes = 'numeric'
-              cell.width = 80
-              if (byIndicator[indicator_id] && byIndicator[indicator_id][column.key]) {
-                cell.datapoint_id = byIndicator[indicator_id][column.key].datapoint_id
-                cell.value = byIndicator[indicator_id][column.key].value
-                cell.note = byIndicator[indicator_id][column.key].note
-              } else {
-                cell.datapoint_id = null
-                cell.value = null
-                cell.note = null
-              }
-              // tooltip
-              if (this.props.locationMap[column.key] && this.props.indicatorMap[indicator_id]) {
-                cell.tooltip = this.props.indicatorMap[indicator_id].name + ' value for ' + this.props.locationMap[column.key].name
-              } else {
-                cell.tooltip = null
-              }
-              // generate validation for values
-              cell.validateValue = function (newVal) {
-                var value, passed
-                if (_.isNull(newVal)) {
-                  value = null
-                  passed = true
-                } else {
-                  value = parseFloat(newVal)
-                  passed = !_.isNaN(value)
-                }
-                return { 'value': value, 'passed': passed }
-              }
-
-              // generate promise for submitting a new value to the API for saving
-              cell.buildSubmitPromise = function (newVal) {
-                var upsert_options = {
-                  datapoint_id: cell.datapoint_id,
-                  campaign_id: parseInt(this.props.campaignId, 10),
-                  indicator_id: indicator_id,
-                  location_id: column.key,
-                  value: parseFloat(newVal)
-                }
-                return api.datapointUpsert(upsert_options)
-              }
-
-              // callback to specifically handle response
-              cell.withResponse = function (response) {}
-
-              // callback to handle error
-              cell.withError = function (error) {
-                console.log(error)
-                if (error.msg && error.msg.message) { window.alert('Error: ' + error.msg.message) }
-                cell.hasError = true
-              }
-              break
-
-            // indicator name
-            case 'label':
-              cell.value = this.props.indicatorMap[indicator_id]
-                ? this.props.indicatorMap[indicator_id].name
-                : 'Missing info for indicator ' + indicator_id
-              cell.classes = 'label'
-              cell.width = 300
-              break
-          }
-          row.push(cell)
-        })
-      } // end normal indicator row
-      rows.push(row)
-    })
-
-    return {rows: rows, columns: columns}
+  percent: function (v) {
+    return formats.percent(v)
   },
 
-  _buildTable: function (data) {
-    console.log(this.state.total.total)
-    TableEditableActions.updateStats(data)
-    console.log(this.state.total.total)
+  _buildTable: function () {
     let contentTitle = (
       <div>
         <h5>
           Form is <span className={this.completionClass(this.state.total.complete / this.state.total.total)}>
-          {this.state.total.complete / this.state.total.total} </span>
+          {this.percent(this.state.total.complete / this.state.total.total)} </span>
            complete ({this.state.total.complete} / {this.state.total.total} values entered)
         </h5>
       </div>
@@ -180,15 +66,16 @@ var TableEditale = React.createClass({
     let tableHeader = ''
     let tableBody = ''
 
-    if (data.rows.length > 0) {
-      tableHeader = data.columns.map(column => {
+    if (this.state.table.rows.length > 0) {
+      tableHeader = this.state.table.columns.map(column => {
         return (
           <tr>
             <th></th>
             <th className={column.headerClasses}>
               <div className='th-inner'>
                 {column.header}
-                <div className={'completionStatus'}>
+                <div className='completionStatus'>
+                  {this.state.total.complete} / {this.state.total.total}
                 </div>
               </div>
             </th>
@@ -197,11 +84,12 @@ var TableEditale = React.createClass({
       })
     }
 
-    tableBody = data.rows.map(row => {
+    tableBody = this.state.table.rows.map(row => {
       return (
         <tr>
           <td className='rowCompletionStatus'>
             <div className='completionStatus'>
+              {this.state.total.complete} / {this.state.total.total}
             </div>
           </td>
           <td>
@@ -233,12 +121,12 @@ var TableEditale = React.createClass({
   },
 
   render: function () {
-    if (!this.props.loaded) {
+    if (!this.props.loaded || !this.state.processed) {
       return (<div className='empty'>Use the options above to load a data entry form.</div>)
     } else if (this.props.indicatorSet.indicators.length < 1) {
       return (<div className='empty'>Use the options above to load a data entry form.</div>)
     } else {
-      return this._buildTable(this._buildData())
+      return this._buildTable()
     }
   }
 })
--- webapp/src/stores/TableEditableStore.js
+++ webapp/src/stores/TableEditableStore.js
@@ -1,5 +1,7 @@
 import _ from 'lodash'
 import Reflux from 'reflux'
+import d3 from 'd3'
+import api from 'data/api'
 
 let newCounter = function () {
   return {
@@ -12,18 +14,19 @@ let TableEditaleStore = Reflux.createStore({
   listenables: [require('actions/TableEditableActions')],
 
   data: {
+    table: {
+      rows: [],
+      columns: []
+    },
     total: newCounter(),
     byRow: [],
-    byColumn: []
+    byColumn: [],
+    processed: false
   },
 
-  getInitialState: function () {
-    return this.data
-  },
-
-  onUpdateStats: function (data) {
-    if (data.rows.length > 0) {
-      _.forEach(data.rows, (row, rowIndex) => {
+  _updateStats: function () {
+    if (this.data.table.rows.length > 0) {
+      _.forEach(this.data.table.rows, (row, rowIndex) => {
         if (this.data.byRow[rowIndex] === undefined) {
           this.data.byRow[rowIndex] = newCounter()
         }
@@ -47,6 +50,146 @@ let TableEditaleStore = Reflux.createStore({
         })
       })
     }
+  },
+
+  getInitialState: function () {
+    return this.data
+  },
+
+  onInit: function (data, indicatorSet, indicatorMap, locationMap, locations, campaignId) {
+    // define columns
+    let columns = [{
+      header: 'Indicator',
+      type: 'label',
+      headerClasses: 'medium-3'
+    }]
+
+    // add location names as columns
+    _.forEach(locations, locationId => {
+      columns.push({
+        header: locationMap[locationId].name,
+        type: 'value',
+        key: locationId,
+        children: null
+      })
+    })
+
+    // cell formatters
+    var numericFormatter = function (v) {
+      return (isNaN(v) || _.isNull(v)) ? v : d3.format('n')(v)
+    }
+
+    // arrange datapoints into an object of indicators > locations
+    var byIndicator = {}
+    data.forEach(function (d) {
+      if (!byIndicator[d.indicator_id]) { byIndicator[d.indicator_id] = {} }
+      byIndicator[d.indicator_id][d.location_id] = d
+    })
+
+    // assemble data points into rows for table
+    var rows = []
+
+    _.each(indicatorSet.indicators, rowInfo => {
+      var row = []
+      if (rowInfo.type && rowInfo.type === 'section-header') { // section header row
+        row.push({
+          isEditable: false,
+          type: 'label',
+          value: rowInfo.title,
+          class: 'section-header',
+          colspan: columns.length
+        })
+      } else { // normal indicator row
+        var indicator_id = rowInfo.id
+        // add columns
+        columns.forEach(column => {
+          var cell = {
+            isEditable: false,
+            type: column.type
+          }
+          switch (column.type) {
+            // editable value
+            case 'value':
+              cell.isEditable = true
+              cell.format = numericFormatter
+              cell.classes = 'numeric'
+              cell.width = 80
+              if (byIndicator[indicator_id] && byIndicator[indicator_id][column.key]) {
+                cell.datapoint_id = byIndicator[indicator_id][column.key].datapoint_id
+                cell.value = byIndicator[indicator_id][column.key].value
+                cell.note = byIndicator[indicator_id][column.key].note
+              } else {
+                cell.datapoint_id = null
+                cell.value = null
+                cell.note = null
+              }
+              // tooltip
+              if (locationMap[column.key] && indicatorMap[indicator_id]) {
+                cell.tooltip = indicatorMap[indicator_id].name + ' value for ' + locationMap[column.key].name
+              } else {
+                cell.tooltip = null
+              }
+              // generate validation for values
+              cell.validateValue = function (newVal) {
+                var value, passed
+                if (_.isNull(newVal)) {
+                  value = null
+                  passed = true
+                } else {
+                  value = parseFloat(newVal)
+                  passed = !_.isNaN(value)
+                }
+                return { 'value': value, 'passed': passed }
+              }
+
+              // generate promise for submitting a new value to the API for saving
+              cell.buildSubmitPromise = function (newVal) {
+                var upsert_options = {
+                  datapoint_id: cell.datapoint_id,
+                  campaign_id: parseInt(campaignId, 10),
+                  indicator_id: indicator_id,
+                  location_id: column.key,
+                  value: parseFloat(newVal)
+                }
+                return api.datapointUpsert(upsert_options)
+              }
+
+              // callback to specifically handle response
+              cell.withResponse = function (response) {}
+
+              // callback to handle error
+              cell.withError = function (error) {
+                console.log(error)
+                if (error.msg && error.msg.message) { window.alert('Error: ' + error.msg.message) }
+                cell.hasError = true
+              }
+              break
+
+            // indicator name
+            case 'label':
+              cell.value = indicatorMap[indicator_id]
+                ? indicatorMap[indicator_id].name
+                : 'Missing info for indicator ' + indicator_id
+              cell.classes = 'label'
+              cell.width = 300
+              break
+          }
+          row.push(cell)
+        })
+      } // end normal indicator row
+      rows.push(row)
+    })
+
+    this.data.table.rows = rows
+    this.data.table.columns = columns
+    this.data.processed = true
+
+    this._updateStats()
+    this.trigger(this.data)
+  },
+
+  onUpdateStats: function () {
+    this._updateStats()
     this.trigger(this.data)
   }
 })
