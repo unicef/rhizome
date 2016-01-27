9652a7325d13b7ef5af25931a54026faa0e6d12a
vivihuang <rwhuang@thoughtworks.com>
2016-01-27 10:15:54 +0800

    refactor the code for using manifest file in gulpRev

--- webapp/tasks/config/revReplace.js
+++ webapp/tasks/config/revReplace.js
@@ -1,12 +1,14 @@
 import gulp from 'gulp'
 
 const jsDestFolder = `${gulp.config('base.dist')}/static`
+var proManifest =  jsDestFolder + '/rev-manifest.json'
+var devManifest = './rev-manifest.json'
+var manifestFile = (process.env.NODE_ENV === 'production') ? proManifest : devManifest
 
 export default {
   files: [
     {
-      'proManifest': jsDestFolder + '/rev-manifest.json',
-      'devManifest': './rev-manifest.json',
+      'manifestFile': manifestFile,
       'templates': `${gulp.config('templates')}`,
       'entry': jsDestFolder,
       'dest': jsDestFolder
