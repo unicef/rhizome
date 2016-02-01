0161637a1b1228c3232dadeba657795011b8490c
Sichu <zhangsichu@gmail.com>
2016-02-01 22:28:12 +0800

    fix the campaign name box value bug.

--- webapp/src/ufadmin/CampaignsPage.js
+++ webapp/src/ufadmin/CampaignsPage.js
@@ -73,7 +73,7 @@ var CampaignsPage = React.createClass({
     let nameSet = (
       <div>
         <label htmlFor='name'>Name: </label>
-        <input type='text' value={this.state.postData.name} onBlur={this._setCampaignName} ref='campaignName'/>
+        <input type='text' defaultValue={this.state.postData.name} onBlur={this._setCampaignName} ref='campaignName'/>
       </div>
     )
 
