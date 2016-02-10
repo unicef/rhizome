0b0e9f966ad29d91b68c91e44a64f824b24226fc
Dima Markus <dmi3000@gmail.com>
2016-02-10 12:58:13 -0500

    Clean up chart wizard component

--- webapp/src/view/chart-wizard/ChartWizard.jsx
+++ webapp/src/view/chart-wizard/ChartWizard.jsx
@@ -6,20 +6,16 @@ import ExpandableSection from 'component/ExpandableSection.jsx'
 import DateRangePicker from 'component/DateTimePicker.jsx'
 import PreviewScreen from './PreviewScreen.jsx'
 import ChartSelect from './ChartSelect.jsx'
+
 import List from 'component/list/List.jsx'
 import ReorderableList from 'component/list/ReorderableList.jsx'
 
 import DropdownMenu from 'component/menus/DropdownMenu.jsx'
 import Chart from 'component/Chart.jsx'
 
-import ExplorerStore from 'stores/ExplorerStore'
-import ExplorerActions from 'actions/ExplorerActions'
 import ChartWizardActions from 'actions/ChartWizardActions'
 import ChartWizardStore from 'stores/ChartWizardStore'
 import SimplePreview from './preview/SimplePreview.jsx'
-
-// import options from './options/options'
-// import previews from './preview/previews'
 
 const defaultChartDef = {
   title: '',
@@ -42,12 +38,7 @@ let ChartWizard = React.createClass({
     cancel: React.PropTypes.func
   },
 
-  mixins: [Reflux.connect(ChartWizardStore, 'data'), Reflux.connect(ExplorerStore)],
-
-  componentWillMount: function () {
-    ExplorerActions.getLocations()
-    ExplorerActions.getIndicators()
-  },
+  mixins: [Reflux.connect(ChartWizardStore, 'data')],
 
   componentDidMount () {
     this.chartDef = this.props.chartDef || defaultChartDef
@@ -70,43 +61,19 @@ let ChartWizard = React.createClass({
     }
   },
 
-  setLocationSearch (pattern) {
+  setLocationSearch: function (pattern) {
     this.setState({
       locationSearch: pattern
     })
   },
 
-  render () {
-    // console.log('this.state: ', this.state)
-    // console.log('this.props: ', this.props)
+  previewData: function (data) {
+    console.log(data)
+  },
 
+  render: function () {
     let availableIndicators = this.state.data.indicatorList
-    let indicatorStep = (
-      <ExpandableSection title='Indicators' refer='preview'>
-        <DropdownMenu
-          items={availableIndicators}
-          sendValue={ChartWizardActions.addIndicator}
-          item_plural_name='Indicators'
-          text='Choose Indicators'
-          style='databrowser__button'/>
-        <ReorderableList items={this.state.data.indicatorSelected} removeItem={ChartWizardActions.removeIndicator} dragItem={ChartWizardActions.reorderIndicator} />
-      </ExpandableSection>
-    )
-
-    let locationStep = (
-      <ExpandableSection title='Locations' refer='preview'>
-        <DropdownMenu
-          items={this.state.data.locationFilteredList}
-          sendValue={ChartWizardActions.addLocation}
-          item_plural_name='Locations'
-          text='Add Location'
-          style='databrowser__button'
-          icon='fa-globe'/>
-        <List items={this.state.data.location} removeItem={ChartWizardActions.removeLocation} />
-        <div id='locations' placeholder='0 selected' multi='true' searchable='true' className='search-button'></div>
-      </ExpandableSection>
-    )
-
+    let palette = this.state.data.chartDef.palette || 'orange'
     let startDate = moment()
     let endDate = moment()
 
@@ -114,31 +81,6 @@ let ChartWizard = React.createClass({
       startDate = moment(this.state.data.chartDef.startDate, 'YYYY-MM-DD').toDate()
       endDate = moment(this.state.data.chartDef.endDate, 'YYYY-MM-DD').toDate()
     }
-
-    let timePeriodStep = (
-      <ExpandableSection title='Time' refer='preview'>
-        <DateRangePicker
-          sendValue={ChartWizardActions.updateDateRangePicker}
-          start={startDate}
-          end={endDate}
-          fromComponent='ChartWizard' />
-      </ExpandableSection>
-    )
-    let chartTypeStep = (
-      <ExpandableSection title='Chart Type' refer='preview'>
-        <ChartSelect charts={this.state.data.chartTypeFilteredList} value={this.state.data.chartDef.type}
-          onChange={ChartWizardActions.changeChart}/>
-      </ExpandableSection>
-    )
-
-    let palette = this.state.data.chartDef.palette || 'orange'
-
-    let chartTitleAndPalette = <SimplePreview
-      chartTitle={this.props.chartDef.title}
-      onEditTitle={ChartWizardActions.editTitle}
-      palette={palette}
-      onChangePalette={ChartWizardActions.changePalette}
-      />
 
     if (!this.state.data.chartDef.type) {
       return null
@@ -148,37 +90,65 @@ let ChartWizard = React.createClass({
       <Chart id='custom-chart' type={this.state.data.chartDef.type} data={this.state.data.chartData}
         options={this.state.data.chartOptions}/>
     )
-    let chartWizardSelector = <div className='medium-3 columns'>
-          <h1>Chart Builder</h1>
-            {indicatorStep}
-            {locationStep}
-            {timePeriodStep}
-            {chartTypeStep}
-            <div className='row'>
-              <button className='chart-wizard__save' onClick={this.saveChart}>
-                  Save
-              </button>
-              <button className='chart-wizard__cancel' onClick={this.props.cancel}>
-                  Cancel
-              </button>
-           </div>
-           <div>
-             <br></br>
-             {chartTitleAndPalette}
-         </div>
-          </div>
 
-    let chartWizardEditor = <div className='row'></div>
     return (
       <div className='chart-wizard'>
-        {chartWizardSelector}
+        <div className='medium-3 columns'>
+          <h1>Chart Builder</h1>
+          <ExpandableSection title='Indicators' refer='preview'>
+            <DropdownMenu
+              items={availableIndicators}
+              sendValue={ChartWizardActions.addIndicator}
+              item_plural_name='Indicators'
+              text='Choose Indicators'
+              style='databrowser__button'/>
+            <ReorderableList items={this.state.data.indicatorSelected} removeItem={ChartWizardActions.removeIndicator} dragItem={ChartWizardActions.reorderIndicator} />
+          </ExpandableSection>
+
+          <ExpandableSection title='Locations' refer='preview'>
+            <DropdownMenu
+              items={this.state.data.locationFilteredList}
+              sendValue={ChartWizardActions.addLocation}
+              item_plural_name='Locations'
+              text='Add Location'
+              style='databrowser__button'
+              icon='fa-globe'/>
+            <List items={this.state.data.selected_locations} removeItem={ChartWizardActions.removeLocation} />
+            <div id='locations' placeholder='0 selected' multi='true' searchable='true' className='search-button'></div>
+          </ExpandableSection>
+
+          <ExpandableSection title='Time' refer='preview'>
+            <DateRangePicker
+              sendValue={ChartWizardActions.updateDateRangePicker}
+              start={startDate}
+              end={endDate}
+              fromComponent='ChartWizard' />
+          </ExpandableSection>
+
+          <ExpandableSection title='Chart Type' refer='preview'>
+            <ChartSelect charts={this.state.data.chartTypeFilteredList} value={this.state.data.chartDef.type}
+              onChange={ChartWizardActions.changeChart}/>
+          </ExpandableSection>
+
+          <div className='row'>
+            <button className='chart-wizard__save' onClick={this.saveChart}>
+                Save
+            </button>
+            <button className='chart-wizard__cancel' onClick={this.props.cancel}>
+                Cancel
+            </button>
+          </div>
+
+          <SimplePreview
+            chartTitle={this.props.chartDef.title}
+            onEditTitle={ChartWizardActions.editTitle}
+            palette={palette}
+            onChangePalette={ChartWizardActions.changePalette} />
+        </div>
         <PreviewScreen isLoading={this.state.data.isLoading}>
-          {this.state.data.canDisplayChart
-            ? chart
-            : (<div className='empty'>No Data</div>)
-          }
+          {this.state.data.canDisplayChart ? chart : (<div className='empty'>No Data</div>) }
         </PreviewScreen>
-        {chartWizardEditor}
+        <div className='row'></div>
       </div>
     )
   }
