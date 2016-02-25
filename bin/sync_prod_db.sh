cf08e0cfddd94212f7e154e645adeafb99dbbc33
Wazir Khan <wazir.ahmadzai@gmail.com>
2016-02-25 15:17:11 +0430

    I just added ignored the apache local file in the gitignore file

--- bin/sync_prod_db.sh
+++ bin/sync_prod_db.sh
@@ -3,20 +3,20 @@
 DB=rhizome
 DB_USER=djangoapp
 DB_PASS="w3b@p01i0"
-PORT=6432
+PORT=5432
 OPTS="-U $DB_USER -h 127.0.0.1 -p $PORT"
 export PGPASSWORD=$DB_PASS
 
-echo "Pulling zipped .sql file"
-curl https://s3.amazonaws.com/rhizome-backup/rhizome_latest.sql.gz -o ~/rhizome_latest.sql.gz
+# echo "Pulling zipped .sql file"
+# curl https://s3.amazonaws.com/rhizome-backup/rhizome_latest.sql.gz -o ~/rhizome_latest.sql.gz
 
 echo "Unzipping sql file"
-gunzip -f ~/rhizome_latest.sql.gz
+gunzip -f ~/Desktop/rhizome_latest.sql.gz
 
 echo "Killing all connections..."
 
 # Kill All Connections to DB #
-psql $OPTS -d postgres -c "select pg_terminate_backend(pid)
+psql $OPTS -d wazir -c "select pg_terminate_backend(pid)
    from pg_stat_activity
    where datname = '$DB';"
 #
@@ -24,7 +24,7 @@ echo "Terminated all of your psql connections.. Dropping the database in 5..4.."
 
 sleep 5
 echo "Drop database $DB"
-psql $OPTS -d postgres -c "DROP DATABASE IF EXISTS $DB;"
+psql $OPTS -d wazir -c "DROP DATABASE IF EXISTS $DB;"
 
 echo "...CREATING DATABASE..."
 #
@@ -32,13 +32,13 @@ psql $OPTS -c "CREATE DATABASE $DB
   WITH OWNER = djangoapp
        ENCODING = 'UTF8'
        TABLESPACE = pg_default
-       CONNECTION LIMIT = -1;" postgres > /dev/null
+       CONNECTION LIMIT = -1;" wazir > /dev/null
 
 # echo Loading production data...
-psql $OPTS -d $DB -f ~/rhizome_latest.sql
+psql $OPTS -d $DB -f ~/Desktop/rhizome_latest.sql
 
 echo "Cleaning temporary SQL files"
-rm ~/rhizome_latest.sql
+rm ~/Desktop/rhizome_latest.sql
 unset PGPASSWORD
 
 echo "Done"
