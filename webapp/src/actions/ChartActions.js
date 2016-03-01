0e0286b1019d661aa6f8fdb0e065f9b510206db0
Dima Markus <dmi3000@gmail.com>
2016-03-01 13:41:28 -0500

    Update chart actions

--- webapp/src/actions/ChartActions.js
+++ webapp/src/actions/ChartActions.js
@@ -1,13 +1,44 @@
 import Reflux from 'reflux'
 import ChartAPI from 'data/requests/ChartAPI'
+import api from 'data/api'
 
 const ChartActions = Reflux.createActions({
-  'prepChartData': 'prepChartData',
-  'fetchCharts': { children: ['completed', 'failed'], asyncResult: true }
+  'fetchCharts': { children: ['completed', 'failed'], asyncResult: true },
+  'fetchChart': { children: ['completed', 'failed'], asyncResult: true },
+  'fetchChartDatapoints': { children: ['completed', 'failed'], asyncResult: true }
 })
 
+// API CALLS
+// ---------------------------------------------------------------------------
 ChartActions.fetchCharts.listenAndPromise(() => {
   return ChartAPI.getCharts()
 })
+
+ChartActions.fetchChart.listen(chart_id => {
+  ChartActions.fetchChart.promise(ChartAPI.getChart(chart_id))
+})
+
+ChartActions.fetchChartDatapoints.listen((chartDef) => {
+  const query = _prepDatapointsQuery(chartDef)
+  ChartActions.fetchChart.promise(api.datapoints(query))
+})
+
+// ACTION HELPERS
+// ---------------------------------------------------------------------------
+const _prepDatapointsQuery = (chartDef) => {
+  let query = {
+    indicator__in: chartDef.indicator_ids,
+    campaign_start: chartDef.startDate,
+    campaign_end: chartDef.endDate,
+    chart_type: chartDef.type
+  }
+
+  if (chartDef.type === 'ChoroplethMap') {
+    query['parent_location_id__in'] = chartDef.location_ids
+  } else {
+    query['location_id__in'] = chartDef.location_ids
+  }
+  return query
+}
 
 export default ChartActions
