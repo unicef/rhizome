84524140c705772b47c32c243295e4911c5b2237
Dima Markus <dmi3000@gmail.com>
2016-02-29 22:30:14 -0500

    Update api calls

--- webapp/src/data/requests/ChartAPI.js
+++ webapp/src/data/requests/ChartAPI.js
@@ -5,7 +5,7 @@ export default {
   getCharts () {
     return api.get_chart(null, null, {'cache-control': 'no-cache'}).then(response => {
       return _(response.objects).sortBy('id').reverse().value()
-    })
+    }).then(error => { return error })
   },
 
   getChart (id) {
