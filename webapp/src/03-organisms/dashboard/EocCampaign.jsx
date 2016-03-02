ac7d45ae20500b2be8eef5afa42a4763eebc1bbb
Dima Markus <dmi3000@gmail.com>
2016-03-02 02:23:34 -0500

    remove logs from eoc campaign board

--- webapp/src/03-organisms/dashboard/EocCampaign.jsx
+++ webapp/src/03-organisms/dashboard/EocCampaign.jsx
@@ -1,9 +1,20 @@
 import _ from 'lodash'
 import React from 'react'
+import Reflux from 'reflux'
+// import moment from 'moment'
+import StateMixin from'reflux-state-mixin'
 import Chart from '02-molecules/Chart.jsx'
+import IndicatorStore from 'stores/IndicatorStore'
+import IndicatorActions from 'actions/IndicatorActions'
 import DashboardActions from 'actions/DashboardActions'
+import api from 'data/api'
+import ChartFactory from '02-molecules/charts'
 
-var EocCampaign = React.createClass({
+var EocPreCampaign = React.createClass({
+
+ mixins: [
+    StateMixin.connect(IndicatorStore),
+  ],
 
   propTypes: {
     dashboard: React.PropTypes.object.isRequired,
@@ -21,42 +32,68 @@ var EocCampaign = React.createClass({
     }
   },
 
-  render () {
-    const colorScale = ['#F9152F', '#FDFD5B', '#27E833']
-    const data = this.props.data
-    const loading = this.props.loading
-    let tableChart = ''
+  getInitialState() {
+    return {
+      tableData: null
+    }
+  },
 
-    if (data.tableData && data.tableData.options) {
-      let tableOptions = data.tableData.options
-      tableOptions['color'] = colorScale
-      tableOptions['onRowClick'] = d => { DashboardActions.navigate({ location: d }) }
-
-      tableChart = (<Chart type='TableChart'
-          data={data.tableData.data}
-          options={tableOptions}
-          loading={loading}
-        />)
+  componentWillMount() {
+    const indicator_ids = this.props.dashboard.charts[0].indicators
+    let query = {
+      indicator__in: this.props.dashboard.charts[0].indicators,
+      campaign_start: '2016-01-01',
+      campaign_end: '2016-02-01',
+      location_id__in: this.props.location_id,
+      chart_type: 'TableChart'
     }
 
-    const trendChart = (
-        <Chart type='LineChart'
-          data={data.trendData}
-          loading={loading}
-          options={{color: ['#000000']
-        }} />
-    )
+    api.datapoints(query).then(response => {
+      this.setState({tableData: response.objects})
+    })
+  },
+
+  render () {
+    let tableChart = ''
+    if (this.state.tableData && this.state.indicatorIndex.length > 0) {
+      const indicator_ids = this.props.dashboard.charts[0].indicators
+      const tableIndicators = indicator_ids.map(id => { return this.state.indicatorIndex[id] })
+      const chart_options = {
+        cellFontSize: 14,
+        cellSize: 36,
+        chartInDashboard: true,
+        color: null,
+        defaultSortOrder: [this.props.location.name],
+        fontSize: 14,
+        headers: tableIndicators,
+        indicatorsSelected: tableIndicators,
+        margin: {bottom: 40, left: 40, right: 40, top: 40},
+        // parentLocationMap: Object
+        xDomain: tableIndicators.map(indicator => { return indicator.short_name }),
+        xFormat: ",.0f",
+        yFormat: ",.0f"
+      }
+      tableChart = ChartFactory('TableChart', React.findDOMNode(this), this.state.tableData, chart_options)
+      console.log('tableChart', tableChart)
+      // tableChart = <Chart type='TableChart' data={this.state.tableData} options={chart_options} loading={loading} />
+    }
+
+    const data = this.props.data
+    const loading = this.props.loading
+    const trendChart = <Chart type='LineChart' data={data.trendData} loading={loading} />
     const mapChart = (
       <Chart type='ChoroplethMap'
         data={data.mapData}
         loading={loading}
         options={{
-          aspect: 0.6,
-          domain: _.constant([0, 0.1]),
-          value: _.property('properties[21]'),
-          color: colorScale,
-          onClick: d => { DashboardActions.navigate({ location: d }) }
-        }}/>
+        aspect: 0.6,
+        domain: _.constant([0, 0.1]),
+        value: _.property('properties[21]'),
+        //  bubbleValue: _.property('properties[177]'),
+        //  stripeValue: _.property('properties[203]'),
+        //  xFormat: d3.format(',.1%'),
+        onClick: d => { DashboardActions.navigate({ location: d }) }
+      }}/>
     )
 
     return (
@@ -86,4 +123,4 @@ var EocCampaign = React.createClass({
   }
 })
 
-export default EocCampaign
+export default EocPreCampaign
