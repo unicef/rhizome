1aa37336a332ee3d7b7a19d5134e450007023699
Dima Markus <dmi3000@gmail.com>
2016-03-03 17:35:25 -0500

    Add refactor Table Chart Component

---
+++ webapp/src/02-molecules/charts_d3/utils/palettes.js
@@ -0,0 +1,14 @@
+export default {
+  orange: [
+    '#2D2525', '#AF373E', '#D95449', '#F58667', '#FABAA2', '#FEE7DC'
+  ],
+  blue: [
+    '#F0F0F0', '#CBCBCB', '#C5D9DD', '#D5E5F2', '#E1E6EC', '#A5A5A5'
+  ],
+  rainbow: [
+    'indianred', 'coral', 'gold', 'limegreen', 'turquoise', 'royalblue'
+  ]
+  // rainbow: [
+  //   'indianred', 'coral', 'gold', 'limegreen', 'turquoise', 'royalblue', 'darkviolet'
+  // ]
+}
