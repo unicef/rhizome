a4283fcce184f8fd81d5867feb33614a0f872a19
John Dingee <dingeej@gmail.com>
2016-03-04 11:48:38 +0430

    fix table chart call for provinces

--- rhizome/api/resources/datapoint.py
+++ rhizome/api/resources/datapoint.py
@@ -118,6 +118,7 @@ class DatapointResource(BaseNonModelResource):
             pass
 
         if chart_type == 'TableChart':
+
             sub_location_ids = list(Location.objects\
                 .filter(parent_location_id__in = self.location_ids,
                     lpd_status__in=[1,2])\
@@ -130,7 +131,7 @@ class DatapointResource(BaseNonModelResource):
                     .filter(lpd_status__in=[1,2])\
                     .values_list('parent_location_id',flat=True).distinct())
 
-                self.location_ids.extend(sub_location_ids)
+            self.location_ids.extend(sub_location_ids)
 
 
         # Pivot the data on request instead of caching ##
