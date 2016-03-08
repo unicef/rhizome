fb52188559f8feb033d7308c14fcabb8a9758664
Dima Markus <dmi3000@gmail.com>
2016-03-08 18:00:21 -0500

    Create query string parser

--- webapp/src/utilities/parsers/path.js
+++ webapp/src/utilities/parsers/path.js
@@ -295,3 +295,15 @@ exports.set = function (obj, path, val) {
   }
   return true
 }
+
+const getParamFromQueryString = function (name, url) {
+  if (!url) url = window.location.href
+  name = name.replace(/[\[\]]/g, '\\$&')
+  const regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)')
+  const results = regex.exec(url)
+  if (!results) return null
+  if (!results[2]) return ''
+  return decodeURIComponent(results[2].replace(/\+/g, ' '))
+}
+
+export default getParamFromQueryString
