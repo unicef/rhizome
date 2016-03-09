cbc3a74f4e1b72a8d8ab488ad362bf076699cc03
Dima Markus <dmi3000@gmail.com>
2016-03-09 15:32:36 -0500

    Break up chart wizard into smaller components

--- webapp/src/components/pages/ChartWizard.jsx
+++ webapp/src/components/pages/ChartWizard.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {PropTypes} from 'react'
 import Reflux from 'reflux'
 import moment from 'moment'
 import api from 'data/api'
@@ -6,15 +6,14 @@ import api from 'data/api'
 import DateRangePicker from 'components/molecules/DateRangePicker'
 import DownloadButton from 'components/molecules/DownloadButton'
 import DatabrowserTable from 'components/molecules/DatabrowserTable'
-import TitleInput from 'components/molecules/TitleInput'
 import List from 'components/molecules/list/List'
 import ReorderableList from 'components/molecules/list/ReorderableList'
 import DropdownMenu from 'components/molecules/menus/DropdownMenu'
 import Chart from 'components/molecules/Chart'
 
+import ChartDataSelect from 'components/organisms/chart-wizard/ChartDataSelect'
+import ChartProperties from 'components/organisms/chart-wizard/ChartProperties'
 import PreviewScreen from 'components/organisms/chart-wizard/PreviewScreen'
-import ChartSelect from 'components/organisms/chart-wizard/ChartSelect'
-import PalettePicker from 'components/organisms/chart-wizard/preview/PalettePicker'
 
 import ChartWizardActions from 'actions/ChartWizardActions'
 import DataFiltersStore from 'stores/DataFiltersStore'
@@ -37,21 +36,19 @@ const defaultChartDef = {
 
 let ChartWizard = React.createClass({
   propTypes: {
-    chartDef: React.PropTypes.object,
-    save: React.PropTypes.func,
-    cancel: React.PropTypes.func
+    chartDef: PropTypes.object,
+    chart_id: PropTypes.number,
+    save: PropTypes.func,
+    cancel: PropTypes.func
   },
 
   mixins: [Reflux.connect(ChartWizardStore, 'data'), Reflux.connect(DataFiltersStore, 'raw_data')],
 
   componentDidMount () {
     if (this.props.chart_id) {
-      ChartAPI.getChart(this.props.chart_id).then(function(response){
-        ChartWizardActions.initialize(response)
-      })
+      ChartAPI.getChart(this.props.chart_id).then(response => ChartWizardActions.initialize(response))
     } else {
       this.chartDef = this.props.chartDef || defaultChartDef
-      console.log('im runnin', this.chartDef)
       ChartWizardActions.initialize(this.chartDef)
     }
   },
@@ -68,24 +65,10 @@ let ChartWizard = React.createClass({
         chart_json: JSON.stringify(this.state.data.chartDef)
       }
       api.post_chart(chart).then(res => {
-        window.location.replace("/charts/" + res.objects.id);
+        window.location.replace('/charts/' + res.objects.id)
       }, res => {
         console.log('update chart error,', res)
       })
-    })
-  },
-
-  toggleStep (refer) {
-    return () => {
-      this.setState({
-        refer: refer
-      })
-    }
-  },
-
-  setLocationSearch: function (pattern) {
-    this.setState({
-      locationSearch: pattern
     })
   },
 
@@ -103,138 +86,77 @@ let ChartWizard = React.createClass({
   },
 
   render: function () {
-    let availableIndicators = this.state.data.indicatorList
-    let palette = this.state.data.chartDef.palette || 'orange'
-    let startDate = moment()
-    let endDate = moment()
+    const data = this.state.data
+    const chartDef = data.chartDef
+    const start_date = chartDef ? moment(chartDef.startDate, 'YYYY-MM-DD').toDate() : moment()
+    const end_date = chartDef ? moment(chartDef.endDate, 'YYYY-MM-DD').toDate() : moment()
 
-    if (this.state.data.chartDef) {
-      startDate = moment(this.state.data.chartDef.startDate, 'YYYY-MM-DD').toDate()
-      endDate = moment(this.state.data.chartDef.endDate, 'YYYY-MM-DD').toDate()
-    }
-
-    if (!this.state.data.chartDef.type) {
+    if (!chartDef.type)
       return null
     }
 
-    let chart = (
+    const download_button = <DownloadButton onClick={this._downloadRawData} enable={data.rawData} text='Download Raw Data' working='Downloading' cookieName='dataBrowserCsvDownload'/>
+
+    const chart = (
       <Chart
-          id='custom-chart'
-          type={this.state.data.chartDef.type}
-          data={this.state.data.chartData}
-          options={this.state.data.chartOptions}
-          campaings={this.state.data.campaignFilteredList}
-          defaultCampaign={this.state.data.campaign}
-          />
+        id='custom-chart'
+        type={chartDef.type}
+        data={data.chartData}
+        options={data.chartOptions}
+        campaings={data.campaignFilteredList}
+        defaultCampaign={data.campaign}
+      />
     )
 
-    let location_options = [
-      { title: 'by Status', value: this.state.data.location_lpd_statuses },
-      { title: 'by Country', value: this.state.data.locationFilteredList }
+    const location_options = [
+      { title: 'by Status', value: data.location_lpd_statuses },
+      { title: 'by Country', value: data.locationFilteredList }
     ]
-
-    let clear_locations_button = ''
-    if (this.state.data.selectedLocations.length > 3) {
-      clear_locations_button = <a className='remove-filters-link' onClick={ChartWizardActions.clearSelectedLocations}>Remove All </a>
-    }
-
-    let clear_indicators_button = ''
-    if (this.state.data.selectedIndicators.length > 3) {
-      clear_indicators_button = <a className='remove-filters-link' onClick={ChartWizardActions.clearSelectedIndicators}>Remove All </a>
-    }
-
-    let call_to_action = <button className='right button success' disabled={!this.state.data.canDisplayChart} onClick={this.saveChart} ><i className='fa fa-save'></i> Save Chart</button>
-    if (this.state.data.chartDef.type === 'RawData') {
-      call_to_action = <DownloadButton onClick={this._downloadRawData} enable={this.state.data.rawData} text='Download Raw Data' working='Downloading' cookieName='dataBrowserCsvDownload'/>
-    }
-
-    let data_output = (
-      <PreviewScreen isLoading={this.state.data.isLoading}>
-        {this.state.data.canDisplayChart ? chart : (<div className='empty'>No Data</div>) }
-      </PreviewScreen>
-    )
-
-    if (this.state.data.chartDef.type === 'RawData') {
-      data_output = <DatabrowserTable data={this.state.data.rawData} selectedLocations={this.state.data.selectedLocations} selected_indicators={this.state.data.selectedIndicators} />
-    }
-
-    let title_input = <TitleInput save={ChartWizardActions.editTitle}/>
-    if (this.state.data.title) {
-      console.log('this.state.data', this.state.data)
-      title_input = <TitleInput initialText={this.state.data.title} save={ChartWizardActions.editTitle}/>
-    }
-
 
     return (
       <section className='chart-wizard'>
         <h1 className='medium-12 columns text-center'>Explore Data</h1>
         <div className='row'>
-          <div className='medium-3 columns'>
-            <div>
-              <h3>Time</h3>
-              <DateRangePicker
-                sendValue={ChartWizardActions.updateDateRangePicker}
-                start={startDate}
-                end={endDate}
-                fromComponent='ChartWizard' />
-              <br/>
-            </div>
-            <div className='row data-filters'>
-              <br/>
-              <div className='medium-6 columns'>
-                  <h3>
-                    Indicators
-                    <DropdownMenu
-                      items={availableIndicators}
-                      sendValue={ChartWizardActions.addIndicator}
-                      item_plural_name='Indicators'
-                      style='icon-button right'
-                      icon='fa-plus' />
-                  </h3>
-                  {clear_indicators_button}
-                  <ReorderableList items={this.state.data.selectedIndicators} removeItem={ChartWizardActions.removeIndicator} dragItem={ChartWizardActions.reorderIndicator} />
-              </div>
-              <div className='medium-6 columns'>
-                <h3>
-                  Locations
-                  <DropdownMenu
-                    items={location_options}
-                    sendValue={ChartWizardActions.addLocation}
-                    item_plural_name='Locations'
-                    style='icon-button right'
-                    icon='fa-plus'
-                    grouped/>
-                </h3>
-                {clear_locations_button}
-                <List items={this.state.data.selectedLocations} removeItem={ChartWizardActions.removeLocation} />
-                <div id='locations' placeholder='0 selected' multi='true' searchable='true' className='search-button'></div>
-              </div>
-            </div>
-          </div>
+          <ChartDataSelect
+            start_date={start_date}
+            end_date={end_date}
+            all_indicators={data.indicatorList}
+            all_locations={location_options}
+            selected_indicators={data.selectedIndicators}
+            selected_locations={data.selectedLocations}
+            addLocation={ChartWizardActions.addLocation}
+            removeLocation={ChartWizardActions.removeLocation}
+            addIndicator={ChartWizardActions.addIndicator}
+            reorderIndicator={ChartWizardActions.reorderIndicator}
+            removeIndicator={ChartWizardActions.removeIndicator}
+            clearSelectedIndicators={ChartWizardActions.clearSelectedIndicators}
+            clearSelectedLocations={ChartWizardActions.clearSelectedLocations}
+            setDateRange={ChartWizardActions.updateDateRangePicker}
+          />
           <div className='medium-9 columns'>
-              { data_output }
+            {
+              chartDef.type === 'RawData'
+              ? <DatabrowserTable
+                  data={data.rawData}
+                  selected_locations={data.selectedLocations}
+                  selected_indicators={data.selectedIndicators}
+                />
+              : <PreviewScreen isLoading={data.isLoading}>
+                  {data.canDisplayChart ? chart : (<div className='empty'>No Data</div>) }
+                </PreviewScreen>
+            }
           </div>
         </div>
-        <footer className='row'>
-          <div className='medium-6 columns'>
-            <h3>Chart Type</h3>
-            <ChartSelect charts={this.state.data.chartTypeFilteredList} value={this.state.data.chartDef.type}
-                onChange={ChartWizardActions.changeChart}/>
-          </div>
-          <div className='medium-2 columns'>
-            <h3>Color Scheme</h3>
-            <PalettePicker value={palette} onChange={ChartWizardActions.changePalette}/>
-          </div>
-          <div className='medium-4 columns'>
-            <div className='medium-8 columns'>
-              <h3>Chart Title</h3>
-              { title_input }
-            </div>
-            <div className='medium-4 columns'>
-              { call_to_action }
-            </div>
-          </div>
-        </footer>
+        <ChartProperties
+          selected_chart_type={data.chartDef.type}
+          selected_palette={data.chartDef.palette}
+          chart_title={data.title}
+          selectChartType={ChartWizardActions.changeChart}
+          selectPalette={ChartWizardActions.changePalette}
+          saveTitle={ChartWizardActions.editTitle}
+          saveChart={this.saveChart}
+          chartIsReady={!data.canDisplayChart}
+        />
       </section>
     )
   }
