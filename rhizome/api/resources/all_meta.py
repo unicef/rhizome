0947c5af96edd75a41f4ddaef00a185fc355f766
mepoole <marthaepoole@gmail.com>
2016-03-23 13:09:33 -0400

    removing name and id from all_meta api call

--- rhizome/api/resources/all_meta.py
+++ rhizome/api/resources/all_meta.py
@@ -6,12 +6,9 @@ from rhizome.api.serialize import CustomSerializer
 
 class AllMetaResult(object):
 	content = list()
-	name = unicode()
-	id= int()
+
 
 class AllMetaResource(BaseNonModelResource):
-    id = fields.IntegerField(attribute='id')
-    name = fields.CharField(attribute='name')
     content = fields.ListField(attribute='content')
 
     class Meta(BaseNonModelResource.Meta):
@@ -27,8 +24,6 @@ class AllMetaResource(BaseNonModelResource):
     	all_meta_objs ={'indicators': Indicator, 'campaigns':Campaign, 'locations':Location}
     	for idx, (meta_name, meta_resource) in enumerate(all_meta_objs.iteritems()):
             am_result = AllMetaResult()
-            am_result.name = meta_name
             am_result.content = meta_resource.objects.all().values()
-            am_result.id = idx
             qs.append(am_result)
     	return qs
