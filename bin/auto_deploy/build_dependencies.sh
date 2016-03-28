0ad1d9ab6026ce26e10d5c3bb876e3ca773ab358
Ersan <smokecode@gmail.com>
2016-03-28 14:37:29 -0400

    fixed commit mistake

--- bin/auto_deploy/build_dependencies.sh
+++ bin/auto_deploy/build_dependencies.sh
@@ -1,10 +1,13 @@
 cd webapp &&
 npm install &&
-cd webapp &&
 npm run package &&
 mkdir -p ~/deploy/rhizome-work &&
 mv dist/rhizome.zip ~/deploy/rhizome-work/rhizome.zip &&
 cd ~/deploy/rhizome-work &&
 unzip -o rhizome.zip -d /var/www/apps/rhizome/ &&
+<<<<<<< HEAD
 cd ../var/www/apps/rhizome/ &&
+=======
+cd /var/www/apps/rhizome/ &&
+>>>>>>> parent of 3dfdc92... Revert "script clean up"
 sudo rm -rf `find . -name "*.pyc*"`
