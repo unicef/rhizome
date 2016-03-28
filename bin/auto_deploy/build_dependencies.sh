49a9782e8e515188ec77b79abfc2573cfadff215
Ersan <smokecode@gmail.com>
2016-03-28 14:39:54 -0400

    fixed merge

--- bin/auto_deploy/build_dependencies.sh
+++ bin/auto_deploy/build_dependencies.sh
@@ -5,9 +5,5 @@ mkdir -p ~/deploy/rhizome-work &&
 mv dist/rhizome.zip ~/deploy/rhizome-work/rhizome.zip &&
 cd ~/deploy/rhizome-work &&
 unzip -o rhizome.zip -d /var/www/apps/rhizome/ &&
-<<<<<<< HEAD
-cd ../var/www/apps/rhizome/ &&
-=======
 cd /var/www/apps/rhizome/ &&
->>>>>>> parent of 3dfdc92... Revert "script clean up"
 sudo rm -rf `find . -name "*.pyc*"`
