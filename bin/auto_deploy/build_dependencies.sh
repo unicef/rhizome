472f8a3c7caf77dae565df8ae2c7f95c2820ef27
Ersan <smokecode@gmail.com>
2016-03-29 10:57:33 -0400

    push npm package to s3

--- bin/auto_deploy/build_dependencies.sh
+++ bin/auto_deploy/build_dependencies.sh
@@ -1,9 +1,10 @@
-cd webapp &&
-npm install &&
-npm run package &&
+# cd webapp &&
+# npm install &&
+# # npm run package &&
 mkdir -p ~/deploy/rhizome-work &&
 mv dist/rhizome.zip ~/deploy/rhizome-work/rhizome.zip &&
 cd ~/deploy/rhizome-work &&
+mkdir -p /var/www/apps/rhizome/ &&
 unzip -o rhizome.zip -d /var/www/apps/rhizome/ &&
 cd /var/www/apps/rhizome/ &&
 sudo rm -rf `find . -name "*.pyc*"`
