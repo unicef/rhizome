2c556c87918f57e63456cd1f67e912686ddc6c5c
Dima Markus <dmi3000@gmail.com>
2016-03-31 13:36:44 -0400

    Create dropdown icon component

---
+++ webapp/src/components/atoms/DropdownIcon.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+
+var DropdownIcon = React.createClass({
+
+  mixins: [
+    require('components/molecules/menus/MenuControl')
+  ],
+
+  propTypes: {
+    className: React.PropTypes.string,
+    text: React.PropTypes.string.isRequired,
+    icon: React.PropTypes.string,
+    searchable: React.PropTypes.bool,
+    onSearch: React.PropTypes.func
+  },
+
+  getDefaultProps: function () {
+    return {
+      icon: 'fa-bars'
+    }
+  },
+
+  componentWillReceiveProps: function (nextProps) {
+    if (nextProps.text !== this.props.text) {
+      this.setState({ open: false })
+    }
+  },
+
+  render: function () {
+    return (
+      <button className='button icon-button remove-chart-button' onClick={this._toggleMenu}>
+        <i className={'fa ' + this.props.icon}/>&nbsp;
+      </button>
+    )
+  }
+})
+
+export default DropdownIcon
