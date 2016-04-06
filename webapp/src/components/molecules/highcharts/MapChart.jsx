29d926d513b73e4b8945e9f0cd5d97712e85f7f2
John Dingee <dingeej@gmail.com>
2016-04-06 18:22:40 -0400

    fix join condition for geo / datapoint merge into high charts

--- webapp/src/components/molecules/highcharts/MapChart.jsx
+++ webapp/src/components/molecules/highcharts/MapChart.jsx
@@ -2,20 +2,16 @@
 
 import HighChart from 'components/molecules/highcharts/HighChart'
 
-import map_data from './afghan_map'
-
 class MapChart extends HighChart {
 
   constructor (props) {
     super(props)
-    console.log('props: ', props)
 
     const chart_data = this.props.datapoints.meta.chart_data
-
-    console.log('high_chart_map_data: ', map_data)
-
     const db_map_data = {'features': this.props.features, 'type': 'FeatureCollection'}
+
     console.log('rh maps : ', db_map_data)
+    console.log('chart_data : ', chart_data)
 
     this.data = {
       title: {
@@ -36,7 +32,7 @@ class MapChart extends HighChart {
       series: [{
         data: chart_data, // pass transformed datapoints \\
         mapData: db_map_data, // map parameters
-        joinBy: 'hc-key',
+        joinBy: 'location_id',
         name: 'Random data',
         states: {
           hover: {
