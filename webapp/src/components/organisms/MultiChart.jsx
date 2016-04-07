6993e8b5ed7476e81ea6cafcecb9ac9a87768895
Dima Markus <dmi3000@gmail.com>
2016-04-07 13:07:18 -0400

    Break MultiChart apart into components

--- webapp/src/components/organisms/MultiChart.jsx
+++ webapp/src/components/organisms/MultiChart.jsx
@@ -1,25 +1,11 @@
 import _ from 'lodash'
-import React, {PropTypes} from 'react'
-import {DropdownList} from 'react-widgets'
+import React from 'react'
 import Reflux from 'reflux'
-import moment from 'moment'
 
-import RadioGroup from 'react-radio-group'
-
-import IconButton from 'components/atoms/IconButton'
-import ExportIcon from 'components/atoms/ExportIcon'
-import ColorSwatch from 'components/atoms/ColorSwatch'
 import ChartTypeSelector from 'components/molecules/ChartTypeSelector'
-
-import palettes from 'components/molecules/charts/utils/palettes'
 import ChartSelector from 'components/molecules/ChartSelector'
-import CampaignSelector from 'components/molecules/CampaignSelector'
-import IndicatorSelector from 'components/molecules/IndicatorSelector'
-import LocationSelector from 'components/molecules/LocationSelector'
 import DatabrowserTable from 'components/molecules/DatabrowserTable'
-import DateRangePicker from 'components/molecules/DateRangePicker'
 import Placeholder from 'components/molecules/Placeholder'
-import TitleInput from 'components/molecules/TitleInput'
 import TableChart from 'components/molecules/charts/TableChart'
 import BarChart from 'components/molecules/highcharts/BarChart'
 import ColumnChart from 'components/molecules/highcharts/ColumnChart'
@@ -27,27 +13,23 @@ import TrendChart from 'components/molecules/highcharts/TrendChart'
 import MapChart from 'components/molecules/highcharts/MapChart'
 import ChoroplethMap from 'components/molecules/charts/ChoroplethMap'
 
+import MultiChartControls from 'components/organisms/MultiChartControls'
+import MultiChartHeader from 'components/organisms/MultiChartHeader'
+
 import ChartStore from 'stores/ChartStore'
 import LocationStore from 'stores/LocationStore'
 import IndicatorStore from 'stores/IndicatorStore'
 import CampaignStore from 'stores/CampaignStore'
 import RootStore from 'stores/RootStore'
 
-import ChartActions from 'actions/ChartActions'
-
 const MultiChart = React.createClass({
+
   mixins: [
     Reflux.connect(ChartStore, 'all_charts'),
     Reflux.connect(LocationStore, 'locations'),
     Reflux.connect(CampaignStore, 'campaigns'),
     Reflux.connect(IndicatorStore, 'indicators')
   ],
-
-  getInitialState () {
-    return {
-      titleEditMode: false
-    }
-  },
 
   componentDidMount () {
     RootStore.listen(() => {
@@ -68,13 +50,6 @@ const MultiChart = React.createClass({
     return chart_data || nextProps.chart.loading || missing_params
   },
 
-  _toggleTitleEdit (title) {
-    if (_.isString(title)) {
-      this.props.setTitle(title)
-    }
-    this.setState({titleEditMode: !this.state.titleEditMode})
-  },
-
   getChartComponentByType (type) {
     if (type === 'TableChart') {
       return <TableChart {...this.props.chart} />
@@ -93,31 +68,19 @@ const MultiChart = React.createClass({
 
   render () {
     console.info('MultiChart.'+ this.props.chart.title +'.RENDER ==========================================')
-    const chart = this.props.chart
-    const start_date = chart ? moment(chart.start_date, 'YYYY-MM-DD').toDate() : moment()
-    const end_date = chart ? moment(chart.end_date, 'YYYY-MM-DD').toDate() : moment()
-    const disableSave = _.isEmpty(chart.selected_locations) || _.isEmpty(chart.selected_indicators)
-
+    const props = this.props
+    const chart = props.chart
 
     // CHART
     // ---------------------------------------------------------------------------
-    const title_bar = this.state.titleEditMode ?
-      <TitleInput initialText={chart.title} save={this._toggleTitleEdit}/>
-      :
-      <h2>
-        <a onClick={this._toggleTitleEdit} style={{cursor: 'text'}}>
-          {chart.title || 'Untitled Chart'}
-        </a>
-      </h2>
-
     const chart_type_selector = (
       <div className='medium-10 medium-centered text-center columns' style={{position: 'relative', marginTop: '-1.5rem', padding: '4rem 0'}}>
         <h4>View Data As</h4>
-        <ChartTypeSelector onChange={this.props.setType}/>
+        <ChartTypeSelector onChange={props.setType}/>
         <br />
         <h4>or</h4>
         <br />
-        <ChartSelector charts={this.state.all_charts.raw} selectChart={this.props.selectChart} />
+        <ChartSelector charts={this.state.all_charts.raw} selectChart={props.selectChart} />
       </div>
     )
 
@@ -128,133 +91,6 @@ const MultiChart = React.createClass({
         selected_indicators={chart.selected_indicators}
       />
       : this.getChartComponentByType(chart.type)
-
-
-    // ACTION ICONS
-    // ---------------------------------------------------------------------------
-    const change_type_button = (
-      <IconButton
-        icon='fa-bar-chart'
-        text='Change chart type'
-        onClick={this.props.toggleSelectTypeMode}
-        className='left'
-      />
-    )
-    const export_button = (
-      <ExportIcon exportPath={'/charts/' + chart.id}/>
-    )
-    const duplicate_chart_button = this.props.duplicateChart ? (
-      <IconButton
-        icon='fa-copy'
-        text='Duplicate'
-        onClick={() => this.props.duplicateChart(chart.uuid)}
-      />
-    ) : null
-    const save_button = (
-      <IconButton
-        icon='fa-save'
-        text='Save'
-        onClick={() => this.props.saveChart(chart.uuid)}
-      />
-    )
-    const remove_chart_button = this.props.removeChart ? (
-      <IconButton
-        icon='fa-times'
-        text='Remove'
-        onClick={() => this.props.removeChart(chart.uuid)}
-      />
-    ) : null
-
-    // SIDEBAR
-    // ---------------------------------------------------------------------------
-    const date_range_picker = chart.type === 'LineChart' || chart.type === 'RawData' ? (
-      <div className='medium-12 columns'>
-        <h3>Time</h3>
-        <DateRangePicker
-          sendValue={this.props.setDateRange}
-          start={start_date}
-          end={end_date}
-          fromComponent='MultiChart'
-        />
-        <br/>
-      </div>
-    ) : null
-
-    const group_by_selector = chart.type === 'LineChart' ? (
-      <div className='medium-12 columns' style={{position: 'absolute', bottom: 0}}>
-        <RadioGroup name={'groupBy' + chart.uuid} selectedValue={chart.groupBy} onChange={this.props.setGroupBy}>
-          {Radio => (
-            <div>
-              <Radio value='indicator' /> Multiple Indicators
-              <span>&nbsp;&nbsp;</span>
-              <Radio value='location' /> Multiple Locations
-            </div>
-          )}
-        </RadioGroup>
-      </div>
-    ) : null
-
-    const campaign_selector = chart.type !== 'LineChart' && chart.type !== 'RawData' ? (
-      <CampaignSelector
-        campaigns={this.state.campaigns}
-        selected_campaigns={chart.selected_campaigns}
-        selectCampaign={this.props.selectCampaign}
-        deselectCampaign={this.props.deselectCampaign}
-        setCampaigns={this.props.setCampaigns}
-        linkCampaigns={this.props.linkCampaigns}
-        classes='medium-12 columns'
-        linked={chart.linkedCampaigns}
-      />
-    ) : ''
-
-    const multiIndicator = chart.type === 'TableChart' || chart.type === 'RawData'
-    const multiLocation = chart.type === 'TableChart' || chart.type === 'RawData'
-    const groupByIndicator = chart.type === 'LineChart'  && chart.groupBy === 'location'
-    const groupByLocation = chart.type === 'LineChart'  && chart.groupBy === 'indicator'
-
-    const location_selector = (
-      <LocationSelector
-        locations={this.state.locations}
-        selected_locations={chart.selected_locations}
-        selectLocation={this.props.selectLocation}
-        deselectLocation={this.props.deselectLocation}
-        setLocations={this.props.setLocations}
-        clearSelectedLocations={this.props.clearSelectedLocations}
-        classes={multiLocation && !groupByIndicator ? 'medium-6 columns' : 'medium-12 columns'}
-        multi={multiLocation || groupByIndicator}
-      />
-    )
-
-    const indicator_selector = (
-      <IndicatorSelector
-        indicators={this.state.indicators}
-        selected_indicators={chart.selected_indicators}
-        selectIndicator={this.props.selectIndicator}
-        setIndicators={this.props.setIndicators}
-        deselectIndicator={this.props.deselectIndicator}
-        clearSelectedIndicators={this.props.clearSelectedIndicators}
-        reorderIndicator={this.props.reorderIndicator}
-        classes={multiIndicator && !groupByLocation ? 'medium-6 columns' : 'medium-12 columns'}
-        multi={multiIndicator || groupByLocation}
-        avoidBooleans={chart.type === 'LineChart'}
-      />
-    )
-
-    const palette_selector = chart.type !== 'RawData'  && chart.type !== 'LineChart' ? (
-      <div className='medium-12 columns'>
-        <h3>Colors</h3>
-        <DropdownList
-          data={ _.map(palettes, (key, value) => ({colors: key, value: value}) )}
-          textField='value'
-          valueField='value'
-          value={chart.palette}
-          itemComponent={ColorSwatch}
-          valueComponent={ColorSwatch}
-          onChange={item => this.props.setPalette(item.value)}
-        />
-        <br />
-      </div>
-    ) : null
 
     // PLACEHOLDERS
     // ---------------------------------------------------------------------------
@@ -273,32 +109,9 @@ const MultiChart = React.createClass({
 
     return (
       <article className='multi-chart'>
-        <header className='row'>
-          <div className='medium-4 large-3 medium-push-8 large-push-9 columns text-right chart-actions'>
-            <div className='hide-for-medium-up'>
-              { change_type_button }
-            </div>
-            { export_button }
-            { duplicate_chart_button }
-            { save_button }
-            { remove_chart_button }
-          </div>
-          <div className='medium-8 large-9 medium-pull-4 large-pull-3 columns chart-header text-center'>
-            <div className='hide-for-small'>
-              { change_type_button }
-            </div>
-            { title_bar }
-          </div>
-        </header>
+        <MultiChartHeader {...props}/>
         <section className='row'>
-          <aside className='medium-4 large-3 medium-push-8 large-push-9 columns'>
-            { palette_selector }
-            { date_range_picker }
-            { campaign_selector }
-            { indicator_selector }
-            { location_selector }
-            { group_by_selector }
-          </aside>
+          <MultiChartControls {...props} className='medium-4 large-3 medium-push-8 large-push-9 columns' />
           <div className='medium-8 large-9 medium-pull-4 large-pull-3 columns chart-zone'>
             {
               chart.selectTypeMode
