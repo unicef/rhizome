c7467111696075f7109a170cc40fd2acba42b658
Ersan <smokecode@gmail.com>
2016-04-15 21:44:56 -0400

    remove logs

--- webapp/tasks/package.js
+++ webapp/tasks/package.js
@@ -6,24 +6,12 @@ const TASK_NAME = 'package'
 function packageTask () {
   let counter = 1
   gulp.autoRegister(TASK_NAME, config => {
-    console.log('Iteration: ', counter)
-    console.log('=============================\n')
-    console.log('config.src: ', config.src)
-    console.log('=============================\n')
-    console.log('config.options.filename: ', config.options.filename)
-    console.log('=============================\n')
-    console.log('config.dest: ', config.dest)
-    console.log('=============================')
     return gulp.src(config.src)
       .pipe(zip(config.options.filename))
       .pipe(gulp.dest(config.dest))
   })
 }
-console.log('Running TASK_NAME:', TASK_NAME)
-console.log('\n')
-console.log('packageTask', packageTask)
-console.log('\n')
-console.log('=============================')
+
 gulp.task(TASK_NAME, packageTask)
 
 export default packageTask
