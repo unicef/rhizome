dc96f46871db8f3247b83f76dfe330f754049ada
Ersan <smokecode@gmail.com>
2016-04-18 18:10:57 -0400

    correct key path

--- bin/auto_deploy/build_dependencies.sh
+++ bin/auto_deploy/build_dependencies.sh
@@ -1,7 +1,5 @@
-pwd
-npm install &&
 mkdir -p ~/deploy/rhizome-work &&
-mv dist/rhizome.zip ~/deploy/rhizome-work/rhizome.zip &&
+mv rhizome.zip ~/deploy/rhizome-work/rhizome.zip &&
 cd ~/deploy/rhizome-work &&
 mkdir -p /var/www/apps/rhizome/ &&
 unzip -o rhizome.zip -d /var/www/apps/rhizome/ &&
