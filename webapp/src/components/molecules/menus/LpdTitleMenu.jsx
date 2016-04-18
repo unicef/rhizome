e783dea7c6b87ab5c22663a77d7a85d3afc12635
Dima Markus <dmi3000@gmail.com>
2016-04-18 12:52:35 -0400

    Update lpd dropdown

--- webapp/src/components/molecules/menus/LpdTitleMenu.jsx
+++ webapp/src/components/molecules/menus/LpdTitleMenu.jsx
@@ -3,7 +3,14 @@ import React from 'react'
 import TitleMenu from 'components/molecules/menus/TitleMenu'
 import TitleMenuItem from 'components/molecules/menus/TitleMenuItem'
 
-var LpdTitleMenu = React.createClass({
+const filters = [
+  { id: 1, value: 0, name: 'All LPDs', type: 'lpd_status' },
+  { id: 2, value: 1, name: 'LPD 1', type: 'lpd_status' },
+  { id: 3, value: 2, name: 'LPD 2', type: 'lpd_status' },
+  { id: 4, value: 3, name: 'LPD 3', type: 'lpd_status' }
+]
+
+const LpdTitleMenu = React.createClass({
   propTypes: {
     selected: React.PropTypes.object.isRequired,
     sendValue: React.PropTypes.func.isRequired,
@@ -14,18 +21,11 @@ var LpdTitleMenu = React.createClass({
     return {
       statuses: [],
       idsToRender: [],
-      selected: {'name':'Loading ...'}
+      selected: filters[0]
     }
   },
 
   render () {
-    const filters = [
-      { id: 1, value: 0, name: 'All', type: 'lpd_status' },
-      { id: 2, value: 1, name: 'LPD 1', type: 'lpd_status' },
-      { id: 3, value: 2, name: 'LPD 2', type: 'lpd_status' },
-      { id: 4, value: 3, name: 'LPD 3', type: 'lpd_status' }
-    ]
-
     const selected_text = !this.props.selected ? 'LPD Status' : this.props.selected.name
 
     const indicator_menu_items = filters.map(filter =>
