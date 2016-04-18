a11960434da42c20aab77e6bda50b318b1d8782a
Ersan <smokecode@gmail.com>
2016-04-18 12:54:01 -0400

    Revert "find current path, comment rest"
    This reverts commit be27f2418752e9c10cc41a61e897484239af7718.

--- bin/auto_deploy/build_dependencies.sh
+++ bin/auto_deploy/build_dependencies.sh
@@ -1,9 +1,9 @@
-pwd
-# npm install &&
-# mkdir -p ~/deploy/rhizome-work &&
-# mv dist/rhizome.zip ~/deploy/rhizome-work/rhizome.zip &&
-# cd ~/deploy/rhizome-work &&
-# mkdir -p /var/www/apps/rhizome/ &&
-# unzip -o rhizome.zip -d /var/www/apps/rhizome/ &&
-# cd /var/www/apps/rhizome/ &&
-# sudo rm -rf `find . -name "*.pyc*"`
+cd webapp &&
+npm install &&
+mkdir -p ~/deploy/rhizome-work &&
+mv dist/rhizome.zip ~/deploy/rhizome-work/rhizome.zip &&
+cd ~/deploy/rhizome-work &&
+mkdir -p /var/www/apps/rhizome/ &&
+unzip -o rhizome.zip -d /var/www/apps/rhizome/ &&
+cd /var/www/apps/rhizome/ &&
+sudo rm -rf `find . -name "*.pyc*"`
--- bin/auto_deploy/start_apache.sh
+++ bin/auto_deploy/start_apache.sh
@@ -1,1 +1,1 @@
-# sudo /etc/init.d/apache2 start
+sudo /etc/init.d/apache2 start
