499f73c0e526f7efd7c9e93aa1b525f02e3d2bfc
Ersan <smokecode@gmail.com>
2016-04-19 10:53:03 -0400

    add full path for home rather than ~

--- bin/auto_deploy/build_dependencies.sh
+++ bin/auto_deploy/build_dependencies.sh
@@ -1,5 +1,5 @@
-mkdir -p ~/deploy/rhizome-work &&
-cd ~/deploy/rhizome-work &&
+mkdir -p /home/ubuntu/deploy/rhizome-work &&
+cd /home/ubuntu/deploy/rhizome-work &&
 mkdir -p /var/www/apps/rhizome/ &&
 unzip -o rhizome.zip -d /var/www/apps/rhizome/ &&
 cd /var/www/apps/rhizome/ &&
