aa4f13208caae81d417b5c7ae5eb315619e96fbb
Ersan <smokecode@gmail.com>
2016-04-19 10:37:14 -0400

    fix bucket path to work dir

--- bin/auto_deploy/build_dependencies.sh
+++ bin/auto_deploy/build_dependencies.sh
@@ -1,7 +1,7 @@
 mkdir -p ~/deploy/rhizome-work &&
-mv rhizome.zip ~/deploy/rhizome-work/rhizome.zip &&
 cd ~/deploy/rhizome-work &&
 mkdir -p /var/www/apps/rhizome/ &&
 unzip -o rhizome.zip -d /var/www/apps/rhizome/ &&
 cd /var/www/apps/rhizome/ &&
+cd webapp && npm install && cd ..
 sudo rm -rf `find . -name "*.pyc*"`
