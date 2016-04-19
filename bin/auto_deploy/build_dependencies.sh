6e6a128964bd69b1680fd706dbb154c1ba7e17a4
Ersan <smokecode@gmail.com>
2016-04-19 19:41:11 -0400

    small refactor autodeploy

--- bin/auto_deploy/build_dependencies.sh
+++ bin/auto_deploy/build_dependencies.sh
@@ -2,6 +2,6 @@ mkdir -p /home/ubuntu/deploy/rhizome-work &&
 cd /home/ubuntu/deploy/rhizome-work &&
 mkdir -p /var/www/apps/rhizome/ &&
 unzip -o rhizome.zip -d /var/www/apps/rhizome/ &&
-cd /var/www/apps/rhizome/ &&
-cd webapp && npm install && cd ..
+cd /var/www/apps/rhizome/webapp &&
+npm install && cd ..
 sudo rm -rf `find . -name "*.pyc*"`
