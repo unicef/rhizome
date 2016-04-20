798dddb861356ccad3fb6c4d2c79c656b51cf483
mepoole <marthaepoole@gmail.com>
2016-04-20 13:22:36 -0400

    reverting change

--- rhizome/migrations/0014_base_dashboards.py
+++ rhizome/migrations/0014_base_dashboards.py
@@ -26,53 +26,53 @@ def ingest_post_campaign():
 
     chart_0 = CustomChart.objects.create(
         title = 'Post Campaign Table',
-        chart_json = '{\
-            "end_date":"2016-02-29",\
-            "indicator_ids":[28,34,21,29,15,30,32,33],\
-            "campaign_ids":[5],\
-            "location_ids":[33],\
-            "type":"TableChart"\
-            ,"start_date":"2016-02-01"\
-        }',
+        chart_json = {
+            "end_date":"2016-02-29",
+            "indicator_ids":[28,34,21,29,15,30,32,33],
+            "campaign_ids":[5],
+            "location_ids":[33],
+            "type":"TableChart"
+            ,"start_date":"2016-02-01"
+        },
         uuid= '9b38b92e-ee11-4034-8cca-a73cece00927'
     )
 
     chart_1 = CustomChart.objects.create(
         title = 'LQAS Assesment',
-        chart_json = '{\
-            "end_date":"2016-03-01",\
-            "indicator_ids":[29],\
-            "campaign_ids":[5],\
-            "location_ids":[33],\
-            "type":"MapChart",\
-            "start_date":"2016-02-01"\
-        }',
+        chart_json = {
+            "end_date":"2016-03-01",
+            "indicator_ids":[29],
+            "campaign_ids":[5],
+            "location_ids":[33],
+            "type":"MapChart",
+            "start_date":"2016-02-01"
+        },
         uuid= '000fef60-61de-4104-9ad9-69c3c9fd6634'
     )
 
     chart_2 = CustomChart.objects.create(
         title = 'Missed Children Trend ( PCA / Out of House Survey )',
-        chart_json = '{\
-            "end_date":"2016-03-01",\
-            "indicator_ids":[34,21],\
-            "campaign_ids":[5],\
-            "location_ids":[157],\
-            "type":"LineChart",\
-            "start_date":"2015-02-01"\
-        }',
+        chart_json = {
+            "end_date":"2016-03-01",
+            "indicator_ids":[34,21],
+            "campaign_ids":[5],
+            "location_ids":[157],
+            "type":"LineChart",
+            "start_date":"2015-02-01"
+        },
         uuid = 'e2a6e9ac-78be-4def-bfe1-b978f726f447'
     )
 
     chart_3 = CustomChart.objects.create(
         title = 'PCA Percent Missed Children',
-        chart_json = '{\
-            "end_date":"2016-03-01",\
-            "indicator_ids":[34],\
-            "campaign_ids":[5],\
-            "location_ids":[33],\
-            "type":"MapChart",\
-            "start_date":"2016-02-01"\
-        }',
+        chart_json = {
+            "end_date":"2016-03-01",
+            "indicator_ids":[34],
+            "campaign_ids":[5],
+            "location_ids":[33],
+            "type":"MapChart",
+            "start_date":"2016-02-01"
+        },
         uuid = '5582a06b-fa1a-4c91-bf72-ffca6e30cf57'
     )
 
