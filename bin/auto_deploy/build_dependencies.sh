3f434159f1a3f69110e5a2eaa04f6189a59c1bb5
Ersan <smokecode@gmail.com>
2016-04-20 12:14:46 -0400

    reset path each script

--- bin/auto_deploy/build_dependencies.sh
+++ bin/auto_deploy/build_dependencies.sh
@@ -2,5 +2,5 @@ mkdir -p /home/ubuntu/deploy/rhizome-work &&
 cd /home/ubuntu/deploy/rhizome-work &&
 mkdir -p /var/www/apps/rhizome/ &&
 unzip -o rhizome.zip -d /var/www/apps/rhizome/ &&
-cd /var/www/apps/rhizome/
+cd /var/www/apps/rhizome/ &&
 sudo rm -rf `find . -name "*.pyc*"`
--- bin/auto_deploy/run_tests.sh
+++ bin/auto_deploy/run_tests.sh
@@ -1,2 +1,3 @@
+cd /var/www/apps/rhizome/ &&
 coverage run manage.py test --settings=rhizome.settings.test &&
 coverage html --omit='*venv/*,*migrations/*,*admin*,*manage*,*wsgi*,*__init__*,*test*,*settings*,*url*' -i
