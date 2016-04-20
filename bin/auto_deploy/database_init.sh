296bf28c1acecbbaa433a7677ffcaf556d6c3716
Ersan <smokecode@gmail.com>
2016-04-20 13:02:07 -0400

    update appspec and scripts to deploy properly

--- bin/auto_deploy/database_init.sh
+++ bin/auto_deploy/database_init.sh
@@ -1,5 +1,6 @@
-cd /var/www/apps/rhizome/ &&
-python manage.py syncdb --settings=settings &&
-python manage.py migrate --settings=settings &&
-source ../environment_seed.env &&
+cd /var/www/apps/rhizome/
+rm -rf `find . -name "*.pyc*"`
+python manage.py syncdb --settings=settings
+python manage.py migrate --settings=settings
+source ../environment_seed.env
 python manage.py collectstatic --noinput --settings=settings
