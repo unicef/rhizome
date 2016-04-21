894c97c2b6380b025b06344c6590b4bc93b40ced
Ersan <smokecode@gmail.com>
2016-04-21 16:07:03 -0400

    fixed boolean display text on update

--- webapp/src/components/atoms/EditableTableCell.jsx
+++ webapp/src/components/atoms/EditableTableCell.jsx
@@ -44,18 +44,22 @@ let EditableTableCell = React.createClass({
 
   exitEditMode: function (event) {
     if (event.type === 'blur' || event.keyCode === 13) { // Keycode for 'Enter' key
-      let displayValue = event.target.value
-      if (this.props.field.schema.data_format === 'pct') {
-        const pctIndex = event.target.value.indexOf('%')
-        displayValue = pctIndex === -1 ? event.target.value : event.target.value.slice(0, pctIndex)
-        displayValue = format.autoFormat(displayValue/100, this.props.field.schema.data_format, 2)
-        event.target.value = event.target.value/100
-      }
-      if (displayValue !== this.display_value) {
+      if (this._shouldUpdateCell(event)) {
         this.updateCellValue(event.target.value)
       }
       this.setState({editMode: false})
     }
+  },
+
+  _shouldUpdateCell: function(event){
+    let displayValue = event.target.value
+    if (this.props.field.schema.data_format === 'pct') {
+      const pctIndex = event.target.value.indexOf('%')
+      displayValue = pctIndex === -1 ? event.target.value : event.target.value.slice(0, pctIndex)
+      displayValue = format.autoFormat(displayValue/100, this.props.field.schema.data_format, 2)
+      event.target.value = event.target.value/100
+    }
+    return displayValue !== this.display_value
   },
 
   updateCellValue: function (new_value) {
@@ -94,8 +98,9 @@ let EditableTableCell = React.createClass({
         this.hasError = true
         if (!this.isBool) { this.setState({editMode: false}) }
       })
-      this.display_value = format.autoFormat(query_params.value, this.props.field.schema.data_format, 2)
     }
+    this.display_value = this.isBool ? new_value : format.autoFormat(new_value, this.props.field.schema.data_format, 2)
+
     this.forceUpdate()
   },
 
