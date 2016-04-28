734e0223ced56084a71f3a81dbf2e2f9f9f124d2
Ersan <smokecode@gmail.com>
2016-04-28 15:04:52 -0400

    add path for stores tests

---
+++ webapp/src/stores/__tests__/RootStore.spec.js
@@ -0,0 +1,12 @@
+import { expect } from 'chai'
+
+// import RootStore from '../../stores/RootStore.js'
+
+describe (__filename, () => {
+  describe ('.listenables', () => {
+    it.skip ('has RootActions assigned to listenables', () => {
+      // expect(listenables).to.not.be.undefined
+      expect(true).to.be.eql(false)
+    })
+  })
+})
