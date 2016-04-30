d61d940b076766147ffdd99f91f9f7da9b4c7bb6
Ersan <smokecode@gmail.com>
2016-04-30 15:46:55 -0400

    dashboard chart store spec init

---
+++ webapp/src/stores/__tests__/DashboardChartsStore.spec.js
@@ -0,0 +1,11 @@
+import Reflux from 'reflux'
+import { expect } from 'chai'
+import DashboardChartsStore from '../DashboardChartsStore'
+
+describe.skip (__filename, () => {
+  // var someVar = new DashboardChartsStore()
+  // console.log('something', something)
+  it ('is able to be imported', () => {
+    expect (DashboardChartsStore).to.exist
+  })
+})
