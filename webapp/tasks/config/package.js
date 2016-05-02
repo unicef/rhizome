c65364e205d3ba36975e708951bdb75e85cb2011
Ersan <smokecode@gmail.com>
2016-05-02 18:31:29 -0400

    keep migration data file path

--- webapp/tasks/config/package.js
+++ webapp/tasks/config/package.js
@@ -8,7 +8,7 @@ export default {
     'manage.py',
     'appspec.yml',
     'requirements.txt',
-    'migration_data/*.*',
+    'migration_data*/**/*.*',
     'settings.py',
     '*.xlsx',
     '*.csv'
