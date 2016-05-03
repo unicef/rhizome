ef44abc4276ba1ad712d58aef8e313f30786cecb
Ersan <smokecode@gmail.com>
2016-05-03 08:28:57 -0400

    final config for deploy

--- bin/auto_deploy/database_init.sh
+++ bin/auto_deploy/database_init.sh
@@ -1,10 +1,10 @@
 cd /var/www/apps/rhizome/
 find . -name \*.pyc -exec rm -rf {}\;
 source venv/bin/activate
-sudo pip install --upgrade -r requirements.txt
+pip install -r requirements.txt
 export PYTHONPATH="/var/www/apps/rhizome/rhizome"
-python manage.py syncdb --settings=settings
 source ../environment_seed.env
+python manage.py syncdb --settings=settings
 # python manage.py migrate --settings=settings
 python manage.py collectstatic --noinput --settings=settings
 deactivate
