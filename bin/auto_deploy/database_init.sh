a880f3a2299ecd999ba505c4c3910196eded547b
Ersan <smokecode@gmail.com>
2016-05-03 12:40:41 -0400

    remove venv autodeploy

--- bin/auto_deploy/database_init.sh
+++ bin/auto_deploy/database_init.sh
@@ -1,10 +1,6 @@
 cd /var/www/apps/rhizome/
 find . -name \*.pyc -exec rm -rf {}\;
-source venv/bin/activate
 pip install -r requirements.txt
-export PYTHONPATH="/var/www/apps/rhizome/rhizome"
-source ../environment_seed.env
 python manage.py syncdb --settings=settings
 # python manage.py migrate --settings=settings
 python manage.py collectstatic --noinput --settings=settings
-deactivate
