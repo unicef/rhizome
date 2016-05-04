c60b623bc4941fce71c1594db7a554d5ba42a5d4
Dima Markus <dmi3000@gmail.com>
2016-05-04 16:32:41 -0400

    Move export button in all Highcharts

--- webapp/src/components/molecules/highcharts/themes.js
+++ webapp/src/components/molecules/highcharts/themes.js
@@ -3,7 +3,24 @@ export default {
     credits: { enabled: false },
     chart: {
       style: {
-        fontFamily: "'proxima', 'Helvetica', sans-serif' "
+        fontFamily: "'proxima', 'Helvetica', sans-serif' ",
+        paddingTop: '20px'  // Make room for buttons
+      }
+    },
+    exporting: {
+      buttons: {
+        contextButton: {
+          symbol: null,
+          text: 'Export',
+          x: -20,
+          y: -30,
+          theme: {
+            style: {
+              color: '#039',
+              textDecoration: 'underline'
+            }
+          }
+        }
       }
     },
     title: ''
