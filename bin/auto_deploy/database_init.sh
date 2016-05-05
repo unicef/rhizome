d395133e879d32559e6a8d1dae6feceb80b62855
Ersan <smokecode@gmail.com>
2016-05-05 18:48:01 -0400

    update pyc delete

--- bin/auto_deploy/database_init.sh
+++ bin/auto_deploy/database_init.sh
@@ -1,5 +1,5 @@
 cd /var/www/apps/rhizome/
-find . -name \*.pyc -exec rm -rf {}\;
+find . -name '*.pyc' -exec rm -rf {} \;
 pip install -r requirements.txt
 python manage.py syncdb --settings=settings
 # python manage.py migrate --settings=settings
