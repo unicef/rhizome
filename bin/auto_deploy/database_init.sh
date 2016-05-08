aa3e0ca4f575a7e975e2cb931b5bc761d16c67a6
Ersan <smokecode@gmail.com>
2016-05-08 11:00:39 -0400

    moved settings.py to outer folder

--- bin/auto_deploy/database_init.sh
+++ bin/auto_deploy/database_init.sh
@@ -1,5 +1,6 @@
 cd /var/www/apps/rhizome/
 sudo find . -name \*.pyc -type f -exec rm -f {} \;
+cp /var/www/apps/settings.py /var/www/apps/rhizome/settings.py
 pip install -r requirements.txt
 python manage.py syncdb --settings=settings
 # python manage.py migrate --settings=settings
