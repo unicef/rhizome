0d8c2eab9cbd00eba29f01acf9782d8e761de972
Ersan <smokecode@gmail.com>
2016-05-09 14:01:24 -0400

    tests for fetch completed

--- webapp/src/stores/__tests__/LocationStore.spec.js
+++ webapp/src/stores/__tests__/LocationStore.spec.js
@@ -1,5 +1,5 @@
 import { expect } from 'chai'
-import { getLocations } from '../../utilities/test/mockData'
+import { getMockResponse } from '../../utilities/test/mockData'
 import LocationStore from '../LocationStore'
 
 describe (__filename,()=>{
@@ -11,18 +11,7 @@ describe (__filename,()=>{
       expect (LocationStore.locations).to.exist
     })
     it ('initializes with proper default values', () => {
-      const locations = {
-                          meta: null,
-                          raw: null,
-                          index: null,
-                          filtered: [],
-                          list: [],
-                          lpd_statuses: [
-                            {value: 'lpd-1', 'title': 'LPD 1', location_ids: []},
-                            {value: 'lpd-2', 'title': 'LPD 2', location_ids: []},
-                            {value: 'lpd-3', 'title': 'LPD 3', location_ids: []}
-                          ]
-                        }
+      const locations = getEmptyLocations()
       expect (LocationStore.locations).to.deep.equal(locations)
     })
   })
@@ -31,18 +20,7 @@ describe (__filename,()=>{
       expect (LocationStore.getInitialState).to.exist
     })
     it ('returns locations property of class', () => {
-      const locations = {
-                          meta: null,
-                          raw: null,
-                          index: null,
-                          filtered: [],
-                          list: [],
-                          lpd_statuses: [
-                            {value: 'lpd-1', 'title': 'LPD 1', location_ids: []},
-                            {value: 'lpd-2', 'title': 'LPD 2', location_ids: []},
-                            {value: 'lpd-3', 'title': 'LPD 3', location_ids: []}
-                          ]
-                        }
+      const locations = getEmptyLocations()
       expect (LocationStore.getInitialState()).to.deep.equal(locations)
     })
   })
@@ -62,8 +40,43 @@ describe (__filename,()=>{
     it ('exists', () => {
       expect (LocationStore.onFetchLocationsCompleted).to.exist
     })
-    context.skip ('when passed a response parameter', () => {
-      // it ('')
+    context ('when passed a response parameter', () => {
+      const response = getMockResponse()
+      it ('updates meta property of state', () => {
+        expect(() => { LocationStore.onFetchLocationsCompleted(response) }).to.change(LocationStore.state, 'meta')
+      })
+      it.skip ('updates raw property of state', () => {
+        expect(() => { LocationStore.onFetchLocationsCompleted(response) }).to.change(LocationStore.state, 'raw')
+      })
+      it ('updates index property of state', () => {
+        expect(() => { LocationStore.onFetchLocationsCompleted(response) }).to.change(LocationStore.state, 'index')
+      })
+      it ('updates list property of state', () => {
+        expect(() => { LocationStore.onFetchLocationsCompleted(response) }).to.change(LocationStore.state, 'list')
+      })
+      it ('updates filtered property of state', () => {
+        expect(() => { LocationStore.onFetchLocationsCompleted(response) }).to.change(LocationStore.state, 'filtered')
+      })
+    })
+  })
+  describe ('#setLocationLpdStatuses()', () => {
+    it ('exists', () => {
+
     })
   })
 })
+
+function getEmptyLocations () {
+  return {
+          meta: null,
+          raw: null,
+          index: null,
+          filtered: [],
+          list: [],
+          lpd_statuses: [
+            {value: 'lpd-1', 'title': 'LPD 1', location_ids: []},
+            {value: 'lpd-2', 'title': 'LPD 2', location_ids: []},
+            {value: 'lpd-3', 'title': 'LPD 3', location_ids: []}
+          ]
+        }
+}
