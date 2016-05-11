2d1779dd7e1e9017999fa82a6cf27d6e5d0057b1
Ersan <smokecode@gmail.com>
2016-05-11 15:49:29 -0400

    root test folder

---
+++ webapp/src/components/__tests__/dropdowns/Dropdown.spec.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { expect } from 'chai'
+import { shallow } from 'enzyme'
+import Dropdown from '../dropdown/Dropdown'
+import sinon from 'sinon'
+
+describe ('Dropdown', () => {
+  it ('exists', () => {
+    expect (Dropdown).to.exist
+  })
+})
