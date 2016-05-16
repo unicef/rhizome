b242f4ad96810b507caf4a977fd5f3e825105064
Dima Markus <dmi3000@gmail.com>
2016-05-16 11:14:05 -0400

    Update datapoint actions to use new flat datapoints

--- webapp/src/actions/DatapointActions.js
+++ webapp/src/actions/DatapointActions.js
@@ -1,8 +1,5 @@
-import _ from 'lodash'
 import Reflux from 'reflux'
 import api from 'utilities/api'
-
-import builderDefinitions from 'components/d3chart/utils/builderDefinitions'
 
 const DatapointActions = Reflux.createActions({
   'fetchDatapoints': { children: ['completed', 'failed'], asyncResult: true },
@@ -23,11 +20,11 @@ DatapointActions.fetchDatapoints.listen(params => {
 // ---------------------------------------------------------------------------
 const _prepDatapointsQuery = (params) => {
   const type = params.type
-  const needsLocationChildren = _.indexOf(builderDefinitions.multi_location_charts, params.type) !== -1
   let query = {
     campaign__in: params.campaign__in || params.campaign_ids,
     indicator__in: params.indicator_ids,
-    location_id__in: !needsLocationChildren || !type ? params.location_ids : null,
+    location_id__in: params.location_ids,
+    location_depth: params.location_depth,
     campaign_start: params.start_date,
     campaign_end: params.end_date,
     chart_type: params.type,
@@ -35,7 +32,6 @@ const _prepDatapointsQuery = (params) => {
     show_missing_data: params.show_missing_data,
     group_by_time: params.group_by_time,
     source_name: params.source_name,
-    parent_location_id__in: needsLocationChildren ? params.location_ids : null,
     filter_indicator: params.indicator_filter ? params.indicator_filter.type : null,
     filter_value: params.indicator_filter ? params.indicator_filter.value : null,
     location_level: type === 'TableChart' ? 'District' : null
