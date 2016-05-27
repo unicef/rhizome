83430dbadbbe3711327a2e3fd5f4a0a3b3d02190
Ersan <smokecode@gmail.com>
2016-05-27 13:24:08 -0400

    partial coverage

--- webapp/src/components/__tests__/highchart/HighChart.spec.js
+++ webapp/src/components/__tests__/highchart/HighChart.spec.js
@@ -2,42 +2,9 @@ import React from 'react'
 import { expect } from 'chai'
 import { render, shallow } from 'enzyme'
 import HighChart from '../../highchart/HighChart'
-
-describe ('HighChart', () => {
-  it ('exists', () => {
-    expect (HighChart).to.exist
-  })
-  describe.skip ('.propTypes', () => {
-    it ('exists', () => {
-      expect (HighChart.propTypes).to.exist
-    })
-    it ('has the correct keys', () => {
-      expect (HighChart.propTypes).to.have.all.keys('values', 'value', 'name', 'prefix', 'title', 'onChange', 'horizontal')
-    })
-  })
-  describe.skip ('.getDefaultProps()', () => {
-    it ('exists', () => {
-      expect (HighChart.getDefaultProps).to.exist
-    })
-    it ('has proper keys and correct value', () => {
-      expect (HighChart.getDefaultProps()).to.have.all.keys('prefix', 'horizontal')
-      expect (HighChart.getDefaultProps().prefix).to.eq('')
-      expect (HighChart.getDefaultProps().horizontal).to.be.false
-    })
-  })
-  describe.skip ('#render()', () => {
-    it ('exists', () => {
-      const mockDropdownMenuItem = new HighChart()
-      expect (mockDropdownMenuItem.render).to.exist
-    })
-    it ('renders correct jsx', () => {
-      const props = HighChartTest.getProps()
-      const actualComponent = shallow(<HighChart {...props}/>).debug()
-      const expectedComponent = shallow(HighChartTest.getComponent()).debug()
-      expect (actualComponent).to.equal(expectedComponent)
-    })
-  })
-})
+import Highcharts from 'highcharts'
+import themes from 'components/highchart/themes'
+import sinon from 'sinon'
 
 class HighChartTest {
   static getProps() {
@@ -53,8 +20,7 @@ class HighChartTest {
   }
   static getDefaultProps () {
     return {
-      prefix: '',
-      horizontal: false
+      isPureConfig: true
     }
   }
   static getComponent() {
@@ -79,3 +45,77 @@ class HighChartTest {
     )
   }
 }
+
+describe ('HighChart', () => {
+  let mockHighChart
+  beforeEach(() => {
+    mockHighChart = shallow(<HighChart />).instance()
+  })
+  it ('exists', () => {
+    expect (HighChart).to.exist
+  })
+  describe ('#constructor()', () => {
+    it ('has a constructor which has 1 parameter', () => {
+      expect (HighChart.constructor).to.exist.and.have.lengthOf(1)
+    })
+    context ('given an argument', () => {
+      const props = HighChartTest.getProps()
+      it ('props passed should also be passed up to super', () => {
+        const spy = sinon.spy(React.Component.prototype, 'constructor')
+        const spyMockDropdown = new HighChart(props)
+        React.Component.prototype.constructor.restore()
+        expect (spy.called).to.be.true
+        expect (spy.calledWith(props)).to.be.true
+      })
+      it ('calls Highcharts.setOptions with correct argument', () => {
+        const spy = sinon.spy(Highcharts, 'setOptions')
+        const spyMockDropdown = new HighChart(props)
+        Highcharts.setOptions.restore()
+        expect (spy.called).to.be.true
+        expect (spy.calledWith(themes.standard)).to.be.true
+      })
+    })
+  })
+
+  describe ('.propTypes', () => {
+    it ('exists', () => {
+      expect (HighChart.propTypes).to.exist
+    })
+    it ('has the correct keys', () => {
+      expect (HighChart.propTypes).to.have.all.keys('config', 'map', 'isPureConfig', 'neverReflow')
+    })
+  })
+  describe ('.defaultProps', () => {
+    it ('exists', () => {
+      expect (HighChart.defaultProps).to.exist
+    })
+    it ('has proper keys and correct value', () => {
+      const defaultProps = HighChartTest.getDefaultProps()
+      expect (HighChart.defaultProps).to.have.all.keys('isPureConfig')
+      expect (HighChart.defaultProps.isPureConfig).to.eq(defaultProps.isPureConfig)
+    })
+  })
+  describe.skip ('componentDidMount', () => {
+    it ('exists', () => {
+      expect (mockHighChart.componentDidMount).to.exist
+    })
+    it ('calls renderChart()', () => {
+      const spy = sinon.spy(mockHighChart, 'renderChart')
+      mockHighChart.componentDidMount()
+      mockHighChart.renderChart.restore()
+      expect (spy.calledOnce).to.exist
+    })
+  })
+  describe.skip ('#render()', () => {
+    it ('exists', () => {
+      const mockDropdownMenuItem = new HighChart()
+      expect (mockDropdownMenuItem.render).to.exist
+    })
+    it ('renders correct jsx', () => {
+      const props = HighChartTest.getProps()
+      const actualComponent = shallow(<HighChart {...props}/>).debug()
+      const expectedComponent = shallow(HighChartTest.getComponent()).debug()
+      expect (actualComponent).to.equal(expectedComponent)
+    })
+  })
+})
