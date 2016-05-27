f41421494246d6aeaf955c20a6da5a50c12878d5
John Dingee <dingeej@gmail.com>
2016-05-27 11:56:32 -0400

    moving datapoint / raw_datapoint methods to base.py

--- rhizome/api/resources/datapoint.py
+++ rhizome/api/resources/datapoint.py
@@ -1,11 +1,9 @@
 import numpy as np
 import sys
-import itertools
 from pandas import DataFrame, pivot_table, notnull, concat
 from django.http import HttpResponse
 
 from tastypie import fields
-from tastypie.utils.mime import build_content_type
 from tastypie.exceptions import NotFound
 
 from rhizome.api.serialize import CustomSerializer
@@ -35,16 +33,6 @@ class DatapointResource(BaseModelResource):
 
     class Meta(BaseModelResource.Meta):
         '''
-        As this is a NON model resource, we must specify the object_class
-        that will represent the data returned to the applicaton.  In this case
-        as specified by the ResultObject the fields in our response will be
-        location_id, campaign_id, indcator_json.
-        The resource name is datapoint, which means this resource is accessed
-        by /api/v1/datapoint/.
-        The data is serialized by the CustomSerializer which uses the default
-        handler for JSON responses and transforms the data to csv when the
-        user clicks the "download data" button on the data explorer.
-        note - authentication inherited from parent class
         '''
 
         resource_name = 'datapoint'  # cooresponds to the URL of the resource
@@ -57,30 +45,22 @@ class DatapointResource(BaseModelResource):
 
         super(DatapointResource, self).__init__(*args, **kwargs)
 
-        self.error = None
-        self.parsed_params = None
-
-    def create_response(self, request, data, response_class=HttpResponse,
-                        **response_kwargs):
+    def get_list(self, request, **kwargs):
         """
-        THis is overridden from tastypie.  The point here is to be able to
-        Set the content-disposition header for csv downloads.  That is the only
-        instance in which this override should change the response is if the
-        desired format is csv.
-        The content-disposition header allows the user to save the .csv
-        to a directory of their chosing.
+        Overriden from Tastypie..
         """
-        desired_format = self.determine_format(request)
-        serialized = self.serialize(request, data, desired_format)
 
-        response = response_class(content=serialized,
-            content_type=build_content_type(desired_format),**response_kwargs)
+        base_bundle = self.build_bundle(request=request)
+        objects = self.obj_get_list(bundle=base_bundle)
 
-        if desired_format == 'text/csv':
-            response['Content-Disposition'] = 'attachment; filename=polio_data.csv'
-            response.set_cookie('dataBrowserCsvDownload', 'true')
+        response_meta = self.get_datapoint_response_meta(request, objects)
+        response_data = {
+            'objects': objects,
+            'meta': response_meta,
+            'error': None,
+        }
 
-        return response
+        return self.create_response(request, response_data)
 
     def obj_get_list(self, bundle, **kwargs):
         '''
@@ -128,160 +108,30 @@ class DatapointResource(BaseModelResource):
 
         return results
 
-    def get_response_meta(self, request, objects):
-        '''
-        If there is an error for this resource, add that to the response.  If
-        there is no error, than add this key, but set the value to null.  Also
-        add the total_count to the meta object as well
-        '''
-
-        meta = {}
-
-        try:
-            location_ids = request.GET['location_id__in']
-            meta['location_ids'] = location_ids
-        except KeyError:
-            location_ids = None
-
-        try:
-            indicator_ids = request.GET['indicator__in']
-            meta['indicator_ids'] = indicator_ids
-        except KeyError:
-            indicator_ids = None
-
-        try:
-            chart_uuid = request.GET['chart_uuid']
-            meta['chart_uuid'] = chart_uuid
-        except KeyError:
-            indicator_ids = None
-
-        meta['campaign_ids'] = self.parsed_params['campaign__in']
-        meta['total_count'] = len(objects)
-
-        return meta
-
-    def dehydrate(self, bundle):
-        '''
-        This method allws me to remove or add information to each data object,
-        for instance the resource_uri.
-        '''
-
-        bundle.data.pop('resource_uri')
-
-        return bundle
-
-    # #########################
-    # ### HELPER METHODS #####
-    # #########################
-
-    def parse_url_params(self, query_dict):
-        '''
-        For the query dict return another dictionary ( or error ) in accordance
-        to the expected ( both required and optional ) parameters in the request
-        URL.
-        '''
-        parsed_params = {}
-
-        required_params = {'indicator__in': None}
-
-        # try to find optional parameters in the dictionary. If they are not
-        # there return the default values ( given in the dict below)
-        optional_params = {
-            'the_limit': 10000, 'the_offset': 0, 'agg_level': 'mixed',
-            'campaign_start': '2012-01-01', 'campaign_end': '2900-01-01',
-            'campaign__in': None, 'location__in': None,'location_id__in':None,\
-            'filter_indicator':None, 'filter_value': None,\
-            'show_missing_data':None, 'cumulative':0, \
-             'group_by_time': None, 'chart_uuid': None
-        }
-
-        for k, v in optional_params.iteritems():
-            try:
-                parsed_params[k] = query_dict[k]
-            except KeyError:
-                parsed_params[k] = v
-
-        for k, v in required_params.iteritems():
-
-            try:
-                parsed_params[k] = [int(p) for p in query_dict[k].split(',')]
-            except KeyError as err:
-                err_msg = '%s is a required parameter!' % err
-                return err_msg, None
-
-        campaign_in_param = parsed_params['campaign__in']
-
-        if campaign_in_param:
-            campaign_ids = [int(c_id) for c_id in campaign_in_param.split(',')]
-
-
-        else:
-            campaign_ids = self.get_campaign_list(
-                parsed_params['campaign_start'], parsed_params['campaign_end']
-            )
-        self.campaign_qs = Campaign.objects.filter(id__in=campaign_ids)
-        parsed_params['campaign__in'] = campaign_ids
-
-        return parsed_params
-
-    def get_campaign_list(self, campaign_start, campaign_end):
-        '''
-        Based on the parameters passed for campaigns, start/end or __in
-        return to the parsed params dictionary a list of campaigns to query
-        '''
-
-        campaign_qs = Campaign.objects.filter(
-            start_date__gte=campaign_start,
-            start_date__lte=campaign_end,
-            top_lvl_location_id=self.top_lvl_location_id
-        )
-
-        return [c.id for c in campaign_qs]
-
     def build_class_indicator_map(self):
-        query_results = IndicatorClassMap.objects.filter(is_display=True) \
-            .values_list('indicator','enum_value','string_value')
+        '''
+        '''
         class_indicator_map ={}
-        for query in query_results:
-            if query[0] not in class_indicator_map:
-                class_indicator_map[query[0]] ={}
-            class_indicator_map[query[0]][query[1]] = query[2]
+
+        indicator_enum_list = IndicatorClassMap.objects.filter(is_display=True) \
+            .values_list('indicator','enum_value','string_value')
+
+        for row in indicator_enum_list:
+            if row[0] not in class_indicator_map:
+                class_indicator_map[row[0]] ={}
+            class_indicator_map[row[0]][row[1]] = row[2]
 
         return class_indicator_map
 
     def add_class_indicator_val(self, x):
+        '''
+        '''
+
         ind_id = x['indicator_id']
         ind_val = x['value']
+
         if ind_id in self.class_indicator_map and ind_val in self.class_indicator_map[ind_id]:
             new_val = self.class_indicator_map[ind_id][ind_val]
             x['value'] = new_val
+
         return x
-
-
-    def add_missing_data(self, df):
-        '''
-        If the campaign / locaiton cobination has no related datapoitns, we
-        add the keys here so that we can see the row of data in data entry
-        or data browser.
-
-        This in the future can be controlled with a parameter so that for
-        instance with a table chart for a large number of districts, we only
-        show those with data.
-
-        This is largely for Data entry so that we can see a row in the form
-        even when there is no existing data.
-        '''
-
-
-        list_of_lists = [self.parsed_params['indicator__in'], self.location_ids, self.parsed_params['campaign__in']]
-        cart_product = list(itertools.product(*list_of_lists))
-        cart_prod_df = DataFrame(cart_product)
-        if 'campaign_id' in df.columns:
-            columns_list = ['indicator_id','location_id', 'campaign_id']
-        else:
-            columns_list = ['indicator_id','location_id', 'time_grouping']
-
-        cart_prod_df.columns = columns_list
-        df = df.merge(cart_prod_df, how='outer', on=columns_list)
-
-        return df
