4e4c27d2918906faccec09727a5131a0c8504f5d
Dima Markus <dmi3000@gmail.com>
2016-06-06 17:00:44 -0400

    Update react_app routes

--- react_app/src/Routes.jsx
+++ react_app/src/Routes.jsx
@@ -36,45 +36,45 @@ import EnterDataPage from 'components/pages/EnterDataPage'
 import ManageSystemPage from 'components/pages/ManageSystemPage'
 
 const Routes = (
-	<Route path="/" component={BasePageContainer}>
+	<Route path="/react_app" component={BasePageContainer}>
 
 		// RESOURCE ROUTES
 		//---------------------------------------------------------
-	  <Route path="react_app/campaigns" component={CampaignsPage}/>
-	  <Route path="react_app/campaigns/:campaign_id" component={CampaignPage}/>
-	  <Route path="react_app/campaigns/:campaign_id/edit" component={CampaignEditPage}/>
+	  <Route path="/react_app/campaigns" component={CampaignsPage}/>
+	  <Route path="/react_app/campaigns/:campaign_id" component={CampaignPage}/>
+	  <Route path="/react_app/campaigns/:campaign_id/edit" component={CampaignEditPage}/>
 
-	  <Route path="react_app/new_charts" component={ChartsPage}/>
-	  <Route path="react_app/new_charts/:chart_id" component={ChartPage}/>
-	  <Route path="react_app/new_charts/:chart_id/edit" component={ChartEditPage}/>
+	  <Route path="/react_app/new_charts" component={ChartsPage}/>
+	  <Route path="/react_app/new_charts/:chart_id" component={ChartPage}/>
+	  <Route path="/react_app/new_charts/:chart_id/edit" component={ChartEditPage}/>
 
-	  <Route path="react_app/indicators" component={IndicatorsPage}/>
-	  <Route path="react_app/indicators/:indicator_id" component={IndicatorPage}/>
-	  <Route path="react_app/indicators/:indicator_id/edit" component={IndicatorEditPage}/>
+	  <Route path="/react_app/indicators" component={IndicatorsPage}/>
+	  <Route path="/react_app/indicators/:indicator_id" component={IndicatorPage}/>
+	  <Route path="/react_app/indicators/:indicator_id/edit" component={IndicatorEditPage}/>
 
-	  <Route path="react_app/locations" component={LocationsPage}/>
-	  <Route path="react_app/locations/:location_id" component={LocationPage}/>
-	  <Route path="react_app/locations/:location_id/edit" component={LocationEditPage}/>
+	  <Route path="/react_app/locations" component={LocationsPage}/>
+	  <Route path="/react_app/locations/:location_id" component={LocationPage}/>
+	  <Route path="/react_app/locations/:location_id/edit" component={LocationEditPage}/>
 
-	  <Route path="react_app/source_data" component={SourceDatasPage}/>
-	  <Route path="react_app/source_data/:source_data_id" component={SourceDataPage}/>
-	  <Route path="react_app/source_data/:source_data_id/edit" component={SourceDataEditPage}/>
+	  <Route path="/react_app/source_data" component={SourceDatasPage}/>
+	  <Route path="/react_app/source_data/:source_data_id" component={SourceDataPage}/>
+	  <Route path="/react_app/source_data/:source_data_id/edit" component={SourceDataEditPage}/>
 
-	  <Route path="react_app/users" component={UsersPage}/>
-	  <Route path="react_app/users/:user_id" component={UserPage}/>
-	  <Route path="react_app/users/:user_id/edit" component={UserEditPage}/>
+	  <Route path="/react_app/users" component={UsersPage}/>
+	  <Route path="/react_app/users/:user_id" component={UserPage}/>
+	  <Route path="/react_app/users/:user_id/edit" component={UserEditPage}/>
 
 		// INFO ROUTES
 		//---------------------------------------------------------
-	  <Route path="react_app/about" component={AboutPage}/>
-	  <Route path="react_app/bug_report" component={BugReportPage}/>
-	  <Route path="react_app/contact" component={ContactPage}/>
-	  <Route path="react_app/sitemap" component={SitemapPage}/>
+	  <Route path="/react_app/about" component={AboutPage}/>
+	  <Route path="/react_app/bug_report" component={BugReportPage}/>
+	  <Route path="/react_app/contact" component={ContactPage}/>
+	  <Route path="/react_app/sitemap" component={SitemapPage}/>
 
 		// OTHER ROUTES
 		//---------------------------------------------------------
-	  <Route path="react_app/enter_data" component={EnterDataPage}/>
-	  <Route path="react_app/manage_system" component={ManageSystemPage}/>
+	  <Route path="/react_app/enter_data" component={EnterDataPage}/>
+	  <Route path="/react_app/manage_system" component={ManageSystemPage}/>
 	</Route>
 )
 
