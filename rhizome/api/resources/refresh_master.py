f90e16e2c37e5b7b027b4268d4af0401a9096cde
John Dingee <dingeej@gmail.com>
2016-06-07 16:55:04 -0400

    catch exception in master refresh api call

--- rhizome/api/resources/refresh_master.py
+++ rhizome/api/resources/refresh_master.py
@@ -10,10 +10,10 @@ from rhizome.etl_tasks.refresh_master import MasterRefresh
 class RefreshMasterResource(BaseModelResource):
     '''
     **GET Request** Runs refresh master, and agg refresh for a given document
-        - *Required Parameters:* 
+        - *Required Parameters:*
             'document_id'
         - *Errors:*
-            returns 500 error if no document id is provided          
+            returns 500 error if no document id is provided
     '''
     class Meta(BaseModelResource.Meta):
         resource_name = 'refresh_master'
@@ -32,7 +32,9 @@ class RefreshMasterResource(BaseModelResource):
         doc_campaign_ids = set(list(DataPoint.objects
                                     .filter(source_submission__document_id=doc_id)
                                     .values_list('campaign_id', flat=True)))
-        for c_id in doc_campaign_ids:
-            AggRefresh(c_id)
+
+        if not None in doc_campaign_ids:
+            for c_id in doc_campaign_ids:
+                AggRefresh(c_id)
 
         return Document.objects.filter(id=doc_id).values()
