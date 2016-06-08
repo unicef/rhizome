4912cf26b9783fe96d9dacc7e5beb7dda62926a8
Dima Markus <dmi3000@gmail.com>
2016-06-08 14:08:11 -0400

    Update webpack config to work with our deployment process

--- react_app/webpack.config.js
+++ react_app/webpack.config.js
@@ -3,16 +3,45 @@ var webpack = require('webpack')
 
 module.exports = {
   devtool: 'eval',
-  entry: [
-    'webpack-dev-server/client?http://localhost:3000',
-    './src/index.jsx'
-  ],
+  // entry: [
+  //   'webpack-dev-server/client?http://localhost:3000',
+  //   './src/index.jsx'
+  // ],
+  entry: {
+    app: './src/index.jsx',
+    vendor: [
+      'ag-grid',
+      'ag-grid-react',
+      'axios',
+      'd3',
+      'highcharts',
+      'highcharts-more',
+      'lodash',
+      'moment',
+      'react',
+      'react-addons-test-utils',
+      'react-dom',
+      'react-layer',
+      'react-notification',
+      'react-nprogress',
+      'react-redux',
+      'react-reorder',
+      'react-router',
+      'react-router-redux',
+      'react-widgets',
+      'redbox-react',
+      'redux',
+      'redux-actions',
+      'redux-promise'
+    ]
+  },
   output: {
-    path: path.join(__dirname, '../webapp/public/static/js/'),
+    path: path.join(__dirname, '../webapp/src/assets/js/'),
     filename: 'reactApp.js',
-    publicPath: '../webapp/public/static/js/'
+    publicPath: '../webapp/src/assets/js/'
   },
   plugins: [
+    new webpack.optimize.CommonsChunkPlugin('vendor', 'vendor.reactApp.js'),
     new webpack.HotModuleReplacementPlugin(),
     new webpack.NoErrorsPlugin()
   ],
