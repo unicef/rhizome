3d4cc883850a85cef17c19e7e0150e7bc6c1faa6
Dima Markus <dmi3000@gmail.com>
2016-06-15 18:38:01 -0400

    Update dev env settings for hot module reloading

--- react_app/webpack.config.js
+++ react_app/webpack.config.js
@@ -6,6 +6,7 @@ module.exports = {
   entry: {
     app: [
       'webpack-dev-server/client?http://localhost:3000',
+      'webpack/hot/only-dev-server', // "only" prevents reload on syntax errors
       'babel-polyfill',
       './src/index.jsx'
     ],
@@ -38,12 +39,21 @@ module.exports = {
   output: {
     path: path.join(__dirname, '../webapp/public/static/js/'),
     filename: 'reactApp.js',
-    publicPath: '../webapp/public/static/js/'
+    publicPath: '/static/js/',
+    pathinfo: true
   },
   plugins: [
     new webpack.optimize.CommonsChunkPlugin('vendor', 'vendor.reactApp.js'),
     new webpack.HotModuleReplacementPlugin(),
     new webpack.NoErrorsPlugin()
+    // new webpack.DefinePlugin({
+    //   'process.env': { NODE_ENV: JSON.stringify(process.env.NODE_ENV || 'development2') }
+    // }),
+    // new webpack.DefinePlugin({
+    //   'process.env': {
+    //     'NODE_ENV': JSON.stringify('production')
+    //   }
+    // })
   ],
   resolve: {
     root: path.resolve(__dirname, 'src'),
@@ -52,8 +62,11 @@ module.exports = {
   module: {
     loaders: [{
       test: /\.jsx?$/,
-      loaders: ['babel'],
+      loaders: ['react-hot', 'babel'],
       include: path.join(__dirname, 'src')
+    }, {
+      test: /\.scss$/,
+      loaders: 'style!css'
     }]
   }
 }
