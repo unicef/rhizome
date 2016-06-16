013f2160b5ddd09b0be7067abfaeb8e0c2bdd6ae
John Dingee <dingeej@gmail.com>
2016-06-16 15:28:12 -0400

    put test arodnu queue_process

--- rhizome/api/resources/queue_process.py
+++ rhizome/api/resources/queue_process.py
@@ -1,8 +1,8 @@
-from rhizome.api.resources.base_model import BaseModelResource
-from rhizome.models import SourceSubmission, DocumentDetail
+from rhizome.api.resources.base_non_model import BaseNonModelResource
+from rhizome.models import SourceSubmission, DocumentDetail, Document
 
 
-class QueueProcessResource(BaseModelResource):
+class QueueProcessResource(BaseNonModelResource):
     '''
     **GET Request:** Sets SourceSubmission objects for a given document id to "to process"
             - *Required Parameters:*
@@ -10,16 +10,22 @@ class QueueProcessResource(BaseModelResource):
             - *Errors:*
                     returns a 500 error if a required parameter is not supplied
     '''
-    class Meta(BaseModelResource.Meta):
+    class Meta(BaseNonModelResource.Meta):
+        '''
+        '''
         resource_name = 'queue_process'
+        queryset = Document.objects.all().values()
+        default_limit = 1
+        GET_params_required = ['document_id']
 
-    def get_object_list(self, request):
-        document_id = request.GET['document_id']
+    def pre_process_data(self, request):
+        '''
+        update a document with the "to_process" process status.
 
-        SourceSubmission.objects.filter(
-            document_id=document_id).update(process_status='TO_PROCESS')
+        Don't need to catch the keyerror here on docuemnt_id because it is
+        already handled with the GET_params_required attribute on Meta
+        '''
 
-        queryset = DocumentDetail.objects \
-            .filter(document_id=document_id).values('id', 'doc_detail_type_id', 'doc_detail_type__name', 'document_id', 'doc_detail_value')
-
-        return queryset
+        SourceSubmission.objects\
+            .filter(document_id=request.GET['document_id'])\
+            .update(process_status='TO_PROCESS')
--- rhizome/tests/test_api_queue_process.py
+++ rhizome/tests/test_api_queue_process.py
@@ -1,7 +1,5 @@
 from base_test_case import RhizomeApiTestCase
-from rhizome.models import DocDetailType
-from rhizome.models import Document
-from rhizome.models import DocumentDetail
+from rhizome.models import Document, SourceSubmission
 from rhizome.tests.setup_helpers import TestSetupHelpers
 
 
@@ -19,25 +17,41 @@ class QueueProcessResourceTest(RhizomeApiTestCase):
             location_code='Nigeria',
             location_name='Nigeria')
 
-    def test_get(self):
-        # create a document, document_detail, source_submission
-        doc = Document.objects.create(doc_title='new doc, bro!')
-        doc_detail_type = DocDetailType.objects.create(name='some detail type')
-        doc_detail = DocumentDetail.objects.create(document_id=doc.id,
-                                                   doc_detail_type_id=doc_detail_type.id,
-                                                   doc_detail_value='something!')
+        # create a document, and one record for the soruce_submission #
+        self.doc = Document.objects.create(doc_title='new doc')
+        self.ss_obj = SourceSubmission.objects.create(
+            document_id = self.doc.id,
+            process_status = 'PROCESSED',
+            row_number = 0
+        )
 
-        data = {
-            'document_id': doc.id
-        }
+    def test_get(self):
+        '''
+        This method is meant to update the soruce_submission table so that
+        it is queued up for re-processing.
+
+        So to test that.. we create one source_submission, that says it is
+        processed, then we hit the api, and query again ensuring that the
+        processed_status = 'TO_PROCESS', that is.. that the API call did what
+        it was supposed to.
+        '''
+
+        data = { 'document_id': self.doc.id }
 
         resp = self.ts.get(self, '/api/v1/queue_process/', data=data)
-
         response_data = self.deserialize(resp)
         self.assertHttpOK(resp)
-        self.assertEqual(len(response_data['objects']), 1)
-        self.assertEqual(response_data['objects'][0]['id'], doc_detail.id)
+
+        new_ss_obj = SourceSubmission.objects.get(id = self.doc.id)
+        self.assertEqual(new_ss_obj.process_status, 'TO_PROCESS')
+
 
     def test_get_no_param(self):
+        '''
+        We should get an error if we do not pass a document_id
+        '''
         resp = self.ts.get(self, '/api/v1/queue_process/')
+        response_data = self.deserialize(resp)
         self.assertHttpApplicationError(resp)
+        self.assertEqual(str(response_data['error'])\
+            ,'Missing required parameter document_id')
