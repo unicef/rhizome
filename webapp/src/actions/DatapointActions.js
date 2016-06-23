8ac854eaffac58adcff72e17d7a60e215e13c9c2
John Dingee <dingeej@gmail.com>
2016-06-23 14:30:08 -0400

    cleanup how the chart wizard queries for data

--- webapp/src/actions/DatapointActions.js
+++ webapp/src/actions/DatapointActions.js
@@ -1,7 +1,5 @@
-import _ from 'lodash'
 import Reflux from 'reflux'
 import api from 'utilities/api'
-import builderDefinitions from 'components/d3chart/utils/builderDefinitions'
 
 const DatapointActions = Reflux.createActions({
   'fetchDatapoints': { children: ['completed', 'failed'], asyncResult: true },
@@ -22,22 +20,20 @@ DatapointActions.fetchDatapoints.listen(params => {
 // ACTION HELPERS
 // ---------------------------------------------------------------------------
 const _prepDatapointsQuery = (params) => {
-  const chartNeedsNullData = _.indexOf(builderDefinitions.need_missing_data_charts, params.type) !== -1
+  // const chartNeedsNullData = _.indexOf(builderDefinitions.need_missing_data_charts, params.type) !== -1
   let query = {
     campaign__in: params.campaign__in || params.campaign_ids,
     indicator__in: params.indicator_ids.join(),
-    location_id__in: params.location_ids.join(),
-    location_depth: params.location_depth <= 0 ? null : params.location_depth,
     campaign_start: params.start_date,
     campaign_end: params.end_date,
-    chart_type: params.type,
     chart_uuid: params.uuid,
-    show_missing_data: chartNeedsNullData ? 1 : params.show_missing_data,
     group_by_time: params.group_by_time,
-    source_name: params.source_name,
     filter_indicator: params.indicator_filter ? params.indicator_filter.type : null,
     filter_value: params.indicator_filter ? params.indicator_filter.value : null,
-    location_level: params.type === 'TableChart' ? 'District' : null
+    // clean this up... we need ability to select discrete locations in the chart builder.
+    // currently, we will only allow for a single location and a "depth" param in the chart builder
+    location_id: params.location_ids[0],
+    location_depth: params.location_depth <= 0 ? null : params.location_depth
   }
   return query
 }
