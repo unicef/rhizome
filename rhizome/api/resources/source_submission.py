a11aced069d7186b09352177f2492979127c576d
John Dingee <dingeej@gmail.com>
2016-06-29 21:58:05 -0400

    document_id filter on source_submission

--- rhizome/api/resources/source_submission.py
+++ rhizome/api/resources/source_submission.py
@@ -15,3 +15,14 @@ class SourceSubmissionResource(BaseModelResource):
     class Meta(BaseModelResource.Meta):
         resource_name = 'source_submission'
         object_class = SourceSubmission
+        GET_params_required = ['document_id']
+
+    def apply_filters(self, request, applicable_filters):
+        """
+        An ORM-specific implementation of ``apply_filters``.
+        The default simply applies the ``applicable_filters`` as ``**kwargs``,
+        but should make it possible to do more advanced things.
+        """
+
+        doc_filter = {'document_id': request.GET.get('document_id')}
+        return self.get_object_list(request).filter(**doc_filter)
