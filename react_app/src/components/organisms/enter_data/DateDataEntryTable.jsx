3b00b4aee1a0bf3ef4d674380b873648e5947dc5
Dima Markus <dmi3000@gmail.com>
2016-07-13 10:15:16 -0400

    Add button to delete date datapoint in data entry

--- react_app/src/components/organisms/enter_data/DateDataEntryTable.jsx
+++ react_app/src/components/organisms/enter_data/DateDataEntryTable.jsx
@@ -1,5 +1,6 @@
 import _ from 'lodash'
 import React, {Component} from 'react'
+import IconButton from 'components/button/IconButton'
 import ResourceTable from 'components/molecules/ResourceTable'
 import moment from 'moment'
 import format from 'utilities/format'
@@ -38,6 +39,13 @@ class DataEntryTable extends Component {
     this.refs.new_value.value = null
   }
 
+  _removeDatapoint = datapoint => {
+    if (confirm('Are you sure you want to delete this datapoint?')) {
+      console.log('datapoint', datapoint)
+    }
+     // this.props.removeDatapoint(result.data)
+  }
+
   render = () => {
     const props = this.props
     const datapoints = props.datapoints.flattened
@@ -61,6 +69,9 @@ class DataEntryTable extends Component {
       }
       return (
         <tr>
+          <td>
+            <IconButton onClick={() => this._removeDatapoint(datapoint)} icon='fa-times-circle' text='Remove datapoint' />
+          </td>
           <td style={cell_style}>{moment(datapoint.data_date).format('YYYY-MM-DD')}</td>
           <td style={cell_style}>{value_cell}</td>
         </tr>
@@ -69,9 +80,10 @@ class DataEntryTable extends Component {
 
     const new_datapoint_row = (
       <tr>
-        <td>
+        <td></td>
+        <td style={{width: '30rem'}}>
           <DateTimePicker
-            style={{left: '.9rem'}}
+            style={{left: '.65rem'}}
             value={this.state.new_date}
             time={false}
             format={'YYYY-MM-DD'}
@@ -91,6 +103,7 @@ class DataEntryTable extends Component {
       <table style={{width: '30rem'}}>
         <thead>
           <tr>
+            <th></th>
             <th style={cell_style}>Date</th>
             <th style={cell_style}>Value</th>
           </tr>
