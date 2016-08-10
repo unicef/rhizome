f516d2ec55fd375827f4e99f491f53a906d765fb
John Dingee <dingeej@gmail.com>
2016-08-09 20:30:28 -0400

    remove final python office import

--- rhizome/api/resources/cache_meta.py
+++ rhizome/api/resources/cache_meta.py
@@ -1,5 +1,4 @@
 from rhizome.api.resources.base_non_model import BaseNonModelResource
-from rhizome.models.office_models import Office
 from rhizome.cache_meta import cache_all_meta
 
 
@@ -7,7 +6,7 @@ class CacheMetaResource(BaseNonModelResource):
 
     class Meta(BaseNonModelResource.Meta):
         resource_name = 'cache_meta'
-        queryset = Office.objects.all().values()
+        queryset = Campaign.objects.all().values()
 
     def pre_process_data(self, request):
         cache_all_meta()
